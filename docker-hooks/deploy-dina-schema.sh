561696e2b16487280a4b368827b9be47da982480
antonoberg <anton.oberg@mobiento.com>
2018-02-07 09:21:23 +0100

    Add deploy-hook for api

--- docker-hooks/deploy-dina-schema.sh
+++
@@ -1,3 +0,0 @@
-UPDATE_PATH=/var/dina-collections-docker
-
-cd ${UPDATE_PATH} && docker-compose pull dina-schema && docker-compose up -d dina-schema
--- docker-hooks/index.js
+++ docker-hooks/index.js
@@ -1,6 +1,6 @@
 module.exports = {
+  'dina/dina-collections-api': 'deploy-dina-collections-api.sh',
   'dina/dina-collections-ui': 'deploy-dina-collections-ui.sh',
   'dina/dina-collections-ui-mock': 'deploy-dina-collections-ui-mock.sh',
   'dina/dina-semantic-ui-docs': 'deploy-dina-semantic-ui-docs.sh',
-  'dina/dina-schema': 'deploy-dina-schema.sh',
 }
