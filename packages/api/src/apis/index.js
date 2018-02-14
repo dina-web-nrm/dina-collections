5db9eae0c5070c43fac3326e6d039244fbe3e6ef
antonoberg <oberg.anton@gmail.com>
2018-02-14 09:18:48 +0100

    Add status endpoint

--- packages/api/src/apis/index.js
+++ packages/api/src/apis/index.js
@@ -1,4 +1,6 @@
 exports.collectionsApi = require('./collectionsApi')
+exports.statusApi = require('./statusApi')
+exports.statusApu = require('./authApi')
 exports.authApi = require('./authApi')
 exports.localityApi = require('./localityApi')
 exports.slimTaxonomyApi = require('./slimTaxonomyApi')
