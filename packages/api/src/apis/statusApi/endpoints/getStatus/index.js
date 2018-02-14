3b706687f5fbde5a830225e47cbdce40be447fb7
antonoberg <oberg.anton@gmail.com>
2018-02-14 09:18:48 +0100

    Add status endpoint

---
+++ packages/api/src/apis/statusApi/endpoints/getStatus/index.js
@@ -0,0 +1,28 @@
+/* eslint-disable sort-keys */
+const routeHandler = require('./routeHandler')
+
+module.exports = {
+  method: 'get',
+  path: '/collections/api/v01/status',
+  resource: 'status',
+  response: {
+    raw: true,
+    schema: {
+      description: 'this is a desc',
+      content: {
+        type: 'object',
+        additionalProperties: false,
+        properties: {
+          data: {
+            type: 'object',
+            properties: {
+              up: { type: 'boolean' },
+            },
+          },
+        },
+      },
+    },
+  },
+  routeHandler,
+  summary: 'Gets a status object',
+}
---
+++ packages/api/src/apis/statusApi/endpoints/index.js
@@ -0,0 +1,2 @@
+exports.getStatus = require('./getStatus')
+
