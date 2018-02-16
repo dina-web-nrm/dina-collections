76291c7c855416b3acb7711c07fc22e8aa14c0d1
idali0226 <ida.li@nrm.se>
2018-02-16 14:26:29 +0100

    Add test for getIndividualGroups

---
+++ packages/api/src/apis/collectionsApi/endpoints/getIndividualGroups/e2e.spec.js
@@ -0,0 +1,181 @@
+const apiDescribe = require('../../../../utilities/test/apiDescribe')
+const { makeTestCall } = require('../../../../utilities/test/testApiClient')
+const waitForApiRestart = require('../../../../utilities/test/waitForApiRestart')
+
+const validCatalogNumber = '123456'
+const validTaxonName = 'Chironectes minimus'
+
+const fullFormExample = {
+  data: {
+    attributes: {
+      causeOfDeathStandardized: 'Standardized death cause',
+      causeOfDeathText: 'Cause of death ',
+      featureObservations: [
+        {
+          featureObservationText: 'A condition at collecting',
+          featureObservationType: {
+            featureObservationTypeName: 'conditionAtCollecting',
+            id: '2',
+          },
+        },
+        {
+          featureObservationAgent: 'JD',
+          featureObservationDate: 'A date',
+          featureObservationText: 'male',
+          featureObservationType: {
+            featureObservationTypeName: 'sex',
+            id: '1',
+          },
+          methodText: 'method text',
+        },
+      ],
+      identifications: [
+        {
+          identificationRemarks: 'some remarks',
+          identifiedAsVerbatim: 'Sorex minutus',
+          identifiedByAgentText: 'Doe, J.',
+          identifiedDateText: 'Date text',
+          identifiedTaxonNameStandardized: validTaxonName,
+          isCurrentIdentification: true,
+        },
+      ],
+      occurrences: [
+        {
+          collectorsText: 'Bergström, U',
+          dayEnd: 15,
+          dayStart: 15,
+          establishmentMeansStandardized: 'establishmentMeansStandardized',
+          expeditionText: 'Vega Expedition',
+          isDeathEvent: true,
+          localityInformation: {
+            coordinatesVerbatim: 'coord-string',
+            curatedLocalities: [
+              {
+                id: '123',
+              },
+              {
+                id: '125',
+              },
+            ],
+            georeferenceSourcesText: 'georeferenceSourcesText text',
+            localityRemarks: 'localityRemarks text',
+            localityVerbatim: 'Some localityVerbatim text',
+            position: {
+              latitude: 'latitude-string',
+              longitude: 'longitude-string',
+              uncertaintyInMeters: 10,
+            },
+            verticalPosition: {
+              maximumDepthInMeters: 100,
+              maximumElevationInMeters: 100,
+              minimumDepthInMeters: 20,
+              minimumElevationInMeters: 20,
+            },
+          },
+          monthEnd: 1,
+          monthStart: 1,
+          occurrenceDateText: '15 jan 1986',
+          yearEnd: 1986,
+          yearStart: 1986,
+        },
+      ],
+      originStandardized: 'Standardized origin',
+      physicalUnits: [
+        {
+          alternateIdentifiersText: 'alternateIdentifiersText',
+          catalogedUnit: {
+            catalogNumber: validCatalogNumber,
+            publishRecord: true,
+            storedUnderTaxonName: 'Sorex minutus',
+          },
+          normalStorageLocationText: 'normalStorageLocationText',
+          physicalUnitText: 'physicalUnitText',
+        },
+      ],
+    },
+  },
+}
+
+apiDescribe('individualGroup', () => {
+  let authToken
+  beforeAll(() => {
+    return waitForApiRestart().then(() => {
+      authToken = 1234
+    })
+  })
+
+  it('Runs individualGroup tests', () => {
+    expect(!!authToken).toBeTruthy()
+    expect(1).toBe(1)
+  })
+
+  describe('getIndividualGroups', () => {
+    beforeEach(() => {
+      return makeTestCall({
+        authToken,
+        body: fullFormExample,
+        operationId: 'createIndividualGroup',
+      })
+    })
+    describe('by catalogNumber', () => {
+      it('Succeed with valid catalogNumber', () => {
+        return makeTestCall({
+          authToken,
+          operationId: 'getIndividualGroups',
+          queryParams: { 'filter[catalogNumber]': validCatalogNumber },
+        }).then(res => {
+          expect(res).toBeTruthy()
+          expect(res.data).toBeTruthy()
+          expect(Array.isArray(res.data)).toBe(true)
+          expect(res.data.length > 0).toBe(true)
+        })
+      })
+      it('Succeed with valid catalogNumber and includes', () => {
+        return makeTestCall({
+          authToken,
+          operationId: 'getIndividualGroups',
+          queryParams: {
+            'filter[catalogNumber]': validCatalogNumber,
+            include: 'identifications,physicalUnits.catalogedUnit',
+          },
+        }).then(res => {
+          expect(res).toBeTruthy()
+          expect(res.data).toBeTruthy()
+          expect(Array.isArray(res.data)).toBe(true)
+          expect(res.data.length > 0).toBe(true)
+        })
+      })
+    })
+    describe('by identifiedTaxonNameStandardized', () => {
+      it('Succeed with valid identifiedTaxonNameStandardized', () => {
+        return makeTestCall({
+          authToken,
+          operationId: 'getIndividualGroups',
+          queryParams: {
+            'filter[identifiedTaxonNameStandardized]': validTaxonName,
+          },
+        }).then(res => {
+          expect(res).toBeTruthy()
+          expect(res.data).toBeTruthy()
+          expect(Array.isArray(res.data)).toBe(true)
+          expect(res.data.length > 0).toBe(true)
+        })
+      })
+      it('Succeed with valid identifiedTaxonNameStandardized and includes', () => {
+        return makeTestCall({
+          authToken,
+          operationId: 'getIndividualGroups',
+          queryParams: {
+            'filter[identifiedTaxonNameStandardized]': validTaxonName,
+            include: 'identifications,physicalUnits.catalogedUnit',
+          },
+        }).then(res => {
+          expect(res).toBeTruthy()
+          expect(res.data).toBeTruthy()
+          expect(Array.isArray(res.data)).toBe(true)
+          expect(res.data.length > 0).toBe(true)
+        })
+      })
+    })
+  })
+})
