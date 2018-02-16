f4b86b9004357d44cfe432266a2f5a70ca3f5904
Fredrik Olovsson <fogithub@gmail.com>
2018-02-16 13:44:48 +0100

    Refactor Search components to Semantic UI propType
    The "content" prop now needs to be a React Node in the latest version
    of Semantic. We used it just as an object to pass props to the
    resultRenderer, but now we need to adapt so we only pass the allowed
    props (e.g. "title") to the resultRenderer.

--- packages/ui/src/coreModules/form/components/InputDatePart/dateOptions.js
+++ packages/ui/src/coreModules/form/components/InputDatePart/dateOptions.js
@@ -3,17 +3,16 @@ export const months = []
 export const years = []
 const currentYear = new Date().getFullYear()
 
-/* eslint-disable no-plusplus */
-// A string key is needed for Semantic-UI Search component
-for (let day = 1; day <= 31; day++) {
-  days.push({ key: String(day), value: day })
+// A string key and title is needed for Semantic UI Search component
+// (and it is not possible to call it "value" instead)
+for (let day = 1; day <= 31; day += 1) {
+  days.push(String(day))
 }
 
-for (let month = 1; month <= 12; month++) {
-  months.push({ key: String(month), value: month })
+for (let month = 1; month <= 12; month += 1) {
+  months.push(String(month))
 }
 
-for (let year = currentYear; year >= 1500; year--) {
-  years.push({ key: String(year), value: year })
+for (let year = 1500; year <= currentYear; year += 1) {
+  years.push(String(year))
 }
-/* eslint-enable no-plusplus */
--- packages/ui/src/domainModules/taxonomy/components/TaxonNameSearchResult/index.js
+++ packages/ui/src/domainModules/taxonomy/components/TaxonNameSearchResult/index.js
@@ -2,18 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const propTypes = {
-  content: PropTypes.shape({
-    attributes: PropTypes.shape({
-      scientificName: PropTypes.string.isRequired,
-    }).isRequired,
-  }).isRequired,
+  title: PropTypes.string.isRequired,
 }
 
-function TaxonomyAutocompleteResult({ content }) {
-  const scientificName =
-    content && content.attributes && content.attributes.scientificName
-
-  return scientificName ? <div>{scientificName}</div> : null
+function TaxonomyAutocompleteResult({ title }) {
+  return <div>{title}</div>
 }
 
 TaxonomyAutocompleteResult.propTypes = propTypes
