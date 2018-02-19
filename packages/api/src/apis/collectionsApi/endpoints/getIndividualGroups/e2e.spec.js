401e8b472fa9bcfe2f3cf49b5c5b4a163c00aebe
idali0226 <ida.li@nrm.se>
2018-02-19 15:58:17 +0100

    Add getIndividualGroup tests

--- packages/api/src/apis/collectionsApi/endpoints/getIndividualGroups/e2e.spec.js
+++ packages/api/src/apis/collectionsApi/endpoints/getIndividualGroups/e2e.spec.js
@@ -5,6 +5,8 @@ const { fullFormExample } = require('../../testData/individualGroup')
 
 const validCatalogNumber = '123456'
 const validTaxonName = 'Chironectes minimus'
+const invalidCatalogNumber = 'Some catalogNumber'
+const invalidTaxonName = 'Some taxa'
 
 apiDescribe('individualGroup', () => {
   let authToken
@@ -54,6 +56,34 @@ apiDescribe('individualGroup', () => {
           expect(res.data).toBeTruthy()
           expect(Array.isArray(res.data)).toBe(true)
           expect(res.data.length > 0).toBe(true)
+        })
+      })
+    })
+    describe('by invalid catalogNumber', () => {
+      it('Return empty array with invalid catalogNumber', () => {
+        return makeTestCall({
+          authToken,
+          operationId: 'getIndividualGroups',
+          queryParams: {
+            'filter[catalogNumber]': invalidCatalogNumber,
+          },
+        }).then(res => {
+          expect(res).toBeTruthy()
+          expect(res.data.length).toBe(0)
+        })
+      })
+    })
+    describe('by invalid identifiedTaxonNameStandardized', () => {
+      it('Return empty array with invalid identifiedTaxonNameStandardized', () => {
+        return makeTestCall({
+          authToken,
+          operationId: 'getIndividualGroups',
+          queryParams: {
+            'filter[identifiedTaxonNameStandardized]': invalidTaxonName,
+          },
+        }).then(res => {
+          expect(res).toBeTruthy()
+          expect(res.data.length).toBe(0)
         })
       })
     })
