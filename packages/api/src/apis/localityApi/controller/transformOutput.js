c74c34438fc49350155f8b66b3f6c4f92e7579c2
antonoberg <oberg.anton@gmail.com>
2018-02-20 09:20:23 +0100

    Remove not used localityApi controller

--- packages/api/src/apis/localityApi/controller/transformOutput.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-param-reassign */
-// might need to unpack with { dataValues }
-module.exports = function tranformOutput(output) {
-  return {
-    id: output.id,
-    versionId: `${output.versionId}`,
-    ...output.document,
-  }
-}
