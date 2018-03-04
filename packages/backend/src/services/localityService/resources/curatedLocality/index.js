bfcfe3a5ccafd0275c3245883ed2e009f60a3664
antonoberg <oberg.anton@gmail.com>
2018-03-04 12:29:47 +0100

    Update curated locality specification

--- packages/backend/src/services/localityService/resources/curatedLocality/index.js
+++ packages/backend/src/services/localityService/resources/curatedLocality/index.js
@@ -11,12 +11,67 @@ module.exports = {
       type: 'update',
     },
     {
+      includeRelations: false,
+      queryParams: {
+        'filter[descendantLevels]': {
+          description:
+            'Levels of descendants to include. Only relevant if relationships descendants provided',
+          example: ['country', 'city'],
+          required: false,
+          schema: {
+            items: {
+              type: 'string',
+            },
+            type: 'array',
+          },
+        },
+
+        includes: {
+          description: 'Add includes',
+          example: ['descendants', 'children'],
+          required: false,
+          schema: {
+            items: {
+              enum: ['descendants', 'children'],
+              type: 'string',
+            },
+            type: 'array',
+          },
+        },
+        relationships: {
+          description: 'Add relationships',
+          example: ['descendants', 'children'],
+          required: false,
+          schema: {
+            items: {
+              enum: ['descendants', 'children'],
+              type: 'string',
+            },
+            type: 'array',
+          },
+        },
+      },
       type: 'getOne',
     },
     {
+      includeRelations: false,
       type: 'getMany',
     },
   ],
+  relations: {
+    children: {
+      format: 'array',
+      resource: 'curatedLocality',
+    },
+    descendants: {
+      format: 'array',
+      resource: 'curatedLocality',
+    },
+    parent: {
+      format: 'object',
+      resource: 'curatedLocality',
+    },
+  },
   resource: 'curatedLocality',
   resourcePlural: 'curatedLocalities',
 }
