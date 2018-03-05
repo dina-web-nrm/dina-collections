40d33efc3f0d008ccc3e229837fd57afe8a7de01
Fredrik Olovsson <fogithub@gmail.com>
2018-03-05 14:48:22 +0100

    Fix typos

--- packages/ui/src/coreModules/bootstrap/components/DefaultWrapper.test.js
+++ packages/ui/src/coreModules/bootstrap/components/DefaultWrapper.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import uiDescribe from 'utilities/test/uiDescribe'
 import DefaultWrapper from './DefaultWrapper'
 
-uiDescribe('coreModles/bootstrap/DefaultWrapper', () => {
+uiDescribe('coreModules/bootstrap/DefaultWrapper', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div')
     ReactDOM.render(
