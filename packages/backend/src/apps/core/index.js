9d61917784a0bb5dae799782469cbd7da959b8f2
antonoberg <oberg.anton@gmail.com>
2018-03-08 16:05:46 +0100

    Add start time measurements

--- packages/backend/src/apps/core/index.js
+++ packages/backend/src/apps/core/index.js
@@ -1,3 +1,5 @@
+const now = require('performance-now')
+const startTime = now()
 const openApiSpec = require('common/dist/openApi.json')
 const createLog = require('../../utilities/log')
 const createServiceRouter = require('../../lib/serviceRouter')
@@ -7,13 +9,14 @@ const createModels = require('../../lib/sequelize/models')
 const createConnectors = require('../../lib/connectors')
 const serviceDefinitions = require('../../services')
 const createServices = require('../../lib/services')
-
 const config = require('./config')
 
 const log = createLog('server')
+log.info(`Dependencies required after: ${now() - startTime} milliseconds`)
 
 const services = createServices({ config, serviceDefinitions })
 
+const bootstrapStartTime = now()
 initializeSequelize({
   config,
 })
@@ -25,9 +28,11 @@ initializeSequelize({
     })
   })
   .then(({ models }) => {
+    log.info(`Sequalize initialized after: ${now() - startTime} milliseconds`)
     return createConnectors({ config, models, services })
   })
   .then(({ connectors }) => {
+    log.info(`Connectors created after: ${now() - startTime} milliseconds`)
     const serviceRouter = createServiceRouter({
       config,
       connectors,
@@ -37,7 +42,13 @@ initializeSequelize({
       openApiSpec,
       routers: [serviceRouter],
     })
+    log.info(`App configured after: ${now() - startTime} milliseconds`)
     return app.listen(config.api.port, () => {
+      log.info(
+        `Server started after: ${now() -
+          startTime} milliseconds. Bootstrap time: ${now() -
+          bootstrapStartTime}`
+      )
       log.info(`Api listening to port ${config.api.port}`)
     })
   })
