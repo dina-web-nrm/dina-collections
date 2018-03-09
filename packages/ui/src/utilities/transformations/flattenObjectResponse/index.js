3fe1127b532a3ea0910a4b03e4848bb4cadb2aff
Fredrik Olovsson <fogithub@gmail.com>
2018-03-09 16:48:42 +0100

    Add flattenRelationships utility

--- packages/ui/src/utilities/transformations/flattenObjectResponse/index.js
+++ packages/ui/src/utilities/transformations/flattenObjectResponse/index.js
@@ -1,3 +1,22 @@
+export const flattenRelationships = relationships => {
+  if (!relationships) {
+    return {}
+  }
+
+  const relationshipKeys = Object.keys(relationships)
+
+  if (!relationshipKeys.length) {
+    return {}
+  }
+
+  return relationshipKeys.reduce((flatRelations, relationKey) => {
+    return {
+      ...flatRelations,
+      [relationKey]: relationships[relationKey].data,
+    }
+  }, {})
+}
+
 export default function flattenObjectResponse(response) {
   if (!response || !response.data) {
     return response
@@ -5,10 +24,12 @@ export default function flattenObjectResponse(response) {
 
   const { id, attributes, relationships, type } = response.data
 
+  const flattenedRelationships = flattenRelationships(relationships)
+
   return {
     ...attributes,
+    ...flattenedRelationships,
     id,
-    relationships,
     type,
   }
 }
