a2e5b7748bb5892b4404c5d55a3bb0258e8e9ecf
Fredrik Olovsson <fogithub@gmail.com>
2018-03-09 16:50:31 +0100

    Remove unnecessary methodNames

--- packages/ui/src/domainModules/storageService/endpoints.js
+++ packages/ui/src/domainModules/storageService/endpoints.js
@@ -1,31 +1,25 @@
 import createEndpoint from 'utilities/endpointFactory/client'
 
 export const CREATE_PHYSICAL_UNIT = createEndpoint({
-  methodName: 'post',
   operationId: 'createPhysicalUnit',
 })
 
 export const GET_PHYSICAL_UNIT = createEndpoint({
-  methodName: 'get',
   operationId: 'getPhysicalUnit',
 })
 
 export const UPDATE_PHYSICAL_UNIT = createEndpoint({
-  methodName: 'put',
   operationId: 'updatePhysicalUnit',
 })
 
 export const CREATE_STORAGE_LOCATION = createEndpoint({
-  methodName: 'post',
   operationId: 'createStorageLocation',
 })
 
 export const GET_STORAGE_LOCATION = createEndpoint({
-  methodName: 'get',
   operationId: 'getStorageLocation',
 })
 
 export const UPDATE_STORAGE_LOCATION = createEndpoint({
-  methodName: 'put',
   operationId: 'updateStorageLocation',
 })
