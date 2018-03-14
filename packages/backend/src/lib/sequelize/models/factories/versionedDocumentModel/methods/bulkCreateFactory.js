2e4123ce6ff2b1f9a5a4cbde94a3df3225973639
antonoberg <oberg.anton@gmail.com>
2018-03-14 09:43:31 +0100

    Use chanPromises for bulk create

--- packages/backend/src/lib/sequelize/models/factories/versionedDocumentModel/methods/bulkCreateFactory.js
+++ packages/backend/src/lib/sequelize/models/factories/versionedDocumentModel/methods/bulkCreateFactory.js
@@ -1,3 +1,4 @@
+const chainPromises = require('common/src/chainPromises')
 const createLog = require('../../../../../../utilities/log')
 
 const log = createLog(
@@ -11,24 +12,21 @@ module.exports = function bulkCreateFactory({ Model, schemaVersion } = {}) {
 
   // This should only be used to create test initialData
   return function bulkCreate(items) {
-    const data = items.map(item => {
-      if (!item.id) {
-        throw new Error('bulkCreate required specified ids')
-      }
-      return {
-        document: item.doc,
-        id: item.id,
-        isCurrentVersion: true,
-        schemaCompliant: false,
-        schemaVersion: schemaVersion || undefined,
-        versionId: item.id,
+    log.debug(`Start create ${items.length} items for: ${Model.tableName}`)
+
+    const promises = items.map(item => {
+      return () => {
+        return Model.create({
+          document: item.doc,
+          isCurrentVersion: true,
+          schemaCompliant: false,
+          schemaVersion: schemaVersion || undefined,
+        })
       }
     })
-
-    log.debug(`Start create ${data.length} items for: ${Model.tableName}`)
-    return Model.bulkCreate(data).then(result => {
-      log.debug(`Created ${data.length} items for: ${Model.tableName}`)
-      return result
+    return chainPromises(promises).then(() => {
+      log.debug(`Done create ${items.length} items`)
+      return null
     })
   }
 }
