ba913fb621c573f6e1d9cd5264c25c5e1df1b8cc
Fredrik Olovsson <fogithub@gmail.com>
2018-03-19 18:27:08 +0100

    Add getUpdateSpecimenInitialValues selector

--- packages/ui/src/domainModules/collectionMammals/globalSelectors.js
+++ packages/ui/src/domainModules/collectionMammals/globalSelectors.js
@@ -1,4 +1,37 @@
+import { createSelector } from 'reselect'
+
+import {
+  getLocalState as getCuratedListState,
+  getFeatureObservationTypes,
+} from 'domainModules/curatedListService/selectors'
+import {
+  getLocalState as getSpecimenState,
+  getSpecimenIndividualGroup,
+} from 'domainModules/specimenService/selectors'
+import {
+  getLocalState as getStorageState,
+  getPhysicalUnits,
+} from 'domainModules/storageService/selectors'
+
 import wrapSelectors from 'utilities/wrapSelectors'
+import transformInput from './components/MammalForm/transformations/input'
 import * as selectors from './selectors'
 
-export default wrapSelectors(selectors)
+const getUpdateSpecimenInitialValues = createSelector(
+  state => getFeatureObservationTypes(getCuratedListState(state)),
+  (state, specimenId) =>
+    getSpecimenIndividualGroup(getSpecimenState(state), specimenId),
+  state => getPhysicalUnits(getStorageState(state)),
+  (featureObservationTypes, individualGroup, physicalUnits) => {
+    return transformInput({
+      featureObservationTypes,
+      individualGroup,
+      physicalUnits,
+    })
+  }
+)
+
+export default {
+  ...wrapSelectors(selectors),
+  getUpdateSpecimenInitialValues,
+}
