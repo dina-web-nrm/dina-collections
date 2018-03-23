91ea36923853a7bd1cae6e21bf96c9f27084bbe1
Fredrik Olovsson <fogithub@gmail.com>
2018-03-23 10:51:41 +0100

    Remove unused accordion in redux module

--- packages/ui/src/domainModules/collectionMammals/actionCreators/index.js
+++ packages/ui/src/domainModules/collectionMammals/actionCreators/index.js
@@ -1,6 +1,5 @@
 import clearSearchParameters from './clearSearchParameters'
 import lookupMammals from './lookupMammals'
-import setAccordionActiveIndex from './setAccordionActiveIndex'
 import updateFeatureObservationSearchQuery from './updateFeatureObservationSearchQuery'
 import updateLocalityInformationSearchQuery from './updateLocalityInformationSearchQuery'
 import updateSearchParameter from './updateSearchParameter'
@@ -8,7 +7,6 @@ import updateSearchParameter from './updateSearchParameter'
 export {
   clearSearchParameters,
   lookupMammals,
-  setAccordionActiveIndex,
   updateFeatureObservationSearchQuery,
   updateLocalityInformationSearchQuery,
   updateSearchParameter,
