3f36111eebed2cf7abf23eb2a72cd8ed3d4d23a1
antonoberg <oberg.anton@gmail.com>
2018-03-26 15:48:18 +0200

    Make capitalizeFirstLetter not throw if no input

--- packages/common/es5/stringFormatters/capitalizeFirstLetter/index.js
+++ packages/common/es5/stringFormatters/capitalizeFirstLetter/index.js
@@ -5,5 +5,8 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = capitalizeFirstLetter;
 function capitalizeFirstLetter(string) {
+  if (!string) {
+    return string;
+  }
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
--- packages/common/src/stringFormatters/capitalizeFirstLetter/index.js
+++ packages/common/src/stringFormatters/capitalizeFirstLetter/index.js
@@ -1,3 +1,6 @@
 export default function capitalizeFirstLetter(string) {
+  if (!string) {
+    return string
+  }
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
