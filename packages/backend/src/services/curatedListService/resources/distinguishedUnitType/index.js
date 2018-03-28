12774fac8ac45274de6ff80817834267e0e54327
Fredrik Olovsson <fogithub@gmail.com>
2018-03-28 16:21:54 +0200

    Fix API URL for distinguishedUnitTypes

--- packages/backend/src/services/curatedListService/resources/distinguishedUnitType/index.js
+++ packages/backend/src/services/curatedListService/resources/distinguishedUnitType/index.js
@@ -1,7 +1,7 @@
 const createDistinguishedUnitTypeRequestSuccess = require('./operations/create/examples/requestSuccess.json')
 
 module.exports = {
-  basePath: '/curatedListApi/v01',
+  basePath: '/api/curatedList/v01',
   operations: [
     {
       exampleRequests: {
