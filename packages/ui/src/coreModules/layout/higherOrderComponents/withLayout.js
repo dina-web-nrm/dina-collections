adfd24f43fae346c4b11f43b9fa34cb73aace1d9
Fredrik Olovsson <fogithub@gmail.com>
2018-04-11 22:05:18 +0200

    Change withLayout HOC to take optional layoutName
    And pass setLayoutModeByName method, to be able to set any layout.
    setLayoutMode will act on the layoutName provided (or 'default').

--- packages/ui/src/coreModules/layout/higherOrderComponents/withLayout.js
+++ packages/ui/src/coreModules/layout/higherOrderComponents/withLayout.js
@@ -5,33 +5,61 @@ import { connect } from 'react-redux'
 import { compose } from 'redux'
 import { actionCreators, globalSelectors } from '../keyObjectModule'
 
-export default function withLayout(ComposedComponent) {
-  const mapStateToProps = state => ({
-    layoutMode: globalSelectors.get.layoutMode(state),
+const withLayout = (layoutName = 'default') => ComposedComponent => {
+  const mapStateToProps = (state, { name }) => ({
+    layoutMode: globalSelectors.get[':name.layoutMode'](state, {
+      name: name || layoutName, // prefer prop name, if provided
+    }),
   })
 
-  const mapDispathToProps = {
-    setLayoutMode: actionCreators.set.layoutMode,
+  const mapDispatchToProps = {
+    setLayoutMode: actionCreators.set[':name.layoutMode'],
   }
 
   const propTypes = {
-    layoutMode: PropTypes.string.isRequired,
+    layoutMode: PropTypes.string,
+    name: PropTypes.string,
     setLayoutMode: PropTypes.func.isRequired,
+  }
+  const defaultProps = {
+    layoutMode: undefined,
+    name: undefined,
   }
 
   class LayoutHoc extends Component {
+    constructor(props) {
+      super(props)
+      this.setLayoutMode = this.setLayoutMode.bind(this)
+      this.setLayoutModeByName = this.setLayoutModeByName.bind(this)
+    }
+
+    setLayoutMode(value) {
+      return this.props.setLayoutMode(value, {
+        name: this.props.name || layoutName,
+      })
+    }
+
+    setLayoutModeByName(name, value) {
+      return this.props.setLayoutMode(value, { name })
+    }
+
     render() {
-      const { setLayoutMode, layoutMode } = this.props
+      const { layoutMode } = this.props
       return (
         <ComposedComponent
-          layoutMode={layoutMode}
-          setLayoutMode={setLayoutMode}
           {...this.props}
+          layoutMode={layoutMode}
+          setLayoutMode={this.setLayoutMode}
+          setLayoutModeByName={this.setLayoutModeByName}
         />
       )
     }
   }
 
   LayoutHoc.propTypes = propTypes
-  return compose(connect(mapStateToProps, mapDispathToProps))(LayoutHoc)
+  LayoutHoc.defaultProps = defaultProps
+
+  return compose(connect(mapStateToProps, mapDispatchToProps))(LayoutHoc)
 }
+
+export default withLayout
