c6715135ea6f1e25af77606506acea12a92961c7
Fredrik Olovsson <fogithub@gmail.com>
2018-04-11 21:51:09 +0200

    Update storageLocation model & add parent/child

--- packages/backend/src/services/storageService/resources/storageLocation/index.js
+++ packages/backend/src/services/storageService/resources/storageLocation/index.js
@@ -1,4 +1,5 @@
 const createStorageLocationRequestSuccess = require('./operations/create/examples/requestSuccess.json')
+const buildWhere = require('./operations/getMany/buildWhere')
 
 module.exports = {
   basePath: '/api/storage/v01',
@@ -15,19 +16,91 @@ module.exports = {
     //   type: 'updateRelationHasMany',
     // },
     {
+      relationKey: 'parent',
+      type: 'updateRelationHasOne',
+    },
+    {
       relationKey: 'physicalObjects',
       type: 'getRelationHasMany',
     },
     {
       includeRelations: true,
+      queryParams: {
+        'filter[descendantLevels]': {
+          description:
+            'NOT IMPLEMENTED - Levels of descendants to include. Only relevant if relationships descendants provided. example: [level 3, level 4]',
+          required: false,
+          schema: {
+            items: {
+              type: 'string',
+            },
+            type: 'array',
+          },
+        },
+        includes: {
+          description:
+            'NOT IMPLEMENTED - Add includes. example [descendants, children]',
+          required: false,
+          schema: {
+            items: {
+              enum: ['descendants', 'children'],
+              type: 'string',
+            },
+            type: 'array',
+          },
+        },
+        relationships: {
+          description: 'Add relationships. example [descendants, children]',
+          required: false,
+          schema: {
+            items: {
+              enum: ['descendants', 'children'],
+              type: 'string',
+            },
+            type: 'array',
+          },
+        },
+      },
       type: 'getOne',
     },
     {
+      buildWhere,
       includeRelations: true,
+      queryParams: {
+        'filter[group]': {
+          description: 'Filter by group, example: country',
+          required: false,
+          schema: {
+            type: 'string',
+          },
+        },
+        'filter[parentId]': {
+          description: 'Filter by parentId, example 123',
+          required: false,
+          schema: {
+            type: 'string',
+          },
+        },
+        'filter[search]': {
+          description: 'Filter by string search, example bone room',
+          required: false,
+          schema: {
+            type: 'string',
+          },
+        },
+      },
       type: 'getMany',
     },
   ],
   relations: {
+    children: {
+      format: 'array',
+      resource: 'storageLocation',
+    },
+    parent: {
+      format: 'object',
+      resource: 'storageLocation',
+    },
     physicalObjects: {
       format: 'array',
       resource: 'physicalObject',
