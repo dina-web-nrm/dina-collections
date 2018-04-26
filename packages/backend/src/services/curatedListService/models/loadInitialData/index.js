a81761d4e60171ee4d63f116abe4c542e1d02ec8
antonoberg <oberg.anton@gmail.com>
2018-04-26 16:54:34 +0200

    [BACKEND] Simplify curatedList loadInitialData

--- packages/backend/src/services/curatedListService/models/loadInitialData/index.js
+++ packages/backend/src/services/curatedListService/models/loadInitialData/index.js
@@ -1,7 +1,7 @@
 const readInitialData = require('../../../../utilities/readInitialData')
 
-const loadType = ({ initialDataName, modelName, models }) => {
-  const types = readInitialData(initialDataName)
+const loadType = ({ initialDataName, modelName, models, isJson = true }) => {
+  const types = readInitialData(initialDataName, { isJson })
   if (!types) {
     return Promise.resolve()
   }
@@ -16,88 +16,37 @@ const loadType = ({ initialDataName, modelName, models }) => {
 }
 
 module.exports = function loadInitialData({ models }) {
-  const typeSpecimenTypes = readInitialData('typeSpecimenTypes')
-  const establishmentMeansTypes = readInitialData('establishmentMeansTypes')
-  const causeOfDeathTypes = readInitialData('causeOfDeathTypes')
-  const preparationTypes = readInitialData('preparationTypes')
-  const featureTypes = readInitialData('featureTypes', { isJson: false })
-
-  const typeSpecimenTypesItems = !typeSpecimenTypes
-    ? null
-    : typeSpecimenTypes.map((typeSpecimenType, index) => {
-        return {
-          doc: typeSpecimenType,
-          id: index + 1,
-        }
-      })
-
-  const establishmentMeansTypeItems = !establishmentMeansTypes
-    ? null
-    : establishmentMeansTypes.map((establishmentMeansType, index) => {
-        return {
-          doc: establishmentMeansType,
-          id: index + 1,
-        }
-      })
-
-  const causeOfDeathTypeItems = !causeOfDeathTypes
-    ? null
-    : causeOfDeathTypes.map((causeOfDeathType, index) => {
-        return {
-          doc: causeOfDeathType,
-          id: index + 1,
-        }
-      })
-
-  const featureTypeItems = !featureTypes
-    ? null
-    : featureTypes.map((featureType, index) => {
-        return {
-          doc: featureType,
-          id: index + 1,
-        }
-      })
-
-  const preparationTypeItems = !preparationTypes
-    ? null
-    : preparationTypes.map(preparationType => {
-        const { id, ...rest } = preparationType
-        return {
-          doc: { ...rest },
-          id,
-        }
-      })
-
-  const typeSpecimenTypesItemsPromises = typeSpecimenTypesItems
-    ? models.typeSpecimenType.bulkCreate(typeSpecimenTypesItems)
-    : Promise.resolve()
-
-  const establishmentMeansTypeItemsPromises = establishmentMeansTypeItems
-    ? models.establishmentMeansType.bulkCreate(establishmentMeansTypeItems)
-    : Promise.resolve()
-
-  const causeOfDeathTypeItemsPromise = causeOfDeathTypeItems
-    ? models.causeOfDeathType.bulkCreate(causeOfDeathTypeItems)
-    : Promise.resolve()
-
-  const featureTypeItemsPromise = featureTypeItems
-    ? models.featureType.bulkCreate(featureTypeItems)
-    : Promise.resolve()
-
-  const preparationTypeItemsPromise = preparationTypeItems
-    ? models.preparationType.bulkCreate(preparationTypeItems)
-    : Promise.resolve()
-
   return Promise.all([
     loadType({
       initialDataName: 'identifierTypes',
       modelName: 'identifierType',
       models,
     }),
-    causeOfDeathTypeItemsPromise,
-    establishmentMeansTypeItemsPromises,
-    featureTypeItemsPromise,
-    preparationTypeItemsPromise,
-    typeSpecimenTypesItemsPromises,
+    loadType({
+      initialDataName: 'causeOfDeathTypes',
+      modelName: 'causeOfDeathType',
+      models,
+    }),
+    loadType({
+      initialDataName: 'establishmentMeansTypes',
+      modelName: 'establishmentMeansType',
+      models,
+    }),
+    loadType({
+      initialDataName: 'featureTypes',
+      isJson: false,
+      modelName: 'featureType',
+      models,
+    }),
+    loadType({
+      initialDataName: 'preparationTypes',
+      modelName: 'preparationType',
+      models,
+    }),
+    loadType({
+      initialDataName: 'typeSpecimenTypes',
+      modelName: 'typeSpecimenType',
+      models,
+    }),
   ])
 }
