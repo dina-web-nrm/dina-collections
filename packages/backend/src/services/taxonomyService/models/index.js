cc7302761b421c8557de39ad433cad16d471e539
Fredrik Olovsson <fogithub@gmail.com>
2018-04-27 18:55:06 +0200

    [BACKEND] Add/update taxonomy models

--- packages/backend/src/services/taxonomyService/models/index.js
+++ packages/backend/src/services/taxonomyService/models/index.js
@@ -10,8 +10,17 @@ const taxonFactory = function taxon({ sequelize }) {
   })
 }
 
+const taxonNameFactory = function taxonName({ sequelize }) {
+  return createModel({
+    name: 'TaxonName',
+    schemaModelName: 'taxonName',
+    schemaVersion: '1.0.1',
+    sequelize,
+  })
+}
+
 const setupRelations = function setupRelations({ models }) {
-  const { taxon } = models
+  const { taxon, taxonName } = models
   taxon.Model.hasMany(taxon.Model, {
     as: 'children',
     foreignKey: 'parentVersionId',
@@ -21,12 +30,43 @@ const setupRelations = function setupRelations({ models }) {
     foreignKey: 'parentVersionId',
     targetKey: 'versionId',
   })
+  taxon.Model.hasOne(taxonName.Model, {
+    as: 'acceptedTaxonName',
+    foreignKey: 'acceptedToTaxonVersionId',
+  })
+  taxonName.Model.belongsTo(taxon.Model, {
+    as: 'acceptedToTaxon',
+    foreignKey: 'acceptedToTaxonVersionId',
+    targetKey: 'versionId',
+  })
+  taxon.Model.hasMany(taxonName.Model, {
+    as: 'synonyms',
+    foreignKey: 'synonymToTaxonVersionId',
+  })
+  taxonName.Model.belongsTo(taxon.Model, {
+    as: 'synonymToTaxon',
+    foreignKey: 'synonymToTaxonVersionId',
+    targetKey: 'versionId',
+  })
+  taxon.Model.hasMany(taxonName.Model, {
+    as: 'vernacularNames',
+    foreignKey: 'vernacularToTaxonVersionId',
+  })
+  taxonName.Model.belongsTo(taxon.Model, {
+    as: 'vernacularToTaxon',
+    foreignKey: 'vernacularToTaxonVersionId',
+    targetKey: 'versionId',
+  })
 }
 
 module.exports = [
   {
     factory: taxonFactory,
     name: 'taxon',
+  },
+  {
+    factory: taxonNameFactory,
+    name: 'taxonName',
   },
   {
     factory: setupRelations,
