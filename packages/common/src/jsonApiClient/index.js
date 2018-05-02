d79e8e91f27c5964f221cd7bc5ecd7341636cd93
antonoberg <oberg.anton@gmail.com>
2018-05-02 11:52:14 +0200

    [COMMON] Separate jsonApi client and openApi client

--- packages/common/src/jsonApiClient/index.js
+++ packages/common/src/jsonApiClient/index.js
@@ -1,38 +1,11 @@
-const { createApiClient } = require('../apiClient')
+const createOpenApiClient = require('../openApiClient')
 
 module.exports = function createJsonApiClient({
   apiConfigInput,
   createEndpoint,
 }) {
-  const apiClient = createApiClient(apiConfigInput)
-  const endpointConfigCache = {}
-  const call = (input, userInput) => {
-    let endpointConfig
-    let operationId
-    if (typeof input === 'string') {
-      operationId = input
-    } else {
-      endpointConfig = input
-    }
-
-    if (!endpointConfig) {
-      if (endpointConfigCache[operationId]) {
-        endpointConfig = endpointConfigCache[operationId]
-      } else {
-        endpointConfig = createEndpoint({ operationId })
-        if (!endpointConfig) {
-          throw new Error(
-            `Cant create endpoint config for operationId: ${operationId}`
-          )
-        }
-        endpointConfigCache[operationId] = endpointConfig
-      }
-    }
-
-    return apiClient.call(endpointConfig, userInput)
-  }
-
-  return {
-    call,
-  }
+  return createOpenApiClient({
+    apiConfigInput,
+    createEndpoint,
+  })
 }
