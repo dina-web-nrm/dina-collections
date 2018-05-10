9028b89f0f5726852c3be3d0e1948d85385bf359
Fredrik Olovsson <fogithub@gmail.com>
2018-05-10 12:30:13 +0800

    [BACKEND] Add del controller

--- packages/backend/src/lib/controllers/index.js
+++ packages/backend/src/lib/controllers/index.js
@@ -1,4 +1,5 @@
 exports.create = require('./create')
+exports.del = require('./del')
 exports.getMany = require('./getMany')
 exports.getOne = require('./getOne')
 exports.getRelationHasMany = require('./getRelationHasMany')
