59a0e6e754dacc8f01616477b562ef703fa5b8b2
antonoberg <oberg.anton@gmail.com>
2018-05-13 21:05:22 +0200

    [UI] Adapt curatedList to jClient and new object format

--- packages/ui/src/domainModules/curatedList/globalSelectorFactories.js
+++ packages/ui/src/domainModules/curatedList/globalSelectorFactories.js
@@ -1,28 +1,28 @@
 import { createSelector } from 'reselect'
 
-import curatedListServiceSelectors from 'dataModules/curatedListService/globalSelectors'
+import globalCrudSelectors from 'coreModules/crud/globalSelectors'
 import globalSelectors from './globalSelectors'
 
-const { getFeatureTypes } = curatedListServiceSelectors
+const {
+  featureType: { getItemsObject: getFeatureTypesObject },
+} = globalCrudSelectors
 const { getGroupedFeatureTypeIds } = globalSelectors
 
 export const makeGetFeatureTypesInGroups = () => {
   return createSelector(
     [
-      getFeatureTypes,
+      getFeatureTypesObject,
       getGroupedFeatureTypeIds,
       (_, groups) => (groups ? groups.join() : ''),
     ],
     (featureTypes, groupToIdsMap, groupsString) => {
       return groupsString.split(',').reduce((arr, group) => {
         const featureTypeIds = groupToIdsMap[group]
-
         const groupFeatureTypes =
           featureTypeIds &&
           featureTypeIds.map(id => {
             return featureTypes[id]
           })
-
         return groupFeatureTypes ? [...arr, ...groupFeatureTypes] : arr
       }, [])
     }
