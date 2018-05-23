5c328ea33355e6787d159c8d2cbb8aaef29a84d3
Fredrik Olovsson <fogithub@gmail.com>
2018-05-23 22:47:14 +0200

    [UI] Add user to agent crud manager

--- packages/ui/src/domainModules/agent/components/item/Inspect/index.js
+++ packages/ui/src/domainModules/agent/components/item/Inspect/index.js
@@ -2,26 +2,21 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { Table } from 'semantic-ui-react'
 import { compose } from 'redux'
-import { connect } from 'react-redux'
 import { CustomData } from 'coreModules/form/components'
 
 import { createGetItemById } from 'coreModules/crud/higherOrderComponents'
 import { SET_ITEM_INSPECT } from 'coreModules/crudBlocks/constants'
 import RolesTable from '../../shared/RolesTable'
 
-const mapStateToProps = (state, { item: agent }) => {
-  return {
-    agent,
-  }
-}
-
 const propTypes = {
   agent: PropTypes.object,
   onInteraction: PropTypes.func.isRequired,
+  user: PropTypes.object,
 }
 
 const defaultProps = {
   agent: undefined,
+  user: undefined,
 }
 
 export class Inspect extends PureComponent {
@@ -38,12 +33,14 @@ export class Inspect extends PureComponent {
   }
 
   render() {
-    const { agent } = this.props
+    const { agent, user } = this.props
     if (!agent) {
       return null
     }
 
-    const { attributes } = agent
+    const { attributes: agentAttributes } = agent
+    const { attributes: userAttributes } = user || {}
+
     return (
       <React.Fragment>
         <Table celled>
@@ -53,11 +50,11 @@ export class Inspect extends PureComponent {
               <Table.HeaderCell>Value</Table.HeaderCell>
             </Table.Row>
           </Table.Header>
-          {attributes && (
+          {agentAttributes && (
             <Table.Body>
               <Table.Row>
                 <Table.Cell>Agent type</Table.Cell>
-                <Table.Cell>{attributes.agentType}</Table.Cell>
+                <Table.Cell>{agentAttributes.agentType}</Table.Cell>
               </Table.Row>
               <Table.Row>
                 <Table.Cell>id</Table.Cell>
@@ -65,63 +62,84 @@ export class Inspect extends PureComponent {
               </Table.Row>
               <Table.Row>
                 <Table.Cell>Full name</Table.Cell>
-                <Table.Cell>{attributes.fullName}</Table.Cell>
+                <Table.Cell>{agentAttributes.fullName}</Table.Cell>
               </Table.Row>
               <Table.Row>
                 <Table.Cell>Given name</Table.Cell>
-                <Table.Cell>{attributes.givenName}</Table.Cell>
+                <Table.Cell>{agentAttributes.givenName}</Table.Cell>
               </Table.Row>
               <Table.Row>
                 <Table.Cell>Family name</Table.Cell>
-                <Table.Cell>{attributes.familyName}</Table.Cell>
+                <Table.Cell>{agentAttributes.familyName}</Table.Cell>
               </Table.Row>
               <Table.Row>
                 <Table.Cell>Additional name</Table.Cell>
-                <Table.Cell>{attributes.additionalName}</Table.Cell>
+                <Table.Cell>{agentAttributes.additionalName}</Table.Cell>
               </Table.Row>
               <Table.Row>
                 <Table.Cell>Abbreviation</Table.Cell>
-                <Table.Cell>{attributes.abbreviation}</Table.Cell>
+                <Table.Cell>{agentAttributes.abbreviation}</Table.Cell>
               </Table.Row>
               <Table.Row>
                 <Table.Cell>Also known as</Table.Cell>
-                <Table.Cell>{attributes.alsoKnownAs}</Table.Cell>
+                <Table.Cell>{agentAttributes.alsoKnownAs}</Table.Cell>
               </Table.Row>
               <Table.Row>
                 <Table.Cell>Disambiguating description</Table.Cell>
-                <Table.Cell>{attributes.disambiguatingDescription}</Table.Cell>
+                <Table.Cell>
+                  {agentAttributes.disambiguatingDescription}
+                </Table.Cell>
               </Table.Row>
               <Table.Row>
                 <Table.Cell>Remarks</Table.Cell>
-                <Table.Cell>{attributes.remarks}</Table.Cell>
+                <Table.Cell>{agentAttributes.remarks}</Table.Cell>
               </Table.Row>
               <Table.Row>
                 <Table.Cell>Postal address</Table.Cell>
-                <Table.Cell>{attributes.postalAddress}</Table.Cell>
+                <Table.Cell>{agentAttributes.postalAddress}</Table.Cell>
               </Table.Row>
               <Table.Row>
                 <Table.Cell>Telephone</Table.Cell>
-                <Table.Cell>{attributes.telephone}</Table.Cell>
+                <Table.Cell>{agentAttributes.telephone}</Table.Cell>
               </Table.Row>
               <Table.Row>
                 <Table.Cell>Email</Table.Cell>
-                <Table.Cell>{attributes.email}</Table.Cell>
+                <Table.Cell>{agentAttributes.email}</Table.Cell>
               </Table.Row>
             </Table.Body>
           )}
         </Table>
 
-        <RolesTable roles={attributes.roles} />
-
-        <h2>Read only</h2>
         <CustomData
           autoComplete="off"
-          input={{ name: 'readOnly', value: attributes.readOnly }}
+          displayLabel={false}
+          input={{ name: 'readOnly', value: agentAttributes.readOnly }}
           meta={{}}
+          model="agent"
           module="agent"
           name="readOnly"
           type="read-only"
         />
+
+        <h2>User</h2>
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Username</Table.HeaderCell>
+              <Table.HeaderCell>Email</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>
+                {userAttributes && userAttributes.username}
+              </Table.Cell>
+              <Table.Cell>{userAttributes && userAttributes.email}</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+
+        <RolesTable roles={agentAttributes.roles} />
       </React.Fragment>
     )
   }
@@ -132,8 +150,14 @@ Inspect.defaultProps = defaultProps
 
 export default compose(
   createGetItemById({
-    relationships: [],
+    itemKey: 'agent',
+    relationships: ['user'],
     resource: 'agent',
   }),
-  connect(mapStateToProps)
+  createGetItemById({
+    idPath: 'item.relationships.user.data.id',
+    itemKey: 'user',
+    relationships: [],
+    resource: 'user',
+  })
 )(Inspect)
