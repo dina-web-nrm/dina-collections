73a74ba2682c2ff03f58d0250e47dff98da045ee
antonoberg <oberg.anton@gmail.com>
2018-06-03 17:07:36 +0200

    [UI] Use new search for LookupMammals

---
+++ packages/ui/src/domainModules/collectionMammals/components/LookupMammals/Filter.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Input } from 'semantic-ui-react'
+import { createSearch } from 'coreModules/search/higherOrderComponents'
+
+const propTypes = {
+  search: PropTypes.func.isRequired,
+}
+
+class Filter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+    this.handleFilterChange = this.handleFilterChange.bind(this)
+  }
+
+  handleFilterChange({ filterFunctionName, value }) {
+    const newState = {
+      ...this.state,
+      [filterFunctionName]: value,
+    }
+    this.setState(newState)
+
+    const query = {
+      and: Object.keys(newState)
+        .map(key => {
+          if (!newState[key]) {
+            return null
+          }
+          return {
+            filter: {
+              filterFunction: key,
+              input: {
+                value: newState[key],
+              },
+            },
+          }
+        })
+        .filter(item => {
+          return !!item
+        }),
+    }
+
+    this.props.search(query)
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Filter</h2>
+        <Input
+          onChange={event => {
+            this.handleFilterChange({
+              filterFunctionName: 'id',
+              value: event.target.value,
+            })
+          }}
+          placeholder="id"
+        />
+        <Input
+          onChange={event => {
+            this.handleFilterChange({
+              filterFunctionName: 'collectingLocation',
+              value: event.target.value,
+            })
+          }}
+          placeholder="collectingLocation"
+        />
+        <Input
+          onChange={event => {
+            this.handleFilterChange({
+              filterFunctionName: 'catalogNumber',
+              value: event.target.value,
+            })
+          }}
+          placeholder="catalogNumber"
+        />
+      </div>
+    )
+  }
+}
+
+Filter.propTypes = propTypes
+
+export default createSearch({})(Filter)
---
+++ packages/ui/src/domainModules/collectionMammals/components/LookupMammals/Result.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { compose } from 'redux'
+import PropTypes from 'prop-types'
+import * as actionCreators from 'coreModules/search/actionCreators'
+import { globalSelectors } from 'coreModules/search/keyObjectModule'
+import ReactList from 'react-list'
+import { createBatchFetchItems } from 'coreModules/crud/higherOrderComponents'
+import { createInjectSearchResult } from 'coreModules/search/higherOrderComponents'
+import { push } from 'react-router-redux'
+import Row from './Row'
+
+const mapStateToProps = state => {
+  return {
+    searchResult: globalSelectors.get.searchState(state),
+  }
+}
+
+const mapDispatchToProps = {
+  push,
+  search: actionCreators.search,
+  syncSearch: actionCreators.syncSearch,
+}
+
+const propTypes = {
+  fetchItemById: PropTypes.func.isRequired,
+  push: PropTypes.func.isRequired,
+  searchResult: PropTypes.object.isRequired,
+}
+
+const itemsRenderer = (items, ref) => {
+  return (
+    <div className="ui divided list" ref={ref}>
+      {items}
+    </div>
+  )
+}
+
+class Result extends Component {
+  constructor(props) {
+    super(props)
+    this.renderItem = this.renderItem.bind(this)
+    this.handleRowClick = this.handleRowClick.bind(this)
+  }
+  handleRowClick(specimenId) {
+    this.props.push(`/app/mammals/${specimenId}/edit`)
+  }
+  renderItem(index) {
+    const itemId = this.props.searchResult.items[index]
+    this.props.fetchItemById(itemId)
+    return <Row itemId={itemId} key={itemId} onClick={this.handleRowClick} />
+  }
+
+  render() {
+    if (!(this.props.searchResult && this.props.searchResult.items)) {
+      return <div>Loading</div>
+    }
+    return (
+      <React.Fragment>
+        <h2>Result</h2>
+        <div style={{ height: '100vh', overflow: 'auto' }}>
+          <ReactList
+            itemRenderer={this.renderItem}
+            itemsRenderer={itemsRenderer}
+            length={this.props.searchResult.items.length}
+            type="uniform"
+          />
+        </div>
+      </React.Fragment>
+    )
+  }
+}
+
+Result.propTypes = propTypes
+
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  createInjectSearchResult({
+    resource: 'searchSpecimen',
+  }),
+  createBatchFetchItems({
+    include: [
+      'agents',
+      'featureTypes',
+      'physicalObjects',
+      'places',
+      'taxonNames',
+    ],
+    relationships: ['all'],
+    resource: 'specimen',
+  })
+)(Result)
