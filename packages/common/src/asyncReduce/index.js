c7d2867d08db64a21bc3fdf9f1a1355cd9b7d675
antonoberg <oberg.anton@gmail.com>
2018-06-06 10:45:23 +0200

    [COMMON] Add asyncReduce

---
+++ packages/common/src/asyncReduce/index.js
@@ -0,0 +1,37 @@
+module.exports = function asyncReduce({
+  items = [],
+  initialValue,
+  reduceFunction,
+}) {
+  let value = initialValue
+  const nItems = items.length
+  if (nItems === 0) {
+    return Promise.resolve(value)
+  }
+  let index = 0
+
+  const internalReduce = () => {
+    if (index === nItems) {
+      return null
+    }
+    const currentItem = items[index]
+
+    return Promise.resolve()
+      .then(() => {
+        return reduceFunction({
+          index,
+          item: currentItem,
+          value,
+        })
+      })
+      .then(updatedValue => {
+        value = updatedValue
+        index += 1
+        return internalReduce()
+      })
+  }
+
+  return internalReduce().then(() => {
+    return value
+  })
+}
