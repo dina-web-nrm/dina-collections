0ab49a6b38c6b8d02a90b9a5236a7a00eb8a1c5d
antonoberg <oberg.anton@gmail.com>
2018-06-08 12:40:06 +0200

    [BACKEND] Add missing operation schemas

--- packages/backend/src/lib/operations/viewOperations/schemas/index.js
+++ packages/backend/src/lib/operations/viewOperations/schemas/index.js
@@ -1,6 +1,25 @@
 const base = require('./base')
 
 exports.rebuildView = base
+exports.rebuildView = {
+  additionalProperties: false,
+  properties: {
+    ...base.properties,
+
+    mapFunction: {
+      not: {
+        type: 'string',
+      },
+    },
+    srcResource: {
+      type: 'string',
+    },
+    warmViews: {
+      type: 'array',
+    },
+  },
+  required: [...base.required],
+}
 exports.requestRebuildView = base
 exports.requestUpdateView = base
 exports.updateView = base
