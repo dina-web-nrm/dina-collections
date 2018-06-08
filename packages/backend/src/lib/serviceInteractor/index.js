ee5cbd76590d02370bf6f9c9dc3fceccb4833448
antonoberg <oberg.anton@gmail.com>
2018-06-08 12:35:44 +0200

    [BACKEND] Improve service interactor

--- packages/backend/src/lib/serviceInteractor/index.js
+++ packages/backend/src/lib/serviceInteractor/index.js
@@ -1,5 +1,5 @@
 const createLog = require('../../utilities/log')
-const buildOperationId = require('common/src/buildOperationId')
+const callController = require('./callController')
 
 const log = createLog('lib/serviceInteractor')
 
@@ -10,8 +10,10 @@ module.exports = function createServiceInteractor() {
     'del',
     'getMany',
     'getOne',
+    'getOneSync',
     'getVersion',
     'getVersions',
+    'rebuildView',
     'update',
   ]
 
@@ -27,45 +29,23 @@ module.exports = function createServiceInteractor() {
 
       return {
         ...methods,
-        [operationType]: ({ resource, request = {} }) => {
-          return Promise.resolve().then(() => {
-            if (!resource) {
-              throw new Error('Resource is required')
-            }
-            if (!connectors) {
-              throw new Error('Connectors not added to serviceInteractor')
-            }
-
-            log.debug(
-              `Calling resource: ${resource} and operationType: ${
-                operationType
-              }`
-            )
-            const operationId = buildOperationId({
+        [operationType]: ({ resource, request = {}, sync = false }) => {
+          if (sync) {
+            return callController({
+              connectors,
+              log,
               operationType,
+              request,
               resource,
             })
-
-            const connector = connectors[operationId]
-            if (!connector) {
-              throw new Error(
-                `No connector found for resource: ${
-                  resource
-                } and operationType: ${operationType}`
-              )
-            }
-
-            const { controller } = connector
-            if (!controller) {
-              throw new Error(
-                `No controller found for resource: ${
-                  resource
-                } and operationType: ${operationType}`
-              )
-            }
-
-            return controller({
+          }
+          return Promise.resolve().then(() => {
+            return callController({
+              connectors,
+              log,
+              operationType,
               request,
+              resource,
             })
           })
         },
