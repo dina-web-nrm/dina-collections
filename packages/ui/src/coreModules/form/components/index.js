98db184e3614f4093652f2028f29cb47be31cb88
Fredrik Olovsson <fogithub@gmail.com>
2018-06-17 22:54:24 +0200

    [UI] Add MultipleSearchSelectionDropdown

--- packages/ui/src/coreModules/form/components/index.js
+++ packages/ui/src/coreModules/form/components/index.js
@@ -11,6 +11,9 @@ export { default as HelpTextNotification } from './migrate/HelpTextNotification'
 export { default as Input } from './fields/Input'
 export { default as InputDatePart } from './migrate/InputDatePart'
 export { default as InputText } from './inputs/Input/Text'
+export {
+  default as MultipleSearchSelectionDropdown,
+} from './fields/MultipleSearchSelectionDropdown'
 export { default as Search } from './fields/Search'
 export { default as SingleDate } from './fields/Date/SingleDate'
 export { default as TextArea } from './fields/TextArea'
