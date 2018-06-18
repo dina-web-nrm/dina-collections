e5d2dffcf3e5c46f968a264790d654b6a4af2017
antonoberg <oberg.anton@gmail.com>
2018-06-18 14:05:11 +0200

    fixup Improve mapping format

--- packages/backend/src/services/searchService/resources/searchSpecimen/data/mappings/index.js
+++ packages/backend/src/services/searchService/resources/searchSpecimen/data/mappings/index.js
@@ -1,39 +1,3 @@
 const mappingSpecificationMap = require('./mappingSpecifications')
 
-// const mappingSpecification = {
-//   elasticsearchMapping: {
-//     searchSpecimen: {
-//       properties: {
-//         collectingLocations: {
-//           fields: {
-//             raw: {
-//               ignore_above: 256,
-//               type: 'keyword',
-//             },
-//           },
-//           type: 'text',
-//         },
-//         id: {
-//           fields: {
-//             keyword: {
-//               ignore_above: 256,
-//               type: 'keyword',
-//             },
-//           },
-//           type: 'text',
-//         },
-//         identifiers: {
-//           fields: {
-//             raw: {
-//               ignore_above: 256,
-//               type: 'keyword',
-//             },
-//           },
-//           type: 'text',
-//         },
-//       },
-//     },
-//   },
-// }
-
 module.exports = mappingSpecificationMap
