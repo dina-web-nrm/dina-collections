080d4a113fedba78e091d71c24422b3dbcce0f6c
antonoberg <oberg.anton@gmail.com>
2018-06-19 11:02:01 +0200

    [BACKEND] Add elastic filter for id

--- packages/backend/src/lib/data/filters/sharedFilters/id.js
+++ packages/backend/src/lib/data/filters/sharedFilters/id.js
@@ -2,6 +2,13 @@ const idFilterFunction = require('common/src/search/resources/specimen/filterFun
 
 module.exports = {
   description: 'Filter by id',
+  elasticsearch: ({ value }) => {
+    return {
+      term: {
+        id: value,
+      },
+    }
+  },
   inputSchema: {
     type: 'string',
   },
