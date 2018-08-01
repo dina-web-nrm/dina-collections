9cbdefdef765fb6ed705c497c83de88adb686064
idali0226 <ida.li@nrm.se>
2018-08-01 16:27:07 +0200

    [UI] Use RowLayout to display InfiniteTableHeader and InfiniteTable

--- packages/ui/src/domainModules/collectionMammals/components/MammalManager/MainColumn/ResultTableView/index.js
+++ packages/ui/src/domainModules/collectionMammals/components/MammalManager/MainColumn/ResultTableView/index.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { compose } from 'redux'
+
 import { createInjectSearch } from 'coreModules/search/higherOrderComponents'
+
+import { RowLayout } from 'coreModules/layout/components'
+import { injectWindowHeight } from 'coreModules/size/higherOrderComponents'
 import InfiniteTable from './InfiniteTable'
+import InfiniteTableHeader from './InfiniteTableHeader'
+
+const infiniteTable = {
+  key: 'infiniteTable',
+  renderRow: props => <InfiniteTable {...props} />,
+}
+
+const infiniteTableHeader = {
+  height: '50px',
+  key: 'infiniteTableHeader',
+  renderRow: props => <InfiniteTableHeader {...props} />,
+}
+
+const rows = [infiniteTableHeader, infiniteTable]
+
+const propTypes = {
+  availableHeight: PropTypes.string.isRequired,
+  mainColumnActiveTab: PropTypes.string.isRequired,
+  windowHeight: PropTypes.number.isRequired,
+}
 
 class ResultTableView extends Component {
   render() {
-    return <InfiniteTable />
+    const {
+      availableHeight,
+      mainColumnActiveTab,
+      windowHeight,
+      ...rest
+    } = this.props
+
+    return (
+      <RowLayout
+        availableHeight={availableHeight}
+        mainColumnActiveTab={mainColumnActiveTab}
+        rows={rows}
+        {...rest}
+      />
+    )
   }
 }
 
-export default createInjectSearch({})(ResultTableView)
+ResultTableView.propTypes = propTypes
+
+export default compose(createInjectSearch({}), injectWindowHeight)(
+  ResultTableView
+)
