0656ae1a9fc46a661b43fff94d294c0ea200672c
Fredrik Olovsson <fogithub@gmail.com>
2018-08-02 15:26:08 +0200

    [UI] Add Header and BottomBar in FilterColumn

---
+++ packages/ui/src/domainModules/collectionMammals/components/MammalManager/FilterColumn/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Grid, Header } from 'semantic-ui-react'
+
+const FiltersHeader = () => {
+  return (
+    <Grid padded>
+      <Grid.Column>
+        <Header>Find records</Header>
+      </Grid.Column>
+    </Grid>
+  )
+}
+
+export default FiltersHeader
--- packages/ui/src/domainModules/collectionMammals/components/MammalManager/FilterColumn/index.js
+++ packages/ui/src/domainModules/collectionMammals/components/MammalManager/FilterColumn/index.js
@@ -1,23 +1,36 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { compose } from 'redux'
+import { formValueSelector as formValueSelectorFactory } from 'redux-form'
 
 import { RowLayout } from 'coreModules/layout/components'
 import { injectWindowHeight } from 'coreModules/size/higherOrderComponents'
+import { SPECIMEN_FILTERS_FORM_NAME } from '../../../constants'
+import BottomBar from './BottomBar'
 import Filters from './Filters'
+import Header from './Header'
+
+const formValueSelector = formValueSelectorFactory(SPECIMEN_FILTERS_FORM_NAME)
 
 const main = {
   key: 'main',
   renderRow: props => <Filters {...props} />,
+  style: { overflow: 'auto' },
 }
 
 const header = {
   height: '50px',
   key: 'header',
-  renderRow: () => <h2>Find records</h2>,
+  renderRow: props => <Header {...props} />,
 }
 
-const rows = [header, main]
+const bottomBar = {
+  height: '60px',
+  key: 'bottomBar',
+  renderRow: props => <BottomBar {...props} />,
+}
+
+const rows = [header, main, bottomBar]
 
 const propTypes = {
   windowHeight: PropTypes.number.isRequired,
@@ -27,7 +40,15 @@ class FilterColumn extends PureComponent {
   render() {
     const { windowHeight } = this.props
 
-    return <RowLayout availableHeight={windowHeight - 40} rows={rows} />
+    return (
+      <RowLayout
+        availableHeight={windowHeight - 40}
+        formName={SPECIMEN_FILTERS_FORM_NAME}
+        formValueSelector={formValueSelector}
+        rows={rows}
+        {...this.props}
+      />
+    )
   }
 }
 
