9c5490c580bcc3055d3123c18c71a8067991bae4
Fredrik Olovsson <fogithub@gmail.com>
2018-08-02 15:19:03 +0200

    [UI] Add checkboxes & search tags field components

---
+++ packages/ui/src/coreModules/search/components/MultipleSearchTagsSelect/Field.js
@@ -0,0 +1,4 @@
+import wrapInFieldTemplate from 'coreModules/form/higherOrderComponents/wrapInFieldTemplate'
+import MultipleSearchTagsSelect from './index'
+
+export default wrapInFieldTemplate(MultipleSearchTagsSelect)
--- packages/ui/src/coreModules/search/components/index.js
+++ packages/ui/src/coreModules/search/components/index.js
@@ -1,2 +1,8 @@
 export { default as MultipleChoiceCheckboxes } from './MultipleChoiceCheckboxes'
+export {
+  default as MultipleChoiceCheckboxesField,
+} from './MultipleChoiceCheckboxes/Field'
 export { default as MultipleSearchTagsSelect } from './MultipleSearchTagsSelect'
+export {
+  default as MultipleSearchTagsSelectField,
+} from './MultipleSearchTagsSelect/Field'
