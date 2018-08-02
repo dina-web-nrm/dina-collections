0e7e7813a419b96a24fb30820ac0239ad6c673aa
Fredrik Olovsson <fogithub@gmail.com>
2018-08-02 15:44:03 +0200

    [UI] Fix scroll behavior & more in ResultTableView
    1. Fix scrollbars
    2. Make table header fixed vertically but scrolling horizontally with
    rest of table
    3. Remove padding around table and instead use padded grid
    4. Add border between header and table rows
    5. Add styling for focused table row
    6. Change stateless components to functions instead of classes

--- packages/ui/src/domainModules/collectionMammals/components/MammalManager/MainColumn/ResultTableView/InfiniteTableRow.js
+++ packages/ui/src/domainModules/collectionMammals/components/MammalManager/MainColumn/ResultTableView/InfiniteTableRow.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { compose } from 'redux'
@@ -14,66 +14,67 @@ const mapStateToProps = (state, { itemId }) => {
 }
 
 const propTypes = {
+  background: PropTypes.string.isRequired,
   item: PropTypes.object,
   onClick: PropTypes.func.isRequired,
-  style: PropTypes.string.isRequired,
+  width: PropTypes.number.isRequired,
 }
 
 const defaultProps = {
   item: undefined,
 }
 
-class InfiniteTableRow extends Component {
-  render() {
-    const { item, style } = this.props
-
-    if (!item) {
-      return <div style={{ height: 45, width: 1700 }}>Loading....</div>
-    }
-
+const InfiniteTableRow = ({ background, item, onClick, width }) => {
+  if (!item) {
     return (
-      <Grid.Row
-        onClick={event => {
-          event.preventDefault()
-          this.props.onClick(item.id)
-        }}
-        style={{ background: style, height: 45, width: 1700 }}
-      >
-        <Grid.Column style={{ width: 150 }}>
-          {item.attributes.normalized.identifiers &&
-            item.attributes.normalized.identifiers[0].value}
-        </Grid.Column>
-        <Grid.Column style={{ width: 200 }}>
-          {item.attributes.readOnly.objects.Scientific_Name}
-        </Grid.Column>
-        <Grid.Column style={{ width: 200 }}>
-          {item.attributes.readOnly.objects.Family}
-        </Grid.Column>
-        <Grid.Column style={{ width: 200 }}>
-          {item.attributes.readOnly.objects.Genus}
-        </Grid.Column>
-        <Grid.Column style={{ width: 200 }}>
-          {item.attributes.readOnly.objects.Species}
-        </Grid.Column>
-        <Grid.Column style={{ width: 100 }}>
-          {item.attributes.normalized.events[0].dateRange.startDate.dateText}
-        </Grid.Column>
-        <Grid.Column style={{ width: 300 }}>
-          {item.attributes.normalized.events[0].locationInformation.localityT}
-        </Grid.Column>
-        <Grid.Column style={{ width: 100 }}>
-          {item.attributes.readOnly.objects.WayOfDeath &&
-            item.attributes.readOnly.objects.WayOfDeath_related.DödsorsakEN}
-        </Grid.Column>
-        <Grid.Column style={{ width: 100 }}>
-          {item.attributes.readOnly.analysis.Sex}
-        </Grid.Column>
-        <Grid.Column style={{ width: 100 }}>
-          {item.attributes.readOnly.analysis.AgeStage}
-        </Grid.Column>
+      <Grid.Row style={{ height: 43, width }}>
+        <Grid.Column>Loading...</Grid.Column>
       </Grid.Row>
     )
   }
+
+  return (
+    <Grid.Row
+      onClick={event => {
+        event.preventDefault()
+        onClick(item.id)
+      }}
+      style={{ background, height: 43, width }}
+    >
+      <Grid.Column style={{ width: 150 }}>
+        {item.attributes.normalized.identifiers &&
+          item.attributes.normalized.identifiers[0].value}
+      </Grid.Column>
+      <Grid.Column style={{ width: 200 }}>
+        {item.attributes.readOnly.objects.Scientific_Name}
+      </Grid.Column>
+      <Grid.Column style={{ width: 200 }}>
+        {item.attributes.readOnly.objects.Family}
+      </Grid.Column>
+      <Grid.Column style={{ width: 200 }}>
+        {item.attributes.readOnly.objects.Genus}
+      </Grid.Column>
+      <Grid.Column style={{ width: 200 }}>
+        {item.attributes.readOnly.objects.Species}
+      </Grid.Column>
+      <Grid.Column style={{ width: 100 }}>
+        {item.attributes.normalized.events[0].dateRange.startDate.dateText}
+      </Grid.Column>
+      <Grid.Column style={{ width: 300 }}>
+        {item.attributes.normalized.events[0].locationInformation.localityT}
+      </Grid.Column>
+      <Grid.Column style={{ width: 100 }}>
+        {item.attributes.readOnly.objects.WayOfDeath &&
+          item.attributes.readOnly.objects.WayOfDeath_related.DödsorsakEN}
+      </Grid.Column>
+      <Grid.Column style={{ width: 100 }}>
+        {item.attributes.readOnly.analysis.Sex}
+      </Grid.Column>
+      <Grid.Column style={{ width: 100 }}>
+        {item.attributes.readOnly.analysis.AgeStage}
+      </Grid.Column>
+    </Grid.Row>
+  )
 }
 
 InfiniteTableRow.propTypes = propTypes
