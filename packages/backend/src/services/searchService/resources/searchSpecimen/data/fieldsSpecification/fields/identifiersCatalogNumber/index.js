7f2bd79765dfc1b038965e348c558a98de7dd50a
antonoberg <oberg.anton@gmail.com>
2018-08-09 10:23:03 +0200

    [BACKEND] Add minor fields

---
+++ packages/backend/src/services/searchService/resources/searchSpecimen/data/fieldsSpecification/fields/identifiersCatalogNumber/index.js
@@ -0,0 +1,45 @@
+const {
+  createKeywordMapping,
+} = require('../../../../../../../../lib/data/mappings/factories')
+
+const fieldPath = 'attributes.identifiersCatalogNumber'
+
+const CATALOG_NUMBER_TYPE = 'catalog-number'
+const transformation = ({ migrator, src, target }) => {
+  const identifiers = migrator.getValue({
+    obj: src,
+    path: 'individual.identifiers',
+  })
+
+  if (!identifiers) {
+    return null
+  }
+
+  const catalogNumberIdentifier = identifiers.find(identifier => {
+    const { identifierType: { key } = {} } = identifier
+    if (key === CATALOG_NUMBER_TYPE) {
+      return true
+    }
+    return false
+  })
+
+  if (catalogNumberIdentifier) {
+    migrator.setValue({
+      obj: target,
+      path: fieldPath,
+      value: catalogNumberIdentifier.value,
+    })
+  }
+  return null
+}
+
+module.exports = {
+  fieldPath,
+  key: 'identifiersCatalogNumber',
+  mapping: createKeywordMapping({
+    fieldPath,
+  }),
+  selectable: true,
+  sortable: true,
+  transformation,
+}
--- packages/backend/src/services/searchService/resources/searchSpecimen/data/fieldsSpecification/index.js
+++ packages/backend/src/services/searchService/resources/searchSpecimen/data/fieldsSpecification/index.js
@@ -1,3 +1,13 @@
 const agents = require('./fields/agents')
+const identifiersCatalogNumber = require('./fields/identifiersCatalogNumber')
+const collectingEventCollectors = require('./fields/collectingEventCollectors')
+const collectingEventStartDate = require('./fields/collectingEventStartDate')
 
-module.exports = { fields: [agents] }
+module.exports = {
+  fields: [
+    agents,
+    identifiersCatalogNumber,
+    collectingEventCollectors,
+    collectingEventStartDate,
+  ],
+}
