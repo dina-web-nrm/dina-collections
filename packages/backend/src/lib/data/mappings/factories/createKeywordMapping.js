c43764a6a9928a521645b9ab8badb5a5d3b22ddb
antonoberg <oberg.anton@gmail.com>
2018-08-09 10:21:37 +0200

    [BACKEND] Fix keyword mapping

--- packages/backend/src/lib/data/mappings/factories/createKeywordMapping.js
+++ packages/backend/src/lib/data/mappings/factories/createKeywordMapping.js
@@ -2,13 +2,8 @@ module.exports = function createKeywordMapping({ fieldPath }) {
   return {
     elasticsearch: () => {
       return {
-        fields: {
-          raw: {
-            ignore_above: 256,
-            type: 'keyword',
-          },
-        },
-        type: 'text',
+        ignore_above: 256,
+        type: 'keyword',
       }
     },
     fieldPath,
