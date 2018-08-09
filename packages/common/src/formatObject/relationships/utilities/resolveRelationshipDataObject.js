63e1f7a221e66d569cd3f5d8ca46f3886ad2207f
antonoberg <oberg.anton@gmail.com>
2018-08-09 16:37:04 +0200

    [COMMON] Fix bug in formatObject

--- packages/common/src/formatObject/relationships/utilities/resolveRelationshipDataObject.js
+++ packages/common/src/formatObject/relationships/utilities/resolveRelationshipDataObject.js
@@ -1,3 +1,4 @@
+const objectPath = require('object-path')
 const { resolveById } = require('./resolveById')
 
 const resolveRelationshipDataObject = ({
@@ -6,13 +7,14 @@ const resolveRelationshipDataObject = ({
   relationship,
   relationshipKey,
 }) => {
-  return {
-    ...item,
-    [relationshipKey]: resolveById({
-      formattedRelationshipItems,
-      id: relationship.data.id,
-    }),
-  }
+  const resolvedRelation = resolveById({
+    formattedRelationshipItems,
+    id: relationship.data.id,
+  })
+
+  objectPath.set(item, relationshipKey, resolvedRelation)
+
+  return item
 }
 
 module.exports = { resolveRelationshipDataObject }
