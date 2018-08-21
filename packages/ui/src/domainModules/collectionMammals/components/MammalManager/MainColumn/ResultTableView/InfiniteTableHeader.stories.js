166d11042d7a20953f90640d743ac86fb29bd76e
idali0226 <ida.li@nrm.se>
2018-08-21 14:31:40 +0200

    [UI] Add storybook for InfiniteTableHeader

---
+++ packages/ui/src/domainModules/collectionMammals/components/MammalManager/MainColumn/ResultTableView/InfiniteTableHeader.stories.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import 'common/dist/semantic.css' // eslint-disable-line
+import createStoryDecorator from 'utilities/test/createStoryDecorator'
+import { action } from '@storybook/addon-actions' // eslint-disable-line
+import { storiesOf } from '@storybook/react' // eslint-disable-line
+
+import InfiniteTableHeader from './InfiniteTableHeader'
+// import tableColumnSpecifications from '../tableColumnSpecifications'
+
+// export const actions = {
+//   onExportCsv: action('onExportCsv'),
+//   onFormTabClick: action('onFormTabClick'),
+//   onSettingClick: action('onSettingClick'),
+//   onTableTabClick: action('onTableTabClick'),
+// }
+
+export const tableColumnsToShow = [
+  'catalogNumber',
+  'family',
+  'genus',
+  'species',
+  'collectors',
+  'endDateCollecting',
+  'localityCollecting',
+  'death',
+  'skeleton',
+  'skin',
+  'wet',
+  'sex',
+  'ageStage',
+  'registered',
+  'curatorialName',
+  'identifiersCatalogNumber',
+  'taxonomyCuratorialName',
+  'taxonomyFamily',
+  'taxonomyGenus',
+  'taxonomySpecies',
+  'taxonomySubspecies',
+]
+
+storiesOf(
+  'domainModules/collectionMammals/MammalManager/ResultTableView/InfiniteTableHeader',
+  module
+)
+  .addDecorator(createStoryDecorator())
+  .add('default', () => (
+    <InfiniteTableHeader
+      height={43}
+      tableColumnsToShow={tableColumnsToShow}
+      topOffset={141}
+      width={800}
+    />
+  ))
