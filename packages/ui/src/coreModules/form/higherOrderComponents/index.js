f2ab8435af9a0a57732288c2a39aeb2dc9aef97c
Fredrik Olovsson <fogithub@gmail.com>
2018-08-27 14:28:15 +0200

    [UI] Add other form HOCs in index

--- packages/ui/src/coreModules/form/higherOrderComponents/index.js
+++ packages/ui/src/coreModules/form/higherOrderComponents/index.js
@@ -1,7 +1,17 @@
+import injectErrorKeys from './injectErrorKeys'
+import injectHelpNotificationProps from './injectHelpNotificationProps'
 import injectIsLatestActiveField from './injectIsLatestActiveField'
+import injectLabelKey from './injectLabelKey'
+import injectParameterKey from './injectParameterKey'
 import pathBuilder from './pathBuilder'
+import wrapInFieldTemplate from './wrapInFieldTemplate'
 
 export {
+  injectErrorKeys,
+  injectHelpNotificationProps,
+  injectLabelKey,
+  injectParameterKey,
   injectIsLatestActiveField,
   pathBuilder,
+  wrapInFieldTemplate,
 }
