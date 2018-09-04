2c6d6636082007217b9c2eed3a16cb464e9984a0
antonoberg <oberg.anton@gmail.com>
2018-09-04 11:38:52 +0200

    [UI] Removed not needed eslint disable

--- packages/ui/src/coreModules/resourceManager/higherOrderComponents/createLocalState.js
+++ packages/ui/src/coreModules/resourceManager/higherOrderComponents/createLocalState.js
@@ -1,6 +1,4 @@
-/* eslint-disable class-methods-use-this */
 import React, { Component } from 'react'
-// import PropTypes from 'prop-types'
 
 const initialState = {
   collectionColumn: undefined,
