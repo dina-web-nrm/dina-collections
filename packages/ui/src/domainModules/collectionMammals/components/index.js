9155a73bf0f7e3dc1757de4f53cf205590347741
Fredrik Olovsson <fogithub@gmail.com>
2018-09-10 02:17:47 +0200

    [UI] Remove legacy MammalForm

--- packages/ui/src/domainModules/collectionMammals/components/index.js
+++ packages/ui/src/domainModules/collectionMammals/components/index.js
@@ -1,6 +1,3 @@
-import CreateSpecimen from './CreateSpecimen'
-import EditSpecimen from './EditSpecimen'
-import MammalForm from './MammalForm'
 import MammalManager from './MammalManager'
 
-export { CreateSpecimen, EditSpecimen, MammalForm, MammalManager }
+export { MammalManager }
