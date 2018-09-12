17d7a19b90e17dba9120f693db0c7a0990d47a10
idali0226 <ida.li@nrm.se>
2018-09-12 10:21:19 +0200

    [Backend] Changes made for build

--- packages/backend/src/lib/statistics/index.js
+++ packages/backend/src/lib/statistics/index.js
@@ -3,7 +3,7 @@ const createLog = require('../../utilities/log')
 const log = createLog('statistics')
 
 module.exports = function initializeStatistics({ config }) {
-  if (config.env.isDevelopment) {
+  if (false) {
     const memwatch = require('memwatch-next') // eslint-disable-line
     memwatch.on('stats', stats => {
       log.info('stats', stats)
