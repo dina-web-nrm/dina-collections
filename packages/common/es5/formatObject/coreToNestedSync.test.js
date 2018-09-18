30101480826df3a4e907395872a1dafc93b2701f
Fredrik Olovsson <fogithub@gmail.com>
2018-09-18 12:20:09 +0200

    [COMMON] Add build

--- packages/common/es5/formatObject/coreToNestedSync.test.js
+++ packages/common/es5/formatObject/coreToNestedSync.test.js
@@ -7,9 +7,6 @@ var _typeof3 = _interopRequireDefault(_typeof2);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var coreToNestedSync = require('./coreToNestedSync');
-var apiFormatPhysicalObject = require('./utilities/testData/apiFormatPhysicalObject');
-var apiFormatSpecimen = require('./utilities/testData/apiFormatSpecimen');
-var nestedPhysicalObjectWithRelationships = require('./utilities/testData/nestedPhysicalObjectWithRelationships');
 
 describe('formatObject/coreToNestedSync', function () {
   it('is a function', function () {
