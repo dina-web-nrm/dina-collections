8f689d454ad6437b8cf3b563f1e1cc2f864c496d
Fredrik Olovsson <fogithub@gmail.com>
2018-09-19 14:01:59 +0200

    [UI] Add createAgentInputs factory
    To simplify when writing unit specification

---
+++ packages/ui/src/domainModules/collectionMammals/components/RecordForm/Units/factories/createAgentInputs/index.js
@@ -0,0 +1,33 @@
+export default function createAgentInputs(
+  {
+    baseName,
+    buttonTextKey = 'other.addAgent',
+    initiallyHidden,
+    model,
+    module = 'collectionMammals',
+  } = {}
+) {
+  return [
+    {
+      columnProps: { width: 7 },
+      componentName: 'Input',
+      fluid: true,
+      initiallyHidden,
+      model,
+      name: `${baseName}.textV`,
+      type: 'text',
+    },
+    {
+      buttonTextKey,
+      columnProps: { width: 9 },
+      componentName: 'AgentDropdownPickerSearchTogglable',
+      displayEmptyStateLabel: false,
+      displayResultLabel: false,
+      initiallyHidden,
+      initiallyHiddenNames: [`${baseName}.textI`, `${baseName}.normalized`],
+      module,
+      name: `${baseName}`,
+      resultSuffix: '(agent)',
+    },
+  ]
+}
