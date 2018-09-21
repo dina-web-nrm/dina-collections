81cb2b08ceda2efbd3bab4e75c671c0d6cb3957f
Fredrik Olovsson <fredrikolovsson@users.noreply.github.com>
2018-09-21 10:39:04 +0200

    Revert "[UI] Add new form HOCs and utilities"
    This reverts commit e5df3d111e255469acfaa62d79ca9aee612792e4.

--- packages/ui/src/coreModules/form/higherOrderComponents/index.js
+++ packages/ui/src/coreModules/form/higherOrderComponents/index.js
@@ -1,25 +1,19 @@
 import injectErrorKeys from './injectErrorKeys'
-import injectFormPartStatus from './injectFormPartStatus'
 import injectHelpNotificationProps from './injectHelpNotificationProps'
 import injectIsLatestActiveField from './injectIsLatestActiveField'
 import injectLabelKey from './injectLabelKey'
 import injectParameterKey from './injectParameterKey'
 import injectSearchOptions from './injectSearchOptions'
 import pathBuilder from './pathBuilder'
-import reportFormFieldStatus from './reportFormFieldStatus'
-import wrapInColumn from './wrapInColumn'
 import wrapInFieldTemplate from './wrapInFieldTemplate'
 
 export {
   injectErrorKeys,
-  injectFormPartStatus,
   injectHelpNotificationProps,
   injectIsLatestActiveField,
   injectLabelKey,
   injectParameterKey,
   injectSearchOptions,
   pathBuilder,
-  reportFormFieldStatus,
-  wrapInColumn,
   wrapInFieldTemplate,
 }
