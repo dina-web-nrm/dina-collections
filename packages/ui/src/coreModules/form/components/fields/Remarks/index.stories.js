e8754d90e303c9df3c11bf9e6aac0d8ad88d28b3
Fredrik Olovsson <fredrikolovsson@users.noreply.github.com>
2018-09-21 10:39:04 +0200

    Revert "[UI] Add remarks result prefix"
    This reverts commit cb8be03fb5f64f3942defc805d9a0da162b81b77.

--- packages/ui/src/coreModules/form/components/fields/Remarks/index.stories.js
+++ packages/ui/src/coreModules/form/components/fields/Remarks/index.stories.js
@@ -1,74 +1,120 @@
-import React from 'react'
-import 'common/dist/semantic.css' // eslint-disable-line
-import PropTypes from 'prop-types'
-import createStoryDecorator from 'utilities/test/createStoryDecorator'
-import { action } from '@storybook/addon-actions' // eslint-disable-line
-import { storiesOf } from '@storybook/react' // eslint-disable-line
+// import React from 'react'
+// import 'common/dist/semantic.css' // eslint-disable-line
+// import PropTypes from 'prop-types'
+// import createStoryDecorator from 'utilities/test/createStoryDecorator'
+// import { action } from '@storybook/addon-actions' // eslint-disable-line
+// import { storiesOf } from '@storybook/react' // eslint-disable-line
 
-import { reduxForm } from 'redux-form'
-import { Field } from 'coreModules/form/components'
-import Remarks from './index'
+// import { reduxForm } from 'redux-form'
+// import { Field } from 'coreModules/form/components'
+// import Remarks from './index'
 
-const parameterKey = 'collectionItems.remarks'
-const fieldName = 'remarks'
-const someField = 'someField'
+// export const actions = {
+//   onRemarks: action('onClick', 'Add remarks'),
+// }
 
-const input = {
-  name: 'remarks',
-  value: 'Some added remarks',
-}
+// const parameterKey = 'collectionItems.remarks'
+// const fieldName = 'remarks'
+// const someField = 'someField'
 
-const Parent = ({ children }) => {
-  return <form>{children}</form>
-}
+// const input = {
+//   name: 'remarks',
+//   value: 'Some added remarks',
+// }
 
-Parent.propTypes = {
-  children: PropTypes.object.isRequired,
-}
+// const Parent = ({ children }) => {
+//   return <form>{children}</form>
+// }
 
-const FormWrapper = reduxForm({ form: 'remarksForm' })(Parent)
+// Parent.propTypes = {
+//   children: PropTypes.object.isRequired,
+// }
 
-storiesOf('coreModules/form/Fields/Remarks', module)
-  .addDecorator(createStoryDecorator({}))
-  .add('emptyState', () => (
-    <FormWrapper>
-      <Field
-        autoComplete="off"
-        component={Remarks}
-        isLatestActiveField
-        module="collectionMammals"
-        name={someField}
-        parameterKey={parameterKey}
-        setAsLatestActiveField={action('setAsLatestActiveField')}
-      />
-    </FormWrapper>
-  ))
-  .add('existingRemarkWithPrefixedText', () => (
-    <FormWrapper>
-      <Field
-        autoComplete="off"
-        component={Remarks}
-        input={input}
-        isLatestActiveField
-        module="collectionMammals"
-        name={fieldName}
-        parameterKey={parameterKey}
-        resultPrefix="Taxon"
-        setAsLatestActiveField={action('setAsLatestActiveField')}
-      />
-    </FormWrapper>
-  ))
-  .add('existingRemarkWithOutPrefixedText', () => (
-    <FormWrapper>
-      <Field
-        autoComplete="off"
-        component={Remarks}
-        input={input}
-        isLatestActiveField
-        module="collectionMammals"
-        name={fieldName}
-        parameterKey={parameterKey}
-        setAsLatestActiveField={action('setAsLatestActiveField')}
-      />
-    </FormWrapper>
-  ))
+// const FormWrapper = reduxForm({ form: 'remarksForm' })(Parent)
+
+// storiesOf('coreModules/form/Fields/Remarks', module)
+//   .addDecorator(createStoryDecorator({}))
+//   .add('default', () => (
+//     <FormWrapper>
+//       <Field
+//         autoComplete="off"
+//         component={Remarks}
+//         module="collectionMammals"
+//         name={someField}
+//         parameterKey={parameterKey}
+//       />
+//     </FormWrapper>
+//   ))
+//   .add('existingRemark', () => (
+//     <FormWrapper>
+//       <Field
+//         autoComplete="off"
+//         component={Remarks}
+//         input={input}
+//         module="collectionMammals"
+//         name={fieldName}
+//         parameterKey={parameterKey}
+//       />
+//     </FormWrapper>
+//   ))
+//   .add('noRemark', () => (
+//     <FormWrapper>
+//       <Field
+//         autoComplete="off"
+//         component={Remarks}
+//         module="collectionMammals"
+//         name={someField}
+//         parameterKey={parameterKey}
+//       />
+//     </FormWrapper>
+//   ))
+//   .add('addRemark', () => (
+//     <FormWrapper>
+//       <Field
+//         autoComplete="off"
+//         component={Remarks}
+//         module="collectionMammals"
+//         name={someField}
+//         parameterKey={parameterKey}
+//       />
+//     </FormWrapper>
+//   ))
+//   .add('editRemark', () => (
+//     <FormWrapper>
+//       <Field
+//         autoComplete="off"
+//         component={Remarks}
+//         input={input}
+//         module="collectionMammals"
+//         name={fieldName}
+//         parameterKey={parameterKey}
+//       />
+//     </FormWrapper>
+//   ))
+//   .add('editRemark without help symbol', () => (
+//     <FormWrapper>
+//       <Field
+//         autoComplete="off"
+//         component={Remarks}
+//         enableHelpNotifications={false}
+//         input={input}
+//         module="collectionMammals"
+//         name={fieldName}
+//         parameterKey={parameterKey}
+//       />
+//     </FormWrapper>
+//   ))
+//   .add('remarks custom label', () => (
+//     <FormWrapper>
+//       <Field
+//         autoComplete="off"
+//         component={Remarks}
+//         enableHelpNotifications={false}
+//         input={input}
+//         label="The label text"
+//         module="collectionMammals"
+//         name={fieldName}
+//         parameterKey={parameterKey}
+//       />
+//     </FormWrapper>
+//   ))
