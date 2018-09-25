20a1839d7600b6f8ecda15bf8d9466af8d89cee2
antonoberg <oberg.anton@gmail.com>
2018-09-25 11:27:54 +0200

    [COMMON] Build es5 and schema

--- packages/common/es5/error/constants/parameterErrorCodes.js
+++ packages/common/es5/error/constants/parameterErrorCodes.js
@@ -2,6 +2,8 @@
 
 module.exports = {
   additionalProperties: 'ADDITIONAL_PROPERTIES',
+  duplicate: 'DUPLICATE',
+  enum: 'ENUM',
   maxLength: 'MAX_LENGTH',
   minLength: 'MIN_LENGTH',
   required: 'REQUIRED',
