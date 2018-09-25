6b7cbaad0f383934e79ed85775c667dfd884bdbb
antonoberg <oberg.anton@gmail.com>
2018-09-25 11:28:59 +0200

    [COMMON] Rename jsonSchemaErrorCodes to par..errorCodes

--- packages/common/src/error/constants/parameterErrorCodes.js
+++ packages/common/src/error/constants/parameterErrorCodes.js
@@ -1,5 +1,7 @@
 module.exports = {
   additionalProperties: 'ADDITIONAL_PROPERTIES',
+  duplicate: 'DUPLICATE',
+  enum: 'ENUM',
   maxLength: 'MAX_LENGTH',
   minLength: 'MIN_LENGTH',
   required: 'REQUIRED',
