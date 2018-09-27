a2cfc5e0edf392554aceae252b7d201ea50347b8
Fredrik Olovsson <fogithub@gmail.com>
2018-09-27 14:25:06 +0200

    [UI] Add CatalogCardInformation parts

--- packages/ui/src/domainModules/collectionMammals/components/MammalManager/MainColumn/RecordForm/FormRow/formParts/index.js
+++ packages/ui/src/domainModules/collectionMammals/components/MammalManager/MainColumn/RecordForm/FormRow/formParts/index.js
@@ -1,9 +1,13 @@
 import agentParts from 'domainModules/agent/components/formParts'
+import CatalogCardInformation from './CatalogCardInformation'
+import CatalogCardInformationModal from './CatalogCardInformationModal'
 import IdentifiersTable from './IdentifiersTable'
 import RecordHistoryExternalEvents from './RecordHistoryExternalEvents'
 
 export default {
   ...agentParts,
+  CatalogCardInformation,
+  CatalogCardInformationModal,
   IdentifiersTable,
   RecordHistoryExternalEvents,
 }
