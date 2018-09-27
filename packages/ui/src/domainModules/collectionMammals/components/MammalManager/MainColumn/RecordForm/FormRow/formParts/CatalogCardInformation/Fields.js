4e8f99a6d248911664bc60d90296827ece37c637
Fredrik Olovsson <fogithub@gmail.com>
2018-09-27 14:25:06 +0200

    [UI] Add CatalogCardInformation parts

---
+++ packages/ui/src/domainModules/collectionMammals/components/MammalManager/MainColumn/RecordForm/FormRow/formParts/CatalogCardInformation/Fields.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Grid } from 'semantic-ui-react'
+
+import { Field } from 'coreModules/form/components'
+import { defaultValidate } from 'coreModules/form/components/fields/Date/SingleDate'
+import formParts from 'coreModules/form/components/parts'
+import agentParts from 'domainModules/agent/components/formParts'
+
+const { SingleDate } = formParts
+const { AgentDropdownPickerSearch } = agentParts
+
+const propTypes = {
+  baseName: PropTypes.string.isRequired,
+  setChildDirty: PropTypes.func.isRequired,
+  setChildInvalid: PropTypes.func.isRequired,
+}
+
+const Fields = ({ baseName, setChildDirty, setChildInvalid }) => {
+  return (
+    <React.Fragment>
+      <Grid.Column width={16}>
+        <Field
+          autoComplete="off"
+          component={AgentDropdownPickerSearch}
+          module="collectionMammals"
+          name={`${baseName}.agent`}
+          resultSuffix="(agent)"
+          setChildDirty={setChildDirty}
+          setChildInvalid={setChildInvalid}
+          type="input-text"
+        />
+      </Grid.Column>
+      <Grid.Column width={16}>
+        <Field
+          autoComplete="off"
+          component={SingleDate}
+          label="Condition"
+          module="collectionMammals"
+          name={`${baseName}.date`}
+          setChildDirty={setChildDirty}
+          setChildInvalid={setChildInvalid}
+          validate={defaultValidate}
+        />
+      </Grid.Column>
+    </React.Fragment>
+  )
+}
+
+Fields.propTypes = propTypes
+
+export default Fields
