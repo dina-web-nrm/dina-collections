896d4eb5bf4b8ed71baf87645c661b0261be0c1d
antonoberg <oberg.anton@gmail.com>
2018-10-16 16:05:31 +0200

    [UI] Fix text in LocalityDropdown

--- packages/ui/src/domainModules/locality/components/LocalityDropdownSearch/index.js
+++ packages/ui/src/domainModules/locality/components/LocalityDropdownSearch/index.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
 import { DropdownSearch } from 'coreModules/form/components'
 
+const extractText = nestedItem => {
+  const { attributes = {} } = nestedItem
+  const name = attributes && attributes.name
+  if (!name) {
+    return ''
+  }
+
+  return `${attributes.name} (${attributes.group})`
+}
+
 class LocalityDropdownSearch extends Component {
   render() {
     const { ...rest } = this.props
     return (
       <DropdownSearch
         {...rest}
+        extractText={extractText}
         resource="place"
         type="dropdown-search-resource"
       />
