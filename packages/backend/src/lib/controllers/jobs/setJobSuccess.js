0043e928c9e5bfc3bf1cf983dda9b67959e62093
antonoberg <oberg.anton@gmail.com>
2018-10-18 19:42:53 +0200

    [BACKEND] Clear operationRequest on job success

--- packages/backend/src/lib/controllers/jobs/setJobSuccess.js
+++ packages/backend/src/lib/controllers/jobs/setJobSuccess.js
@@ -11,6 +11,7 @@ module.exports = function setJobSuccess(options) {
       ...input,
       attributes: {
         ...(input.attributes || {}),
+        operationRequest: {},
         succeededAt: getCurrentUTCTimestamp(),
       },
     }
