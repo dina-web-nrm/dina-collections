e3f3f7727129c28981fec90838e5165307923644
antonoberg <oberg.anton@gmail.com>
2018-10-18 19:44:22 +0200

    [COMMON] Bail format timestamp if no dateValue

--- packages/common/src/date/formatAsTimestamp.js
+++ packages/common/src/date/formatAsTimestamp.js
@@ -1,5 +1,8 @@
 const moment = require('moment')
 
 module.exports = function formatAsTimestamp(dateValue) {
+  if (!dateValue) {
+    return dateValue
+  }
   return moment(dateValue).format(moment.defaultFormatUtc)
 }
