e5422e31abca26d66fc466c6491f8d993b0f3943
antonoberg <oberg.anton@gmail.com>
2018-10-18 16:44:25 +0200

    [BACKEND] Add taxon and taxonName filters

--- packages/backend/src/services/taxonomyService/resources/taxonName/data/filterSpecifications/index.js
+++ packages/backend/src/services/taxonomyService/resources/taxonName/data/filterSpecifications/index.js
@@ -1,3 +1,4 @@
+const createStringMatchFilter = require('../../../../../../lib/data/filters/factories/createStringMatchFilter')
 const createGetManyFilterSpecifications = require('../../../../../../lib/data/filters/utilities/createGetManyFilterSpecifications')
 
 const isAcceptedToTaxon = {
@@ -18,9 +19,46 @@ const isAcceptedToTaxon = {
   },
 }
 
+const isVernacularToTaxon = {
+  description: 'Find taxon names that are venacular to any taxon',
+  inputSchema: {
+    type: 'boolean',
+  },
+  key: 'isVernacularToTaxon',
+  sequelizeFilterFunction: ({ Op, value }) => {
+    if (value === undefined) {
+      return null
+    }
+    return {
+      vernacularToTaxonId: {
+        [Op.not]: null,
+      },
+    }
+  },
+}
+
+const rank = createStringMatchFilter({
+  fieldPath: 'rank',
+  key: 'rank',
+})
+
+const rubinNumber = createStringMatchFilter({
+  fieldPath: 'rubinNumber',
+  key: 'rubinNumber',
+})
+
+const taxonNameType = createStringMatchFilter({
+  fieldPath: 'taxonNameType',
+  key: 'taxonNameType',
+})
+
 const filterSpec = createGetManyFilterSpecifications({
   custom: {
     isAcceptedToTaxon,
+    isVernacularToTaxon,
+    rank,
+    rubinNumber,
+    taxonNameType,
   },
   include: ['id', 'ids', 'updatedAfter', 'nameSearch'],
 })
