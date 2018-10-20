6dba5b9f92663984e884cc28ccb47cb9fe2f2f96
antonoberg <oberg.anton@gmail.com>
2018-10-20 21:10:26 +0200

    [BACKEND] Add storeResourceActivity to core resources

--- packages/backend/src/services/taxonomyService/resources/taxon/data/transformationSpecifications/index.js
+++ packages/backend/src/services/taxonomyService/resources/taxon/data/transformationSpecifications/index.js
@@ -3,5 +3,6 @@ const transformationFunctions = require('./transformationFunctions')
 exports.importDataFromFile = {
   description: 'Importing taxa from file',
   srcFileName: 'taxa',
+  storeResourceActivity: true,
   transformationFunctions: [transformationFunctions.transformTaxon],
 }
