902d0713d5c94562fbade90e1e3ae37c8c869d10
antonoberg <oberg.anton@gmail.com>
2018-10-24 11:31:45 +0200

    [BACKEND] Retry elastic search connection on start

--- packages/backend/src/lib/dataStores/elasticsearch/db/index.js
+++ packages/backend/src/lib/dataStores/elasticsearch/db/index.js
@@ -4,24 +4,37 @@ const createLog = require('../../../../utilities/log')
 const log = createLog('lib/elasticsearch')
 
 module.exports = function connectDb({ config }) {
-  const { password, url, username } = config.elasticsearch
+  const { url } = config.elasticsearch
 
-  const host = `${username}:${password}@${url}`
+  let client
 
-  const client = new elasticsearch.Client({
-    host,
+  let count = 0
+  return new Promise(resolve => {
+    const attemptConnect = () => {
+      client = new elasticsearch.Client({
+        host: url,
+        maxRetries: 10,
+      })
+      return client
+        .ping({
+          requestTimeout: 30000,
+        })
+        .then(() => {
+          log.info('Connection has been established successfully.')
+          resolve(client)
+        })
+        .catch(err => {
+          count += 1
+          if (count > 10) {
+            log.alert('Unable to connect to the database:', err)
+            resolve(client)
+          } else {
+            setTimeout(() => {
+              attemptConnect()
+            }, 2000)
+          }
+        })
+    }
+    attemptConnect()
   })
-
-  return client
-    .ping({
-      requestTimeout: 30000,
-    })
-    .then(() => {
-      log.info('Connection has been established successfully.')
-      return client
-    })
-    .catch(err => {
-      log.alert('Unable to connect to the database:', err)
-      return client
-    })
 }
