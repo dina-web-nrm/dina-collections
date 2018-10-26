16311edc7c0396dd7915ec0e9ad4b0278230c561
Fredrik Olovsson <fogithub@gmail.com>
2018-10-26 14:53:49 +0200

    [UI] Remove fixed width in Coordinates

--- packages/ui/src/domainModules/collectionMammals/components/MammalManager/MainColumn/RecordForm/formParts/CollectingLocality/Coordinates.js
+++ packages/ui/src/domainModules/collectionMammals/components/MammalManager/MainColumn/RecordForm/formParts/CollectingLocality/Coordinates.js
@@ -5,15 +5,12 @@ import GeoPoint from 'common/es5/coordinatesConverter'
 
 const iconStyle = {
   float: 'left',
-  paddingRight: '0.2em',
-  width: '2em',
 }
 
 const coordinatesStyle = {
   float: 'left',
-  paddingLeft: '0.2em',
-  paddingRight: '0.2em',
-  width: '20em',
+  paddingLeft: '0.8em',
+  paddingRight: '0.8em',
 }
 
 const propTypes = {
@@ -36,16 +33,16 @@ const getCoordinates = (latitude, longitude) => {
   const lon = longitude < 0 ? 'W' : 'E'
   const geoPoint = new GeoPoint(Math.abs(lonNumber), Math.abs(latNumber))
 
-  const latCoodinate = `${geoPoint.getLatDeg()} ${lat}`
+  const latCoordinate = `${geoPoint.getLatDeg()} ${lat}`
   const lonCoordinate = `${geoPoint.getLonDeg()} ${lon}`
 
   if (latitude && longitude) {
-    return `${latCoodinate} ${lonCoordinate} , WGS84... `
+    return `${latCoordinate} ${lonCoordinate}, WGS84...`
   }
   if (latitude) {
-    return `${latCoodinate}, WGS84... `
+    return `${latCoordinate}, WGS84...`
   }
-  return `${lonCoordinate}, WGS84... `
+  return `${lonCoordinate}, WGS84...`
 }
 
 class Coordinates extends Component {
@@ -53,8 +50,9 @@ class Coordinates extends Component {
     const { latitude, longitude, onEdit: handleEditCoordinate } = this.props
 
     const coordinates = getCoordinates(latitude, longitude)
+
     return (
-      <div>
+      <React.Fragment>
         <div style={iconStyle}>
           <Icon name="marker" />
         </div>
@@ -66,7 +64,7 @@ class Coordinates extends Component {
             style={{ cursor: 'pointer' }}
           />
         </div>
-      </div>
+      </React.Fragment>
     )
   }
 }
