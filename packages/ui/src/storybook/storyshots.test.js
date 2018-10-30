e0fd18ff4d979b3e8c3b4ae464ba377941a083be
Fredrik Olovsson <fogithub@gmail.com>
2018-10-30 01:00:06 +0100

    storybook instead install babel 6 things [UI] Add jest mock for react-dom to fix storyshots
    https://github.com/storybooks/storybook/issues/2522#issuecomment-353199880

--- packages/ui/src/storybook/storyshots.test.js
+++ packages/ui/src/storybook/storyshots.test.js
@@ -2,5 +2,15 @@ import uiDescribe from 'utilities/test/uiDescribe'
 import initStoryshots from '@storybook/addon-storyshots'
 
 uiDescribe('storyshots', () => {
+  jest.mock('react-dom', () => {
+    return {
+      findDOMNode: () => {
+        return {}
+      },
+      render: () => null,
+      unmountComponentAtNode: () => null,
+    }
+  })
+
   initStoryshots({})
 })
