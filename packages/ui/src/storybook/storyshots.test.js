68544e39a83028a5cb2d52a832fb90596cdd24ba
Fredrik Olovsson <fogithub@gmail.com>
2018-10-30 01:00:06 +0100

    [UI] Add jest mock for react-dom to fix storyshots
    https://github.com/storybooks/storybook/issues/2522#issuecomment-353199880

--- packages/ui/src/storybook/storyshots.test.js
+++ packages/ui/src/storybook/storyshots.test.js
@@ -2,5 +2,15 @@ import uiDescribe from 'utilities/test/uiDescribe'
 import initStoryshots from '@storybook/addon-storyshots'
 
 uiDescribe('storyshots', () => {
+  jest.mock('react-dom', () => {
+    return {
+      render: () => null,
+      unmountComponentAtNode: () => null,
+      findDOMNode: () => {
+        return {}
+      },
+    }
+  })
+
   initStoryshots({})
 })
