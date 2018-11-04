b3e59f1d756d97120f5909100ade7be4bfa89371
Fredrik Olovsson <fogithub@gmail.com>
2018-11-04 23:18:17 +0100

    [UI] Add content to PageNotFound

--- packages/ui/src/apps/collectionsUi/viewModules/pageNotFound/Component.js
+++ packages/ui/src/apps/collectionsUi/viewModules/pageNotFound/Component.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button, Grid, Header } from 'semantic-ui-react'
 
 import PageTemplate from 'coreModules/commonUi/components/PageTemplate'
 
 const PageNotFound = () => (
-  <PageTemplate>
-    <h1>PageNotFound</h1>
+  <PageTemplate container fullViewHeight>
+    <Grid columns={1} textAlign="left">
+      <Grid.Column>
+        <Header>The page you are looking for could not be found.</Header>
+        Check the web address and try again. Or navigate to DINA Collections
+        start page.
+      </Grid.Column>
+      <Grid.Column>
+        <Link to="/">
+          <Button primary>DINA Collections start</Button>
+        </Link>
+      </Grid.Column>
+    </Grid>
   </PageTemplate>
 )
 
