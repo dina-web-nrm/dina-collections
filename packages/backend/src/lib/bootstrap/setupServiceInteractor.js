b3ed3b01d9540cb6faba58c07465209bd26d119a
antonoberg <oberg.anton@gmail.com>
2018-11-07 09:03:05 +0100

    [BACKEND] Allow custom model and api version  in sInteractor

--- packages/backend/src/lib/bootstrap/setupServiceInteractor.js
+++ packages/backend/src/lib/bootstrap/setupServiceInteractor.js
@@ -4,19 +4,32 @@ const {
   getResourceRelationshipParamsMap,
 } = require('common/src/schemaInterface')
 
+const loadVersionSet = require('common/src/schemaInterface/loadVersionSet')
+const singletons = require('common/src/schemaInterface/singletons')
+
 const createBaseConfig = require('../config/createBaseConfig')
 const createServiceInteractor = require('../serviceInteractor')
 const createFileInteractor = require('../fileInteractor')
 const createServices = require('../services')
 const setupConnectors = require('./setupConnectors')
 
-const resourceRelationshipParamsMap = getResourceRelationshipParamsMap()
-
 module.exports = function setupServiceInteractor({
+  apiVersion,
+  dataModelVersion,
   env,
   serviceDefinitions,
   serviceOrder,
 }) {
+  if (apiVersion && dataModelVersion) {
+    singletons.set(
+      loadVersionSet({
+        apiVersion,
+        dataModelVersion,
+      })
+    )
+  }
+
+  const resourceRelationshipParamsMap = getResourceRelationshipParamsMap()
   const config = createBaseConfig({ env })
 
   initializeStatistics({ config })
