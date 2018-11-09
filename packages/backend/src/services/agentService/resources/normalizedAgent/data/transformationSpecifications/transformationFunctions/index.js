a4d792a0c0d87b709a3ea07b6f03369abdbe8ac9
antonoberg <oberg.anton@gmail.com>
2018-11-09 10:57:41 +0100

    [BACKEND] Create new migration function for agent import

--- packages/backend/src/services/agentService/resources/normalizedAgent/data/transformationSpecifications/transformationFunctions/index.js
+++ packages/backend/src/services/agentService/resources/normalizedAgent/data/transformationSpecifications/transformationFunctions/index.js
@@ -3,7 +3,12 @@ const createDeleteProperties = require('common/src/createDeleteProperties')
 const deleteNullProperties = createDeleteProperties(null)
 
 /* eslint-disable no-param-reassign */
-exports.transformAgent = function transformTaxonName({ src, target }) {
+exports.transformAgent = function transformAgent({
+  src,
+  target,
+  migrator,
+  globalIndex,
+}) {
   const {
     legacyData,
     migrationData: {
@@ -32,49 +37,78 @@ exports.transformAgent = function transformTaxonName({ src, target }) {
     sourceData,
   } = src
 
-  const role = {
-    affiliation: {
-      name: affiliationName,
-    },
-    dataRange: {
-      endDate: {
-        year: roleEndYear,
-      },
-      startDate: {
-        year: roleStartYear,
-      },
-    },
-    name: roleName,
+  const role = {}
+
+  if (affiliationName) {
+    migrator.setValue({
+      obj: role,
+      path: 'affiliation.name',
+      value: affiliationName,
+    })
   }
 
-  // remarks
+  if (roleName) {
+    migrator.setValue({
+      obj: role,
+      path: 'name',
+      value: roleName,
+    })
+  }
+
+  if (roleStartYear) {
+    migrator.setValue({
+      obj: role,
+      path: 'dateRange.startDate.year',
+      value: Number(roleStartYear),
+    })
+  }
+  if (roleEndYear) {
+    migrator.setValue({
+      obj: role,
+      path: 'dateRange.endDate.year',
+      value: Number(roleEndYear),
+    })
+  }
 
   const attributes = {
     abbreviation,
-    additionalName,
     agentType,
-    alsoKnownAs,
-    birthDate,
     city,
     country,
-    deathDate,
-    disambiguatingDescription,
     email,
     familyName,
     fullName,
     givenName,
+    legacyData,
     postalCode,
     postOfficeBoxNumber,
-    roles: [role],
+    remarks,
+    roles: role && Object.keys(role).length ? [role] : [],
     stateProvince,
-    streetAddress,
-    telephone,
+    streetAddress: streetAdress,
+    telephone: telePhone,
+    title,
+  }
+
+  if (birthDateyear) {
+    migrator.setValue({
+      obj: attributes,
+      path: 'birthDate.startDate.year',
+      value: Number(birthDateyear),
+    })
+  }
+
+  if (deathDateyear) {
+    migrator.setValue({
+      obj: attributes,
+      path: 'deathDate.startDate.year',
+      value: Number(deathDateyear),
+    })
   }
 
   target.attributes = deleteNullProperties(attributes)
-  target.id = id
+  target.id = `${globalIndex + 1}`
   target.meta = {
     sourceData,
-    legacyData,
   }
 }
