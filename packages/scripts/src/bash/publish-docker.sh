030c376e1b1342f34f7e37b6d14eb42e96fa8146
antonoberg <oberg.anton@gmail.com>
2018-11-15 15:45:54 +0100

    [SCRIPTS] Add publish-docker.sh

---
+++ packages/scripts/src/bash/publish-docker.sh
@@ -0,0 +1,33 @@
+#!/bin/sh -
+# OBS:login with docker-hub credentials, set in ~/.docker/config
+# failsafe: check if that tag/version has already been published before pushing?
+# ./packages/scripts/src/bash/publish-docker.sh  -t 4.5.2
+usage()
+{
+  echo "usage: $0 -t <TAG>"
+}
+
+while getopts t: option
+ do
+  case "${option}"
+   in
+    (t) TAG=${OPTARG};;
+  esac
+ done
+
+if [ -z "$TAG" ]
+then
+  usage
+  exit 1
+fi
+
+echo "Info: This script publishes 4 images to Docker"
+echo "Pushing TAG=$TAG to Dockerhub"
+
+#docker login
+
+#push to docker hub
+docker push dina/dina-collections-api:$TAG
+docker push dina/dina-collections-ui:$TAG
+docker push dina/dina-semantic-ui-docs:$TAG
+docker push dina/dina-collections-migrations:$TAG
