f7dc39ed4307afb2a2291f61d48bf32e89bc5a80
Ingimar Erlingsson <inkimar@gmail.com>
2018-11-16 15:52:16 +0100

    [SCRIPTS] fix absolute path

--- packages/scripts/src/bash/check-services.sh
+++ packages/scripts/src/bash/check-services.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -
-$PWD/compare-services.sh
+FULL_PATH=$(dirname "$0")
+
+$FULL_PATH/compare-services.sh
 if [ $? -eq 0 ]; then
   echo 'Match! All docker-services are running!'; else
   echo 'Warning: one or more docker-services are down!';
--- packages/scripts/src/bash/compare-services.sh
+++ packages/scripts/src/bash/compare-services.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -
+FULL_PATH=$(dirname "$0")
+
 EXIT_STATUS=1;
 
-if [ "$(wc -l < $PWD/services.txt)" -eq "$(docker ps | grep -f $PWD/services.txt | wc -l)" ]; then 
+if [ "$(wc -l < $FULL_PATH/services.txt)" -eq "$(docker ps | grep -f $FULL_PATH/services.txt | wc -l)" ]; then 
   EXIT_STATUS=0;
 fi
 
