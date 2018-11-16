87430a3b937cc5bd30123c09f9f1bab85452e3c2
antonoberg <oberg.anton@gmail.com>
2018-11-16 14:43:58 +0100

    [SCRIPTS] Add new version of migrate-data.sh

--- packages/scripts/src/bash/migrate-data.sh
+++ packages/scripts/src/bash/migrate-data.sh
@@ -1,3 +1,22 @@
-#!/bin/bash
+#!/bin/sh -
+# ./packages/scripts/src/bash/migrate-docker.sh  -t 4.5.2
 
-docker-compose -f docker-compose.data.yaml up -d migrations
+while getopts t: option
+ do
+  case "${option}"
+   in
+    t) TAG=${OPTARG};;
+  esac
+ done
+
+if [ -z "$TAG" ]
+then
+   echo "usage: $0 -t <TAG>"
+   exit 1
+fi
+
+echo "pulling TAG=$TAG and migrating using 'docker-compose -f docker-compose.data.yaml up -d migrations'"
+
+
+# uses local images if they exist, otherwise fetches the images from hub.docker.com
+TAG=$TAG docker-compose -f docker-compose.data.yaml up -d migrations
