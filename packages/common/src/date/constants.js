e96ad338514d2c25e904f82cb597cedb918bb498
Fredrik Olovsson <fogithub@gmail.com>
2018-11-22 16:48:43 +0100

    [-] Add openRange to dateType enum

--- packages/common/src/date/constants.js
+++ packages/common/src/date/constants.js
@@ -1,11 +1,13 @@
 const LATEST = 'latest'
+const OPEN_RANGE = 'openRange'
 const RANGE = 'range'
 const SINGLE = 'single'
-const DATE_TYPES = [LATEST, RANGE, SINGLE]
+const DATE_TYPES = [LATEST, OPEN_RANGE, RANGE, SINGLE]
 
 module.exports = {
   DATE_TYPES,
   LATEST,
+  OPEN_RANGE,
   RANGE,
   SINGLE,
 }
