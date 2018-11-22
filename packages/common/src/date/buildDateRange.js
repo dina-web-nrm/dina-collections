5009e4097dcc6f342432470941a702a1c105c584
Fredrik Olovsson <fogithub@gmail.com>
2018-11-22 13:16:14 +0100

    [COMMON] Improve date range building utils
    1. Rewrite buildDateRange to have no logic except building the object
    and removing undefined values and in the end checking if the object has
    any value, then return it else return null
    
    2. Add getInterpretedDateRange* that looks at the dateType and decides
    what to pass to the buildDateRange function. E.g. if dateType is latest
    then set use getEarliestTimestamp for startInterpretedTimestamp and set
    date inputs as end date. If not latest date, then set date inputs as
    both start and end date inputs

--- packages/common/src/date/buildDateRange.js
+++ packages/common/src/date/buildDateRange.js
@@ -1,62 +1,65 @@
-const getTimestampFromYMD = require('./getTimestampFromYMD')
+const { DATE_TYPES } = require('./constants')
+const createDeleteProperties = require('../createDeleteProperties')
 
-const DATE_TYPES = ['latest', 'range', 'single']
+const deleteNullProperties = createDeleteProperties(null)
+const deleteUndefinedProperties = createDeleteProperties(undefined)
+
+const getDatePart = ({ day, month, year, timestamp, interpretedTimestamp }) => {
+  const datePart = deleteNullProperties(
+    deleteUndefinedProperties({
+      day,
+      interpretedTimestamp,
+      month,
+      timestamp,
+      year,
+    })
+  )
+
+  return Object.keys(datePart).length ? datePart : undefined
+}
 
 module.exports = function buildDateRange({
   startDay,
   startMonth,
   startYear,
+  startTimestamp,
+  startInterpretedTimestamp,
 
   endDay,
   endMonth,
   endYear,
-  dateType,
+  endTimestamp,
+  endInterpretedTimestamp,
 
+  dateType,
   dateText,
   remarks,
 }) {
-  if (!DATE_TYPES.includes(dateType)) {
-    throw new Error(`Unknown dataType: ${dateType}`)
+  if (dateType && !DATE_TYPES.includes(dateType)) {
+    throw new Error(`Unknown dateType: ${dateType}`)
   }
 
-  const range = {}
-  if (startDay || startMonth || startYear) {
-    const interpretedTimestamp = getTimestampFromYMD({
-      day: startDay,
-      isStartDate: true,
-      month: startMonth,
-      year: startYear,
+  const dateRange = deleteNullProperties(
+    deleteUndefinedProperties({
+      dateText,
+      dateType,
+      endDate: getDatePart({
+        day: endDay,
+        interpretedTimestamp: endInterpretedTimestamp,
+        month: endMonth,
+        timestamp: endTimestamp,
+        year: endYear,
+      }),
+      remarks,
+      startDate: getDatePart({
+        day: startDay,
+        interpretedTimestamp: startInterpretedTimestamp,
+        month: startMonth,
+        timestamp: startTimestamp,
+        year: startYear,
+      }),
     })
-    range.startDate = {
-      day: startDay ? Number(startDay) : undefined,
-      interpretedTimestamp,
-      month: startMonth ? Number(startMonth) : undefined,
-      year: startYear ? Number(startYear) : undefined,
-    }
-  }
+  )
 
-  if (remarks) {
-    range.remarks = remarks
-  }
-
-  if (endDay || endMonth || endYear) {
-    const interpretedTimestamp = getTimestampFromYMD({
-      day: endDay,
-      isEndDate: true,
-      month: endMonth,
-      year: endYear,
-    })
-    range.endDate = {
-      day: endDay ? Number(endDay) : undefined,
-      interpretedTimestamp,
-      month: endMonth ? Number(endMonth) : undefined,
-      year: endYear ? Number(endYear) : undefined,
-    }
-  }
-
-  if (dateText) {
-    range.dateText = dateText
-  }
-
-  return Object.keys(range).length ? range : null
+  return Object.keys(dateRange).length ? dateRange : null
 }
---
+++ packages/common/src/date/constants.js
@@ -0,0 +1,11 @@
+const LATEST = 'latest'
+const RANGE = 'range'
+const SINGLE = 'single'
+const DATE_TYPES = [LATEST, RANGE, SINGLE]
+
+module.exports = {
+  DATE_TYPES,
+  LATEST,
+  RANGE,
+  SINGLE,
+}
