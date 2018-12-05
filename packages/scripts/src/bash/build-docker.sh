3b5de2ead23b011fd25a422238114a65e4a614ea
antonoberg <oberg.anton@gmail.com>
2018-12-05 10:23:56 +0100

    [SCRIPTS] Add abort in build-docker.sh if error

--- packages/scripts/src/bash/build-docker.sh
+++ packages/scripts/src/bash/build-docker.sh
@@ -23,7 +23,26 @@ echo "Info: This script builds 3 docker-images"
 echo "Pushing TAG=$TAG to Dockerhub"
 
 npm run build:ui;
+if [ $? -ne 0 ]; then
+      echo "Aborting. exit is $?"
+      exit $?
+fi
+
 docker build -f ./packages/ui/Dockerfile -t dina/dina-collections-ui:$TAG -t dina/dina-collections-ui:latest ./packages/ui;
+if [ $? -ne 0 ]; then
+      echo "Aborting. exit is $?"
+      exit $?
+fi
+
 npm run uninstall;
 docker build -f ./packages/backend/Dockerfile -t dina/dina-collections-api:$TAG -t dina/dina-collections-api:latest ./packages;
+if [ $? -ne 0 ]; then
+      echo "Aborting. exit is $?"
+      exit $?
+fi
+
 docker build -f ./packages/migrations/Dockerfile -t dina/dina-collections-migrations:$TAG -t dina/dina-collections-migrations:latest ./packages;
+if [ $? -ne 0 ]; then
+      echo "Aborting. exit is $?"
+      exit $?
+fi
