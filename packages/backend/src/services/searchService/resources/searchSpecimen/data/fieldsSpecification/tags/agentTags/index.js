b663caca6750f8e2aa16057b5f8ab3bc9dc3985b
antonoberg <oberg.anton@gmail.com>
2018-12-11 13:59:06 +0100

    [BACKEND] Add more agent types

--- packages/backend/src/services/searchService/resources/searchSpecimen/data/fieldsSpecification/tags/agentTags/index.js
+++ packages/backend/src/services/searchService/resources/searchSpecimen/data/fieldsSpecification/tags/agentTags/index.js
@@ -21,33 +21,174 @@ const matchFilterName = 'matchAgentTags'
 
 const delimiter = 'ddaadd'
 
-const transformation = ({ migrator, src, target }) => {
-  const collectingInformation = migrator.getValue({
-    obj: src,
-    path: 'individual.collectingInformation',
+const extractAgentProps = ({ agent, migrator }) => {
+  if (!agent) {
+    return {}
+  }
+
+  const props = {}
+
+  const textI = migrator.getValue({
+    obj: agent,
+    path: 'textI',
   })
 
-  if (!collectingInformation) {
-    return null
+  if (textI) {
+    props.textI = textI
   }
+
+  const normalizedAgentFullName = migrator.getValue({
+    obj: agent,
+    path: 'normalized.fullName',
+  })
+
+  if (normalizedAgentFullName) {
+    props.normalizedAgentFullName = normalizedAgentFullName
+  }
+
+  return props
+}
+
+const addTags = ({ tags, tagType, textI, normalizedAgentFullName }) => {
+  if (textI) {
+    const tagValue = textI
+    tags.push({
+      key: `${tagType}${delimiter}${tagValue}`,
+      tagType,
+      tagValue,
+    })
+  }
+
+  if (normalizedAgentFullName) {
+    const tagValue = normalizedAgentFullName
+    tags.push({
+      key: `${tagType}${delimiter}${tagValue}`,
+      tagType,
+      tagValue,
+    })
+  }
+}
+
+const transformation = ({ migrator, src, target }) => {
+  const collectingInformation =
+    migrator.getValue({
+      obj: src,
+      path: 'individual.collectingInformation',
+    }) || []
+
   const tags = []
 
   collectingInformation.forEach(collectingInformationItem => {
-    const collectorFullName = migrator.getValue({
-      obj: collectingInformationItem,
-      path: 'collectedByAgent.textI',
+    const { textI, normalizedAgentFullName } = extractAgentProps({
+      agent: collectingInformationItem.collectedByAgent,
+      migrator,
     })
 
-    if (collectorFullName) {
-      const tagType = 'Collector'
-      const tagValue = collectorFullName
-      tags.push({
-        key: `${tagType}${delimiter}${tagValue}`,
-        tagType,
-        tagValue,
-      })
-    }
+    addTags({
+      normalizedAgentFullName,
+      tags,
+      tagType: 'Collector',
+      textI,
+    })
   })
+
+  const acquisition = migrator.getValue({
+    obj: src,
+    path: 'individual.acquisition',
+  })
+
+  if (acquisition) {
+    const { textI, normalizedAgentFullName } = extractAgentProps({
+      agent: acquisition.handedInByAgent,
+      migrator,
+    })
+    addTags({
+      normalizedAgentFullName,
+      tags,
+      tagType: 'Handed in by',
+      textI,
+    })
+  }
+
+  const collectionItems =
+    migrator.getValue({
+      obj: src,
+      path: 'individual.collectionItems',
+    }) || []
+
+  collectionItems.forEach(collectionItem => {
+    const curatorialAssessments = collectionItem.curatorialAssessments || []
+    curatorialAssessments.forEach(curatorialAssessment => {
+      const { textI, normalizedAgentFullName } = extractAgentProps({
+        agent: curatorialAssessment && curatorialAssessment.agent,
+        migrator,
+      })
+      addTags({
+        normalizedAgentFullName,
+        tags,
+        tagType: 'Curatorial assessed by',
+        textI,
+      })
+    })
+  })
+
+  const determinations =
+    migrator.getValue({
+      obj: src,
+      path: 'individual.determinations',
+    }) || []
+
+  determinations.forEach(determination => {
+    const { textI, normalizedAgentFullName } = extractAgentProps({
+      agent: determination && determination.determinedByAgent,
+      migrator,
+    })
+    addTags({
+      normalizedAgentFullName,
+      tags,
+      tagType: 'Determined by',
+      textI,
+    })
+  })
+
+  const featureObservations =
+    migrator.getValue({
+      obj: src,
+      path: 'individual.featureObservations',
+    }) || []
+
+  featureObservations.forEach(featureObservation => {
+    const { textI, normalizedAgentFullName } = extractAgentProps({
+      agent: featureObservation && featureObservation.featureObservationAgent,
+      migrator,
+    })
+    addTags({
+      normalizedAgentFullName,
+      tags,
+      tagType: 'Feature observed by',
+      textI,
+    })
+  })
+
+  const recordHistoryEvents =
+    migrator.getValue({
+      obj: src,
+      path: 'individual.recordHistoryEvents',
+    }) || []
+
+  recordHistoryEvents.forEach(recordHistoryEvent => {
+    const { textI, normalizedAgentFullName } = extractAgentProps({
+      agent: recordHistoryEvent && recordHistoryEvent.agent,
+      migrator,
+    })
+    addTags({
+      normalizedAgentFullName,
+      tags,
+      tagType: 'Record history by',
+      textI,
+    })
+  })
+
   if (tags && tags.length) {
     migrator.setValue({
       obj: target,
