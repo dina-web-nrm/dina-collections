b1aaf6d9802354d192c953c4ea4b9e245345f34e
antonoberg <oberg.anton@gmail.com>
2018-12-14 09:16:28 +0100

    [UI] Inject LocalManager into Locality picker

--- packages/ui/src/domainModules/locality/components/index.js
+++ packages/ui/src/domainModules/locality/components/index.js
@@ -1,5 +1,10 @@
-import LocalityDropdownPickerSearch from './LocalityDropdownPickerSearch'
+import LocalLocalityManager from './LocalityManager/Local'
+import LocalityDropdownPickerSearch, {
+  setLocalManager,
+} from './LocalityDropdownPickerSearch'
 import LocalityDropdownSearch from './LocalityDropdownSearch'
 import LocalityManager from './LocalityManager/Query'
+
+setLocalManager(LocalLocalityManager)
 
 export { LocalityDropdownPickerSearch, LocalityDropdownSearch, LocalityManager }
