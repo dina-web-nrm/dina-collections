f28e442ac66595de720ee78a3b6a78794810c52b
antonoberg <oberg.anton@gmail.com>
2018-12-17 16:26:57 +0100

    [BACKEND] Add indexes for storageLocation

---
+++ packages/backend/src/services/storageService/resources/storageLocation/data/migrations/addIndex.js
@@ -0,0 +1,27 @@
+const relationshipIndexes = [
+  {
+    name: 'idx_storageLocation_rel_taxa',
+    relationship: 'taxa',
+  },
+]
+
+module.exports = {
+  down: ({ queryInterface }) => {
+    const promises = relationshipIndexes.map(({ name }) => {
+      return queryInterface.sequelize.query(`DROP INDEX ${name}`)
+    })
+    return Promise.all(promises)
+  },
+  up: ({ queryInterface }) => {
+    const promises = relationshipIndexes.map(({ name, relationship }) => {
+      return queryInterface.sequelize.query(
+        `CREATE INDEX ${
+          name
+        } ON "storageLocations" USING gin ((relationships->'${
+          relationship
+        }'->'data'));`
+      )
+    })
+    return Promise.all(promises)
+  },
+}
