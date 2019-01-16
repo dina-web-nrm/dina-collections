2b7eaa2ee4798c53d7a8817accab04d347900b3b
antonoberg <oberg.anton@gmail.com>
2019-01-16 11:16:13 +0100

    [SCRIPTS] Update checkAllServers
    Use get-server-info-json.sh

--- packages/scripts/src/js/checkAllServers.js
+++ packages/scripts/src/js/checkAllServers.js
@@ -1,11 +1,30 @@
 /* eslint-disable sort-keys */
 require('console.table')
 const localExecCmd = require('./utilities/localExecCmd')
+const localExecScript = require('./utilities/localExecScript')
+const remoteExecScript = require('./utilities/remoteExecScript')
 const getServerHost = require('./utilities/getServerHost')
 const remoteExecCmd = require('./utilities/remoteExecCmd')
 const getAvailableServerNames = require('./utilities/getAvailableServerNames')
 
 const serverNames = getAvailableServerNames()
+
+const execScript = ({ serverName, scriptName }) => {
+  return Promise.resolve().then(() => {
+    if (serverName === 'local') {
+      return localExecScript({
+        scriptName,
+        printResult: false,
+        server: serverName,
+      })
+    }
+    return remoteExecScript({
+      scriptName,
+      printResult: false,
+      server: serverName,
+    })
+  })
+}
 
 const execCmd = ({ serverName, cmd }) => {
   return Promise.resolve().then(() => {
@@ -13,7 +32,6 @@ const execCmd = ({ serverName, cmd }) => {
       return localExecCmd({
         cmd,
         printResult: false,
-        server: serverName,
       })
     }
     return remoteExecCmd({
@@ -25,44 +43,78 @@ const execCmd = ({ serverName, cmd }) => {
 }
 
 const determineRunningVersions = serverName => {
+  let serverStatusObject = {
+    scriptEnvServerName: serverName,
+    scriptEnvHost: getServerHost(serverName),
+    externalIp: 'unknown',
+    gitBranch: 'unknown',
+    lastGitTag: 'unknown',
+    hostName: 'unknown',
+    connected: 'false',
+  }
+  let serviceStatusObject = {
+    scriptEnvServerName: serverName,
+    'dina-api': 'unknown',
+    'dina-collections-ui': 'unknown',
+    'dina-semantic-ui-docs': 'unknown',
+    'dina-worker': 'unknown',
+    elasticsearch: 'unknown',
+    keycloak: 'unknown',
+    'keycloak-mysql': 'unknown',
+    postgres: 'unknown',
+    proxy: 'unknown',
+  }
   return execCmd({
     serverName,
     cmd:
       'docker ps --filter "status=running"  --format "{{.Names}} {{.Image}}"',
-  }).then(statusMessage => {
-    return execCmd({
-      cmd: 'hostname',
-      serverName,
-    }).then(hostName => {
-      const statusObject = {
-        localServerName: serverName,
-        hostName: hostName.split('\n')[0],
-        host: getServerHost(serverName),
-        'dina-api': 'not running',
-        'dina-collections-ui': 'not running',
-        'dina-semantic-ui-docs': 'not running',
-        'dina-worker': 'not running',
-        elasticsearch: 'not running',
-        keycloak: 'not running',
-        'keycloak-mysql': 'not running',
-        postgres: 'not running',
-        proxy: 'not running',
-      }
-      statusMessage.split('\n').forEach(row => {
-        if (row) {
-          const segments = row.split(' ')
-          const serviceName = segments[0]
-          const image = segments[1]
-          const imageSegments = image.split(':')
-          const version = imageSegments[imageSegments.length - 1]
-
-          statusObject[serviceName] = version
-        }
-      })
-
-      return statusObject
-    })
   })
+    .then(statusMessage => {
+      return execScript({
+        scriptName: 'get-server-info-json.sh',
+        serverName,
+      }).then(infoString => {
+        const info = JSON.parse(infoString.replace(/(\r\n\t|\n|\r\t)/gm, ''))
+        serverStatusObject = {
+          ...serverStatusObject,
+          externalIp: info.externalIp,
+          gitBranch: info.branch,
+          lastGitTag: info.lastTag,
+          hostName: info.hostName,
+          connected: 'true',
+        }
+        serviceStatusObject = {
+          ...serviceStatusObject,
+          'dina-api': 'not running',
+          'dina-collections-ui': 'not running',
+          'dina-semantic-ui-docs': 'not running',
+          'dina-worker': 'not running',
+          elasticsearch: 'not running',
+          keycloak: 'not running',
+          'keycloak-mysql': 'not running',
+          postgres: 'not running',
+          proxy: 'not running',
+        }
+
+        statusMessage.split('\n').forEach(row => {
+          if (row) {
+            const segments = row.split(' ')
+            const serviceName = segments[0]
+            const image = segments[1]
+            const imageSegments = image.split(':')
+            const version = imageSegments[imageSegments.length - 1]
+
+            serviceStatusObject[serviceName] = version
+          }
+        })
+
+        return { serverStatusObject, serviceStatusObject }
+      })
+    })
+    .catch(err => {
+      console.log('err', err)
+      return { serverStatusObject, serviceStatusObject }
+    })
 }
 
 const promises = serverNames.map(serverName => {
@@ -72,7 +124,15 @@ const promises = serverNames.map(serverName => {
 Promise.all(promises)
   .then(output => {
     console.log('\n\n')
-    console.table(output)
+    console.table(
+      'Server status',
+      output.map(({ serverStatusObject }) => serverStatusObject)
+    )
+    console.log('\n\n')
+    console.table(
+      'Service status',
+      output.map(({ serviceStatusObject }) => serviceStatusObject)
+    )
   })
   .catch(err => {
     console.log('err', err)
