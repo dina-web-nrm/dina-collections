9bd976e9061bcc34dbc564fa5ed11355d41a42da
antonoberg <oberg.anton@gmail.com>
2019-01-16 20:07:16 +0100

    [SCRIPTS] Ensure server exist in captureServer...

--- packages/scripts/src/js/exec.js
+++ packages/scripts/src/js/exec.js
@@ -1,20 +1,8 @@
-const getAvailableServerNames = require('./utilities/getAvailableServerNames')
 const localExecScript = require('./utilities/localExecScript')
 const remoteExecScript = require('./utilities/remoteExecScript')
 const captureServerNameFromArg = require('./utilities/captureServerNameFromArg')
 
 const serverName = captureServerNameFromArg()
-if (!serverName) {
-  throw new Error(`Server name not provided`)
-}
-
-const availableServerNames = getAvailableServerNames()
-
-if (!availableServerNames.includes(serverName)) {
-  throw new Error(
-    `Invalid server name: "${serverName}" not found in script env`
-  )
-}
 
 if (serverName === 'local') {
   localExecScript()
