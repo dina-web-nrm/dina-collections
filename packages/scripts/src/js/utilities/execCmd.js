5816bef2d749241068737a7cf8b179c758488217
antonoberg <oberg.anton@gmail.com>
2019-01-17 09:52:56 +0100

    [SCRIPTS] Add utilities execCmd and execScript

---
+++ packages/scripts/src/js/utilities/execCmd.js
@@ -0,0 +1,13 @@
+const localExecCmd = require('./localExecCmd')
+const remoteExecCmd = require('./remoteExecCmd')
+
+module.exports = ({ serverName, ...rest } = {}) => {
+  if (serverName === 'local') {
+    return localExecCmd({
+      ...rest,
+    })
+  }
+  return remoteExecCmd({
+    ...rest,
+  })
+}
