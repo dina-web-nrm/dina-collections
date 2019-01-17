8efb44795166b4744fee67cb8061e317aa801dcc
antonoberg <oberg.anton@gmail.com>
2019-01-17 13:20:18 +0100

    [SCRIPTS] Make sure import-data-from-sql exit

--- packages/scripts/src/bash/docker-import-data-from-sql.sh
+++ packages/scripts/src/bash/docker-import-data-from-sql.sh
@@ -1,4 +1,4 @@
 source ./env/.backend
 
-cat ./data/dump.sql | docker exec -e PGPASSWORD=$DB_PASSWORD -i postgres psql -w -U $DB_USERNAME -d dina_dev -h $DB_URL
+cat ./data/dump.sql | docker exec -e PGPASSWORD=$DB_PASSWORD -i postgres psql -w -U $DB_USERNAME -d dina_dev -h $DB_URL && exit 0
 
