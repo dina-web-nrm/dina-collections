2353321f6530aaddd03e628a1b4b2444dd7b2d7c
antonoberg <oberg.anton@gmail.com>
2019-01-17 13:20:41 +0100

    [SCRIPTS] Throw error if missing file in build-data-zip

--- packages/scripts/src/bash/build-data-zip.sh
+++ packages/scripts/src/bash/build-data-zip.sh
@@ -3,7 +3,15 @@ args=()
 cd ./data
 
 for f in sample.*; do
+  if [ ! -f ${f/sample/} ]
+    then
+      echo ${f/sample/} 'dont exist'
+      exit 1
+    fi
     args+=(${f/sample/})
+
+
+
 done
 
 zip data.zip ${args[@]}
