39c11b0dcd04c16580d44a239fd6bee22bcad9cd
antonoberg <oberg.anton@gmail.com>
2019-01-18 10:12:38 +0100

    [BACKEND] Add isRoot to import transformations

--- packages/backend/src/services/storageService/resources/storageLocation/data/transformationSpecifications/transformationFunctions/index.js
+++ packages/backend/src/services/storageService/resources/storageLocation/data/transformationSpecifications/transformationFunctions/index.js
@@ -4,13 +4,25 @@ exports.transformStorageLocation = function transformStorageLocation({
   src,
   target,
 }) {
-  const { migrationData: { name, group, id, parentId }, sourceData } = src
+  const {
+    migrationData: { name, group, id, isRoot, parentId },
+    sourceData,
+  } = src
   if (!name) {
     return
   }
+
+  if (isRoot && parentId) {
+    throw new Error(`id: ${id} is root but have a parent id`)
+  }
+
   target.attributes = {
     group,
     name,
+  }
+
+  if (isRoot) {
+    target.attributes.isRoot = isRoot
   }
 
   target.id = id
