d34a059e6dfefd7153dd4e083353e4f5a3c8ec37
Fredrik Olovsson <fogithub@gmail.com>
2019-01-24 16:19:51 +0100

    [UI] Add FeatureTypeRange component

---
+++ packages/ui/src/coreModules/search/components/FeatureTypeRange/index.js
@@ -0,0 +1,114 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Grid } from 'semantic-ui-react'
+
+import { DropdownSearch, Field, Input } from 'coreModules/form/components'
+import TagTypeDropdown from '../TagTypeDropdown'
+
+const propTypes = {
+  buildLocalAggregationQuery: PropTypes.func.isRequired,
+  module: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  resource: PropTypes.string.isRequired,
+  search: PropTypes.func.isRequired,
+  tagTypeDropdownPlaceholder: PropTypes.string,
+  tagTypeInitialOptionValue: PropTypes.string,
+  tagTypeInlineDescription: PropTypes.node,
+  tagTypeMatchAllOptionText: PropTypes.string,
+  unitOptions: PropTypes.arrayOf(
+    PropTypes.shape({
+      key: PropTypes.string.isRequired,
+      text: PropTypes.number.isRequired,
+      value: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+}
+const defaultProps = {
+  tagTypeDropdownPlaceholder: undefined,
+  tagTypeInitialOptionValue: undefined,
+  tagTypeInlineDescription: undefined,
+  tagTypeMatchAllOptionText: undefined,
+}
+
+const FeatureTypeRange = ({
+  buildLocalAggregationQuery,
+  module,
+  name: baseName,
+  resource,
+  search,
+  tagTypeDropdownPlaceholder,
+  tagTypeInitialOptionValue,
+  tagTypeInlineDescription,
+  tagTypeMatchAllOptionText,
+  unitOptions,
+}) => {
+  return (
+    <Grid textAlign="left" verticalAlign="top">
+      <Grid.Row>
+        <Grid.Column width={16}>
+          <Field
+            autoComplete="off"
+            buildLocalAggregationQuery={buildLocalAggregationQuery}
+            component={TagTypeDropdown}
+            inline
+            inlineDescription={tagTypeInlineDescription}
+            module={module}
+            name={`${baseName}.rangeType`}
+            placeholder={tagTypeDropdownPlaceholder}
+            resource={resource}
+            search={search}
+            tagTypeInitialOptionValue={tagTypeInitialOptionValue}
+            tagTypeMatchAllOptionText={tagTypeMatchAllOptionText}
+          />
+        </Grid.Column>
+        <Grid.Column width={5}>
+          <Field
+            autoComplete="off"
+            component={Input}
+            enableHelpNotifications={false}
+            fluid
+            label="from"
+            min={0}
+            module={module}
+            name={`${baseName}.rangeValue.min`}
+            subLabel
+            type="number"
+          />
+        </Grid.Column>
+        <Grid.Column width={5}>
+          <Field
+            autoComplete="off"
+            component={Input}
+            enableHelpNotifications={false}
+            fluid
+            label="to"
+            min={0}
+            module={module}
+            name={`${baseName}.rangeValue.max`}
+            subLabel
+            type="number"
+          />
+        </Grid.Column>
+        <Grid.Column width={6}>
+          <Field
+            autoComplete="off"
+            component={DropdownSearch}
+            enableHelpNotifications={false}
+            fluid
+            label="unit"
+            module={module}
+            name={`${baseName}.rangeUnit`}
+            options={unitOptions}
+            subLabel
+            type="dropdown-search-local"
+          />
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  )
+}
+
+FeatureTypeRange.propTypes = propTypes
+FeatureTypeRange.defaultProps = defaultProps
+
+export default FeatureTypeRange
--- packages/ui/src/coreModules/search/components/index.js
+++ packages/ui/src/coreModules/search/components/index.js
@@ -1,18 +1,14 @@
+export { default as FeatureTypeRange } from './FeatureTypeRange'
 export {
   default as MultipleChoiceCheckboxes,
 } from './MultipleChoiceCheckboxes/Local'
-
-export { default as SearchPreviewField } from './SearchPreview/Field'
-
 export {
   default as MultipleChoiceCheckboxesField,
 } from './MultipleChoiceCheckboxes/Field'
-
-export { default as TagTypeDropdownField } from './TagTypeDropdown/Field'
-
 export { default as MultipleSearchTagsSelect } from './MultipleSearchTagsSelect'
 export {
   default as MultipleSearchTagsSelectField,
 } from './MultipleSearchTagsSelect/Field'
-
 export { default as NoResultsFound } from './NoResultsFound'
+export { default as SearchPreviewField } from './SearchPreview/Field'
+export { default as TagTypeDropdownField } from './TagTypeDropdown/Field'
