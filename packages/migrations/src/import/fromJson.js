e8fa7c3c583552dfe5fe20af78d1126b858f399b
antonoberg <oberg.anton@gmail.com>
2019-01-29 11:54:59 +0100

    [MIGRATIONS] Adapt to backend data/index renaming

--- packages/migrations/src/import/fromJson.js
+++ packages/migrations/src/import/fromJson.js
@@ -1,5 +1,5 @@
 require('../utilities/loadBackendEnv')
 
-const loadDataPath = `backend/src/apps/${process.env.NODE_ENV}/data/index.js`
+const loadDataPath = `backend/src/apps/${process.env.NODE_ENV}/data/import.js`
 
 require(loadDataPath) // eslint-disable-line
