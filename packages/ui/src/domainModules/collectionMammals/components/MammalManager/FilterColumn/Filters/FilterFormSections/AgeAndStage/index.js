ca0c9a2b2debd2894387885f9cf0fd77fef2f8ac
Fredrik Olovsson <fogithub@gmail.com>
2019-01-31 14:26:21 +0100

    [UI] Fix header layout in age and stage filter

--- packages/ui/src/domainModules/collectionMammals/components/MammalManager/FilterColumn/Filters/FilterFormSections/AgeAndStage/index.js
+++ packages/ui/src/domainModules/collectionMammals/components/MammalManager/FilterColumn/Filters/FilterFormSections/AgeAndStage/index.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react'
-import { Grid } from 'semantic-ui-react'
+import { Form, Grid } from 'semantic-ui-react'
 
-import { Field, Input } from 'coreModules/form/components'
+import { Field, FieldLabel, Input } from 'coreModules/form/components'
 import { MultipleChoiceCheckboxesField } from 'coreModules/search/components'
 import { higherOrderComponents } from '../../../queryBuilder'
 
@@ -13,34 +13,44 @@ class AgeStageFilterForm extends PureComponent {
   render() {
     return (
       <Grid textAlign="left" verticalAlign="top">
-        <Grid.Row>
-          <Grid.Column width={16}>Age in years</Grid.Column>
-          <Grid.Column width={7}>
-            <Field
-              autoComplete="off"
-              component={Input}
-              fluid
-              label="From"
-              module="collectionMammals"
-              name="ageAndStage.age.min"
-              type="number"
-            />
-          </Grid.Column>
-          <Grid.Column textAlign="center" verticalAlign="middle" width={2}>
-            {'—'}
-          </Grid.Column>
-          <Grid.Column width={7}>
-            <Field
-              autoComplete="off"
-              component={Input}
-              fluid
-              label="To"
-              module="collectionMammals"
-              name="ageAndStage.age.max"
-              type="number"
-            />
-          </Grid.Column>
-        </Grid.Row>
+        <Grid.Column width={16}>
+          <Form.Field
+            style={{
+              position: 'relative',
+              width: '100%',
+            }}
+          >
+            <FieldLabel label="Age in years" />
+            <Grid textAlign="left" verticalAlign="top">
+              <Grid.Column width={7}>
+                <Field
+                  autoComplete="off"
+                  component={Input}
+                  enableHelpNotifications={false}
+                  fluid
+                  label="from"
+                  module="collectionMammals"
+                  name="ageAndStage.age.min"
+                  subLabel
+                  type="number"
+                />
+              </Grid.Column>
+              <Grid.Column width={7}>
+                <Field
+                  autoComplete="off"
+                  component={Input}
+                  enableHelpNotifications={false}
+                  fluid
+                  label="to"
+                  module="collectionMammals"
+                  name="ageAndStage.age.max"
+                  subLabel
+                  type="number"
+                />
+              </Grid.Column>
+            </Grid>
+          </Form.Field>
+        </Grid.Column>
         <Grid.Column width={16}>
           <Field
             component={WrappedMultipleChoiceCheckboxesField}
