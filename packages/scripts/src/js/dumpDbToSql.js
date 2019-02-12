3e8c63254c5f60e7a2ffbe8e3e9344c0b7411bd2
Fredrik Olovsson <fogithub@gmail.com>
2019-02-12 13:50:21 +0100

    [SCRIPTS] Fix script names

--- packages/scripts/src/js/dumpDbToSql.js
+++ packages/scripts/src/js/dumpDbToSql.js
@@ -24,7 +24,7 @@ return promptContinue({
         console.log('Dump created with success')
         if (serverName !== 'local') {
           console.log(
-            `Run: "yarn admin:download:data:files:sql -s ${
+            `Run: "yarn remote:download:data:sql -s ${
               serverName
             }" to download dump`
           )
