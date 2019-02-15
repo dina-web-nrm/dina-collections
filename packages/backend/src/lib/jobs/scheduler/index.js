4db2009f7e60045036e6112ea6f30e9aa15c60ae
antonoberg <oberg.anton@gmail.com>
2019-02-15 11:16:32 +0100

    [BACKEND] Don't reindex-search specimen
    on deploy new worker

--- packages/backend/src/lib/jobs/scheduler/index.js
+++ packages/backend/src/lib/jobs/scheduler/index.js
@@ -2,28 +2,6 @@ const createLog = require('../../../utilities/log')
 
 const log = createLog('lib/jobs/scheduler')
 
-module.exports = function createScheduler({ serviceInteractor }) {
+module.exports = function createScheduler() {
   log.info('Start scheduler')
-
-  return serviceInteractor
-    .call({
-      operationId: 'searchSpecimenRequestRebuildView',
-      request: {
-        body: {
-          attributes: {
-            operationRequest: {
-              queryParams: {
-                limit: 1000000,
-              },
-            },
-          },
-        },
-      },
-    })
-    .then(() => {
-      log.info('Adding job success')
-    })
-    .catch(err => {
-      log.err('Adding job fail', err)
-    })
 }
