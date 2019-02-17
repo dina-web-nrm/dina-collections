cbf85788dcb9362c4706a601a241f2d20bf3f8c6
Fredrik Olovsson <fogithub@gmail.com>
2019-02-17 21:46:36 +0100

    [BACKEND] Add filters to resourceActivity
    For polymorphic relationships

--- packages/backend/src/services/historyService/resources/resourceActivity/data/filterSpecifications/index.js
+++ packages/backend/src/services/historyService/resources/resourceActivity/data/filterSpecifications/index.js
@@ -1,5 +1,6 @@
 const createGetManyFilterSpecifications = require('../../../../../../lib/data/filters/utilities/createGetManyFilterSpecifications')
 const createEqualFilter = require('../../../../../../lib/data/filters/factories/createEqualFilter')
+const createResourceActivityFilterByIds = require('../../../../../../lib/data/filters/factories/createResourceActivityFilterByIds')
 
 const equalFilterStringParameters = [
   'action',
@@ -12,7 +13,7 @@ const equalFilterStringParameters = [
   'userId',
 ]
 
-const customFilters = equalFilterStringParameters.reduce(
+const stringFilters = equalFilterStringParameters.reduce(
   (obj, filterParameter) => {
     let fieldPath = filterParameter
     if (filterParameter === 'relationshipId') {
@@ -38,7 +39,37 @@ const customFilters = equalFilterStringParameters.reduce(
 )
 
 const filters = createGetManyFilterSpecifications({
-  custom: customFilters,
+  custom: {
+    ...stringFilters,
+    normalizedAgentIds: createResourceActivityFilterByIds({
+      key: 'normalizedAgentIds',
+      resource: 'normalizedAgent',
+    }),
+    physicalObjectIds: createResourceActivityFilterByIds({
+      key: 'physicalObjectIds',
+      resource: 'physicalObject',
+    }),
+    placeIds: createResourceActivityFilterByIds({
+      key: 'placeIds',
+      resource: 'place',
+    }),
+    specimenIds: createResourceActivityFilterByIds({
+      key: 'specimenIds',
+      resource: 'specimen',
+    }),
+    storageLocationIds: createResourceActivityFilterByIds({
+      key: 'storageLocationIds',
+      resource: 'storageLocation',
+    }),
+    taxonIds: createResourceActivityFilterByIds({
+      key: 'taxonIds',
+      resource: 'taxon',
+    }),
+    taxonNameIds: createResourceActivityFilterByIds({
+      key: 'taxonNameIds',
+      resource: 'taxonName',
+    }),
+  },
   include: ['id', 'ids', 'updatedAfter'],
 })
 
