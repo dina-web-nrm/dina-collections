06290806762dcdb9a3e17e2730a6c113c1cd74f7
antonoberg <oberg.anton@gmail.com>
2019-02-20 14:40:19 +0100

    [COMMON] Refactor JAC update relationships

--- packages/common/src/jsonApiClient/modify/updateRelationship.js
+++ packages/common/src/jsonApiClient/modify/updateRelationship.js
@@ -2,6 +2,7 @@ const schemaInterface = require('../../schemaInterface')
 const createLog = require('../../log')
 const { Dependor } = require('../../Dependor')
 const buildOperationId = require('../../buildOperationId')
+const { inverseUpdateRelationship } = require('./inverseUpdateRelationship')
 
 const openApiSpec = schemaInterface.getOpenApiSpec()
 
@@ -22,88 +23,6 @@ Object.keys(openApiSpec.paths).forEach(path => {
   })
 }, {})
 
-const deleteNotIncludedRelationships = ({
-  item,
-  log,
-  openApiClient,
-  relationItemsToUpdate,
-  relationKey,
-  type,
-  inverseOperationId,
-}) => {
-  const getRelationshipOperationId = buildOperationId({
-    operationType: 'getRelationship',
-    relationKey,
-    resource: type,
-  })
-  const inverseGetOperationId =
-    inverseOperationIdMap[getRelationshipOperationId]
-
-  return openApiClient
-    .call(inverseGetOperationId || getRelationshipOperationId, {
-      pathParams: {
-        id: item.id,
-      },
-    })
-    .then(result => {
-      const { data: existingRelations } = result
-
-      if (existingRelations) {
-        const existingRelationsArray = Array.isArray(existingRelations)
-          ? existingRelations
-          : [existingRelations]
-
-        const relationItemsToUpdateArray = Array.isArray(relationItemsToUpdate)
-          ? relationItemsToUpdate
-          : [relationItemsToUpdate]
-
-        const relationsToRemove = existingRelationsArray.filter(
-          ({ id: existingId }) => {
-            const isPreexisting = relationItemsToUpdateArray.find(({ id }) => {
-              return id === existingId
-            })
-
-            return !isPreexisting
-          }
-        )
-
-        if (relationsToRemove.length) {
-          log.debug(
-            `The following ${relationKey} should be removed:`,
-            relationsToRemove
-          )
-
-          const updateRelationshipOperationId = buildOperationId({
-            operationType: 'updateRelationship',
-            relationKey,
-            resource: type,
-          })
-
-          const promises = relationsToRemove.map(relationToRemove => {
-            return openApiClient.call(
-              inverseOperationId || updateRelationshipOperationId,
-              {
-                body: {
-                  data: null,
-                },
-                pathParams: {
-                  id: inverseOperationId ? relationToRemove.id : item.id,
-                },
-              }
-            )
-          })
-          return Promise.all(promises)
-        }
-
-        log.debug(`Nothing to remove for ${relationKey}`)
-        return Promise.resolve()
-      }
-
-      log.debug(`No existing relations for ${relationKey}`)
-      return Promise.resolve({ data: null })
-    })
-}
-
 function updateRelationship({
   item,
   log = defaultLog,
@@ -120,89 +39,42 @@ function updateRelationship({
     relationKey,
     resource: type,
   })
-  const inverseOperationId = inverseOperationIdMap[operationId]
-  if (inverseOperationId) {
-    log.debug(
-      `inverse updateRelationship with ${inverseOperationId} for ${resourcePath} -> ${
-        item.id
-      } @ key: ${relationKey}. relationships: `,
-      data
-    )
 
-    return deleteNotIncludedRelationships({
-      inverseOperationId,
+  const inverseUpdateOperationId = inverseOperationIdMap[operationId]
+  if (inverseUpdateOperationId) {
+    return inverseUpdateRelationship({
+      data,
+      id,
+      inverseUpdateOperationId,
       isArray,
       item,
-      log,
-      openApiClient,
-      relationItemsToUpdate: data,
-      relationKey,
-      type,
-    }).then(removedRelationships => {
-      log.debug(
+      log: log.scope(
         `inverse updateRelationship for ${resourcePath} -> ${
           item.id
-        } @ key: ${relationKey}. relationships: `,
-        data
-      )
-      if (isArray) {
-        const promises = data.map(relationshipItem => {
-          return openApiClient.call(inverseOperationId, {
-            body: {
-              data: {
-                id,
-                type,
-              },
-            },
-            pathParams: {
-              id: relationshipItem.id,
-            },
-          })
-        })
-        return Promise.all(promises)
-      }
-
-      if (data && data.id) {
-        return openApiClient.call(inverseOperationId, {
-          body: {
-            data: {
-              id,
-              type,
-            },
-          },
-          pathParams: {
-            id: data.id,
-          },
-        })
-      }
-
-      return removedRelationships
+        } @ key: ${relationKey} with type: ${isArray ? 'array' : 'object'}`
+      ),
+      openApiClient,
+      relationKey,
+      type,
     })
   }
 
-  return deleteNotIncludedRelationships({
-    inverseOperationId,
-    isArray,
-    item,
-    log,
-    openApiClient,
-    relationItemsToUpdate: data,
-    relationKey,
-    type,
-  }).then(() => {
-    log.debug(
-      `updateRelationship for ${resourcePath} -> ${
-        item.id
-      } @ key: ${relationKey}. relationships: `,
-      data
-    )
-    return openApiClient.call(operationId, {
-      body: { data },
-      pathParams: {
-        id,
-      },
-    })
-  })
+  log.debug(
+    `updateRelationship for ${resourcePath} -> ${
+      item.id
+    } @ key: ${relationKey}. relationships: `,
+    data
+  )
+
+  const request = {
+    body: { data },
+    pathParams: {
+      id,
+    },
+  }
+  log.debug(`updating with operationId: ${operationId} and request:`, request)
+
+  return openApiClient.call(operationId, request)
 }
 
 module.exports = {
