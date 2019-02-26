e61f8151ab313a748bdde5cf32f67684e1876c3f
antonoberg <oberg.anton@gmail.com>
2019-02-26 12:31:28 +0100

    [SCRIPTS] Prepare scripts for travis db and api tests

---
+++ packages/scripts/src/bash/ci-install.sh
@@ -0,0 +1,75 @@
+#!/bin/sh -
+#./packages/scripts/src/bash/ci-install.sh
+
+
+if [ "$CI_INSTALL_UI" = true ]; then
+  echo "Installing ui"
+  yarn install:ui
+fi
+
+if [ "$CI_INSTALL_MODELS" = true ]; then
+  echo "Installing models"
+  yarn install:schema
+fi
+
+if [ "$CI_INSTALL_BACKEND" = true ]; then
+  echo "Installing backend"
+  yarn install:backend
+fi
+
+if [ "$CI_INSTALL_COMMON" = true ]; then
+  echo "Installing common"
+  yarn install:common
+fi
+
+
+if [ "$CI_INSTALL_SCRIPTS" = true ]; then
+  echo "Installing scripts"
+  yarn install:scripts
+fi
+
+
+if [ "$CI_INSTALL_MIGRATIONS" = true ]; then
+  echo "Installing migrations"
+  yarn install:migrations
+fi
+
+
+if [ "$CI_LINK_COMMON" = true ]; then
+  : "${CI_INSTALL_COMMON?CI_INSTALL_COMMON Has to be true}"
+
+  echo "Linking common"
+  yarn setup:links:common
+fi
+
+if [ "$CI_LINK_UI" = true ]; then
+  : "${CI_INSTALL_UI?CI_INSTALL_UI Has to be true}"
+  : "${CI_LINK_COMMON?CI_LINK_COMMON Has to be true}"
+
+  echo "Linking ui"
+  yarn setup:links:ui
+fi
+
+
+if [ "$CI_LINK_SCRIPTS" = true ]; then
+  : "${CI_INSTALL_SCRIPTS?CI_INSTALL_SCRIPTS Has to be true}"
+  echo "Linking scripts"
+  yarn setup:links:scripts
+fi
+
+if [ "$CI_LINK_BACKEND" = true ]; then
+  : "${CI_INSTALL_BACKEND?CI_INSTALL_BACKEND Has to be true}"
+  : "${CI_LINK_COMMON?CI_LINK_COMMON Has to be true}"
+
+  echo "Linking backend"
+  yarn setup:links:backend
+fi
+
+if [ "$CI_LINK_MIGRATIONS" = true ]; then
+  : "${CI_INSTALL_MIGRATIONS?CI_INSTALL_MIGRATIONS Has to be true}"
+
+  : "${CI_LINK_BACKEND?CI_LINK_BACKEND Has to be true}"
+  : "${CI_LINK_SCRIPTS?CI_LINK_SCRIPTS Has to be true}"
+  echo "Linking migrations"
+  yarn setup:links:migrations
+fi
---
+++ packages/scripts/src/bash/ci-test.sh
@@ -0,0 +1,140 @@
+#!/bin/sh -
+#./packages/scripts/src/bash/ci-test.sh
+
+START_DIRECTORY=$PWD
+
+if [ "$CI_TEST_ALL" = true ]; then
+  yarn test
+  if [ $? -ne 0 ]; then
+    echo "Aborting. exit is not 0"
+    exit 1
+  fi
+fi
+
+if [ "$CI_TEST_UI_LINT" = true ]; then
+  echo "Running test suite CI_TEST_UI_LINT"
+  cd ./packages/ui && yarn lint:js
+  if [ $? -ne 0 ]; then
+    echo "Aborting. exit is not 0"
+    exit 1
+  fi
+
+  cd $START_DIRECTORY
+fi
+
+if [ "$CI_TEST_UI_UNIT" = true ]; then
+  echo "Running test suite CI_TEST_UI_UNIT"
+  cd ./packages/ui && yarn test:unit
+  if [ $? -ne 0 ]; then
+    echo "Aborting. exit is not 0"
+    exit 1
+  fi
+
+  cd $START_DIRECTORY
+fi
+
+if [ "$CI_TEST_MODELS_LINT" = true ]; then
+  echo "Running test suite CI_TEST_MODELS_LINT"
+  cd ./packages/models && yarn lint:js
+  if [ $? -ne 0 ]; then
+    echo "Aborting. exit is not 0"
+    exit 1
+  fi
+
+  cd $START_DIRECTORY
+fi
+
+if [ "$CI_TEST_COMMON_LINT" = true ]; then
+  echo "Running test suite CI_TEST_COMMON_LINT"
+  cd ./packages/common && yarn lint:js
+  if [ $? -ne 0 ]; then
+    echo "Aborting. exit is not 0"
+    exit 1
+  fi
+
+  cd $START_DIRECTORY
+fi
+
+if [ "$CI_TEST_COMMON_UNIT" = true ]; then
+  echo "Running test suite CI_TEST_COMMON_UNIT"
+  cd ./packages/common && yarn test:unit
+  if [ $? -ne 0 ]; then
+    echo "Aborting. exit is not 0"
+    exit 1
+  fi
+
+  cd $START_DIRECTORY
+fi
+
+
+if [ "$CI_TEST_SCRIPTS_LINT" = true ]; then
+  echo "Running test suite CI_TEST_SCRIPTS_LINT"
+  cd ./packages/scripts && yarn lint:js
+  if [ $? -ne 0 ]; then
+    echo "Aborting. exit is not 0"
+    exit 1
+  fi
+
+  cd $START_DIRECTORY
+fi
+
+if [ "$CI_TEST_BACKEND_LINT" = true ]; then
+  echo "Running test suite CI_TEST_BACKEND_LINT"
+  yarn setup:env && cd ./packages/backend && yarn lint:js
+  if [ $? -ne 0 ]; then
+    echo "Aborting. exit is not 0"
+    exit 1
+  fi
+
+  cd $START_DIRECTORY
+fi
+
+if [ "$CI_TEST_BACKEND_UNIT" = true ]; then
+  echo "Running test suite CI_TEST_BACKEND_UNIT"
+  yarn setup:env && cd ./packages/backend && yarn test:unit
+  if [ $? -ne 0 ]; then
+    echo "Aborting. exit is not 0"
+    exit 1
+  fi
+
+  cd $START_DIRECTORY
+fi
+
+if [ "$CI_TEST_BACKEND_DB" = true ]; then
+  echo "Running test suite CI_TEST_BACKEND_DB"
+  cd ./packages/backend && yarn test:db
+  if [ $? -ne 0 ]; then
+    echo "Aborting. exit is not 0"
+    exit 1
+  fi
+  exit 0
+fi
+
+
+if [ "$CI_TEST_BACKEND_API" = true ]; then
+  echo "Running test suite CI_TEST_BACKEND_API"
+  cd ./packages/backend && yarn test:api
+  if [ $? -ne 0 ]; then
+    echo "Aborting. exit is not 0"
+    exit 1
+  fi
+  exit 0
+fi
+
+
+if [ "$CI_TEST_MIGRATIONS_LINT" = true ]; then
+  echo "Running test suite CI_TEST_MIGRATIONS_LINT"
+  cd ./packages/migrations && yarn lint:js
+  if [ $? -ne 0 ]; then
+    echo "Aborting. exit is not 0"
+    exit 1
+  fi
+
+  cd $START_DIRECTORY
+fi
+
+
+
+
+
+
