75b1a06b43c14b095ff3504cf9e256284e49bab9
antonoberg <oberg.anton@gmail.com>
2019-03-04 17:04:44 +0100

    [BACKEND] Add specimenSearch sample data test

---
+++ packages/backend/src/services/searchService/integrationTests/sampleData.test.js
@@ -0,0 +1,44 @@
+const {
+  describe: apiSampleDescribe,
+  hook,
+} = require('common/src/testUtilities/envBackendApiSampleData')
+const waitForApiRestart = require('../../../utilities/test/waitForApiRestart')
+const { makeTestCall } = require('../../../utilities/test/testApiClient')
+
+apiSampleDescribe(`sample data`, () => {
+  hook(beforeAll, () => {
+    return waitForApiRestart()
+  })
+
+  it('returns expected sample data', () => {
+    return makeTestCall({
+      body: {
+        data: {
+          attributes: {
+            limit: 1000,
+            sort: ['attributes.idNumeric:asc'],
+          },
+        },
+      },
+      operationId: 'searchSpecimenQuery',
+      validateOutput: true,
+    }).then(res => {
+      expect(res.data.length).toBe(16)
+
+      const cleanData = res.data.map(item => {
+        if (item.attributes.recordEventLastModified) {
+          /* eslint-disable no-param-reassign */
+          item.attributes.recordEventLastModified = item.attributes.recordEventLastModified.map(
+            () => {
+              return 'timestamp'
+            }
+          )
+          /* eslint-enable no-param-reassign */
+        }
+        return item
+      })
+
+      expect(cleanData).toMatchSnapshot()
+    })
+  })
+})
