28b41fd1d0fd5c1e7a68b873cd0ea0d3d33f6d58
Ida Li <ida.li@nrm.se>
2019-03-04 11:20:49 +0100

    [UI] Add e2e test for home page

--- packages/ui/cypress/e2e/home/index.test.js
+++ packages/ui/cypress/e2e/home/index.test.js
@@ -1,5 +1,5 @@
 context('Home', () => {
-  beforeEach(() => {
+  before(() => {
     cy.visit('/')
   })
 
@@ -8,5 +8,28 @@ context('Home', () => {
       .should('exist')
       .should('have.attr', 'src')
       .should('match', /^\/static\/media\/logo\..+\.png$/)
+  })
+
+  it('has title', () => {
+    cy.title().should('eq', 'DINA Collection Management System')
+  })
+
+  it('has header', () => {
+    cy.getByTestId('home-header').should('have.text', 'Collections UI')
+  })
+
+  it('has descriptions', () => {
+    cy.getByTestId('home-descriptions')
+      .children()
+      .should('exist')
+      .and('have.length', 2)
+  })
+
+  it('has link to footer', () => {
+    cy.getByTestId('home-readmore-button').shouldHaveHref('#footer')
+  })
+
+  it('has link to login', () => {
+    cy.getByTestId('home-login-button').shouldHaveHref('/login')
   })
 })
