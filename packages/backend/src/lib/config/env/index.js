d3c28a77fd0940821aedacd3e19e8884554997bc
antonoberg <oberg.anton@gmail.com>
2019-03-06 15:36:36 +0100

    [BACKEND] Move ensureNodeEnv to separate file

--- packages/backend/src/lib/config/env/index.js
+++ packages/backend/src/lib/config/env/index.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv')
 const resolveEnvVariables = require('./resolveEnvVariables')
+const ensureNodeEnv = require('./ensureNodeEnv')
 
 dotenv.config()
 
@@ -55,34 +56,6 @@ function readKey(key) {
 
 function readBoolKey(key) {
   return readKey(key) === true
-}
-
-const allowedEnvs = ['production', 'development', 'test']
-
-function ensureNodeEnv(expectedEnv) {
-  if (!allowedEnvs.includes(expectedEnv)) {
-    throw new Error(
-      `Unexpected env: ${expectedEnv} Has to be one of: [${allowedEnvs.join(
-        ', '
-      )}]`
-    )
-  }
-
-  if (expectedEnv !== readKey('NODE_ENV')) {
-    throw new Error(
-      `expectedEnv: ${expectedEnv} !== readKey('NODE_ENV'): ${readKey(
-        'NODE_ENV'
-      )} `
-    )
-  }
-
-  if (expectedEnv !== process.env.NODE_ENV) {
-    throw new Error(
-      `expectedEnv: ${expectedEnv} !== process.env.NODE_ENV: ${
-        process.env.NODE_ENV
-      } `
-    )
-  }
 }
 
 module.exports = {
