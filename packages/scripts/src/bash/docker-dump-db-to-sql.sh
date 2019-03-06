d136d9de17dab7ba0069f348ace884a7eab227e0
antonoberg <oberg.anton@gmail.com>
2019-03-06 17:14:15 +0100

    [SCRIPTS] Improve docker-dump-db-to-sql.sh
    Allow specifying target file

--- packages/scripts/src/bash/docker-dump-db-to-sql.sh
+++ packages/scripts/src/bash/docker-dump-db-to-sql.sh
@@ -1,19 +1,26 @@
 source ./env/.backend
 
 DB_NAME=$DB_DATABASE
+TARGET="${TARGET:-./data/dump.sql}"
 
-while getopts d: option
+while getopts d:f: option
  do
   case "${option}"
    in
     d) DB_NAME=${OPTARG};;
   esac
+  case "${option}"
+   in
+    f) TARGET=${OPTARG};
+  esac
  done
 
-echo "Exporting db with name: $DB_NAME"
 
-rm -f ./data/dump.sql
-docker exec -e PGPASSWORD=$DB_PASSWORD -i postgres pg_dump --clean -c -U $DB_USERNAME -d $DB_NAME -h $DB_URL > ./data/dump.sql
-chmod 777 ./data/dump.sql
+
+echo "Exporting db with name: $DB_NAME to $TARGET"
+
+rm -f $TARGET
+docker exec -e PGPASSWORD=$DB_PASSWORD -i postgres pg_dump --clean -c -U $DB_USERNAME -d $DB_NAME -h $DB_URL > $TARGET
+chmod 777 $TARGET
 
 echo "Exported db success"
