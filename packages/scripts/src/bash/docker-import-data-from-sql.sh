bbaa26a77093a074b95df3af4f03b25a4b46d288
antonoberg <oberg.anton@gmail.com>
2019-03-06 17:13:47 +0100

    [SCRIPTS] Improve docker-import-data-from-sql.sh
    Allow for specifying DB_NAME and SOURCE file with flags

--- packages/scripts/src/bash/docker-import-data-from-sql.sh
+++ packages/scripts/src/bash/docker-import-data-from-sql.sh
@@ -1,4 +1,25 @@
 source ./env/.backend
 
-cat ./data/dump.sql | docker exec -e PGPASSWORD=$DB_PASSWORD -i postgres psql -w -U $DB_USERNAME -d dina_dev -h $DB_URL && exit 0
+SOURCE="${SOURCE:-./data/dump.sql}"
+DB_NAME="${DB_NAME:-$DB_DATABASE}"
+
+while getopts d:f: option
+ do
+  case "${option}"
+   in
+    d) DB_NAME=${OPTARG};
+  esac
+  case "${option}"
+   in
+    f) SOURCE=${OPTARG};
+  esac
+ done
+
+
+if [[ "$DB_NAME" != "dina_dev" && "$DB_NAME" != "dina_test" ]]; then
+  echo "Aborting. Not allowed to import into db $DB_NAME"
+  exit 1
+fi
+
+cat $SOURCE | docker exec -e PGPASSWORD=$DB_PASSWORD -i postgres psql -w -U $DB_USERNAME -d $DB_NAME -h $DB_URL && exit 0
 
