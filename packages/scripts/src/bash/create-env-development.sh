26b8efd2bcb09bbaac7c195f7d728dcc56b336cf
antonoberg <oberg.anton@gmail.com>
2019-03-11 13:09:46 +0100

    [SCRIPTS] Adapt create-env scripts to new structure
    Env files scoped in env folder

--- packages/scripts/src/bash/create-env-development.sh
+++ packages/scripts/src/bash/create-env-development.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+FULL_PATH=$(dirname "$0")
+START_DIRECTORY=$PWD
 
-cp -n ./env/dev.backend ./env/.backend
-cp -n ./env/dev.ui ./env/.ui
+cd ./env/sample
+for f in *; do
+  if [[ $f == *".dev"* ]]; then
+    TARGET_NAME=".${f/.dev/}"
+    if [ ! -f "../${TARGET_NAME}" ]; then
+      echo "Copying file ./env/sample/$f to ./env/$TARGET_NAME"
+      cp -n ${f} "../${TARGET_NAME}"
+    else
+      echo "Not replacing existing file ./env/$TARGET_NAME"
+    fi
 
-echo $PWD
-cd ./env && for f in sample.*; do cp -n ${f} ${f/sample/}; done
+  fi
+done
+
+cd $START_DIRECTORY
+
+$FULL_PATH/create-env.sh
 
 exit 0
--- packages/scripts/src/bash/create-env.sh
+++ packages/scripts/src/bash/create-env.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-echo $PWD
-cd ./env && for f in sample.*; do cp -n ${f} ${f/sample/}; done
+cd ./env/sample
+for f in *; do
+  if [[ $f == *".dev"* ]]; then
+    echo "Not copying file: $f. (dev specific)"
+  else
+    if [ ! -f "../.${f}" ]; then
+      echo "Copying file ./env/sample/$f to ./env/.$f"
+      cp -n ${f} "../.${f}"
+    else
+      echo "Not replacing existing file ./env/.$f"
+    fi
+  fi
+done
 exit 0
