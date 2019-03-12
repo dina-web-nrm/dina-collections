9afda43b41709c2b31f7bd917418882a8c40c7d5
Fredrik Olovsson <fogithub@gmail.com>
2019-03-12 16:12:40 +0100

    [UI] Add tests for app Start page

---
+++ packages/ui/cypress/e2e/app/index.spec.js
@@ -0,0 +1,40 @@
+describe('App', () => {
+  describe('without login', () => {
+    it('redirect to login', () => {
+      cy.visit('/app')
+        .url()
+        .should('include', '/login')
+    })
+  })
+
+  describe('with login', () => {
+    before(() => {
+      cy.login()
+      cy.visit('/app')
+    })
+
+    it('has header and links', () => {
+      cy.getByText('Welcome Test Test! What would you like to do?').should(
+        'exist'
+      )
+
+      cy.getByTestId('start-register-mammal').shouldHaveHref(
+        '/app/specimens/mammals/create/sections/0'
+      )
+
+      cy.getByTestId('start-find-mammals').shouldHaveHref(
+        '/app/specimens/mammals'
+      )
+
+      cy.getByTestId('start-support-link').shouldHaveHref(
+        'mailto:support-dina@nrm.se'
+      )
+
+      cy.getByTestId('start-changelog-link')
+        .shouldHaveHref(
+          'https://github.com/DINA-Web/dina-collections/blob/master/CHANGELOG.md'
+        )
+        .should('have.attr', 'target', '_blank')
+    })
+  })
+})
