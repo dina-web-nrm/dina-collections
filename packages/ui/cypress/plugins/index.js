0c699a36231f36d1d6a406ccc9195ea430c419f2
Fredrik Olovsson <fogithub@gmail.com>
2019-03-12 16:09:47 +0100

    [UI] Add comment w. webpack config for cypress
    If necessary in the future to import code from ui/src to cypress tests

--- packages/ui/cypress/plugins/index.js
+++ packages/ui/cypress/plugins/index.js
@@ -15,3 +15,38 @@
 // `on` is used to hook into various events Cypress emits
 // `config` is the resolved Cypress config
 // }
+
+/*
+* This makes it possible to import code from src, but it adds transpilation time
+*
+const path = require('path')
+const webpack = require('@cypress/webpack-preprocessor')
+
+module.exports = on => {
+  const options = webpack.defaultOptions
+  options.webpackOptions.module.rules[0].use[0].options.plugins = [
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-transform-modules-commonjs',
+    '@babel/plugin-transform-runtime',
+  ]
+
+  options.webpackOptions.module.rules[0].use[0].options.presets = [
+    '@babel/preset-env',
+    '@babel/preset-react',
+  ]
+
+  options.webpackOptions.resolve = {
+    alias: {
+      apps: path.resolve(__dirname, '../../src/apps'),
+      config: path.resolve(__dirname, '../../src/config'),
+      coreModules: path.resolve(__dirname, '../../src/coreModules'),
+      domainModules: path.resolve(__dirname, '../../src/domainModules'),
+      scripts: path.resolve(__dirname, '../../src/scripts'),
+      store: path.resolve(__dirname, '../../src/store'),
+      utilities: path.resolve(__dirname, '../../src/utilities'),
+    },
+  }
+
+  on('file:preprocessor', webpack(options))
+}
+*/
