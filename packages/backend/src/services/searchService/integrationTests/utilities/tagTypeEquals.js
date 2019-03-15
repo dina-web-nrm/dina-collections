e67751260dfeaec5a2bb95f4c81c4d37f6e06c33
antonoberg <oberg.anton@gmail.com>
2019-03-15 14:04:03 +0100

    [BACKEND] Remove old test runner code

--- packages/backend/src/services/searchService/integrationTests/utilities/tagTypeEquals.js
+++
@@ -1,8 +0,0 @@
-module.exports = function tagTypeEquals(expectedType) {
-  return res => {
-    res.data.forEach(item => {
-      expect(item.attributes.tagType).toBe(expectedType)
-    })
-    return res
-  }
-}
