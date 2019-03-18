179022a077e2db63657b5bfd8cc7216d3a0ad7bf
Fredrik Olovsson <fogithub@gmail.com>
2019-03-18 15:59:42 +0100

    [UI] Activate full scrolling test

--- packages/ui/cypress/e2e/app/specimens/table.js
+++ packages/ui/cypress/e2e/app/specimens/table.js
@@ -19,37 +19,32 @@ export default () =>
         cy.get('[data-testid="resultTableScrollContainer"]')
           .as('table')
           .scrollTo(0, 2000, { duration: 500 })
-        cy.get('[data-testid="InfiniteTable"]')
-          .find('[data-testid="InfiniteTableRow-16"]', {
+        cy.get(
+          '[data-testid="InfiniteTable"] [data-testid="InfiniteTableRow-16"]',
+          {
             timeout: 20000,
-          })
-          .should('contain', '500001')
+          }
+        ).should('contain', '500001')
 
-        // cy.log('Sort and scroll to top and check 500001 is now on top')
-        // cy.get(
-        //   '[data-testid="InfiniteTableHeader-identifiersCatalogNumber"]'
-        // ).click()
-        // cy.get('@table').scrollTo('topLeft', { duration: 500 })
-        // cy.wait(5000)
-        // cy.get('[data-testid="InfiniteTable"]').find('.row:first', {
-        //   timeout: 20000,
-        // }).should(
-        //   'contain',
-        //   '50000'
-        // )
+        cy.log('Sort and scroll to top and check 500001 is now on top')
+        cy.get(
+          '[data-testid="InfiniteTableHeader-identifiersCatalogNumber"]'
+        ).click()
+        cy.get('@table').scrollTo('topLeft', { duration: 500 })
+        cy.get('[data-testid="InfiniteTable"] .row:first').should(
+          'contain',
+          '50000'
+        )
 
-        // cy.log('Sort again and check 500001 is now on bottom')
-        // cy.get(
-        //   '[data-testid="InfiniteTableHeader-identifiersCatalogNumber"]'
-        // ).click()
-        // cy.get('@table').scrollTo('bottomLeft', { duration: 500 })
-        // cy.wait(5000)
-        // cy.get('@table').scrollTo(0, 2000, { duration: 500 })
-        // cy.wait(5000)
-        // cy.get('[data-testid="InfiniteTable"] .row:last').should(
-        //   'contain',
-        //   'Mustela erminea'
-        // )
+        cy.log('Sort again and check 500001 is now on bottom')
+        cy.get(
+          '[data-testid="InfiniteTableHeader-identifiersCatalogNumber"]'
+        ).click()
+        cy.get('@table').scrollTo(0, 2000, { duration: 500 })
+        cy.get('[data-testid="InfiniteTable"] .row:last').should(
+          'contain',
+          'Mustela erminea'
+        )
       })
     })
   })
