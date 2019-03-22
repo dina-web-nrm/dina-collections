031cc040220bdcbb41fefc430ef2bf062c7fbfe6
antonoberg <oberg.anton@gmail.com>
2019-03-22 16:08:33 +0100

    [BACKEND] Add extra phrase test case

--- packages/backend/src/services/searchService/integrationTests/searchSpecimen/query/filters/taxonomyTags/tagValueAggregation/unitTests/testSpecification/phrases.js
+++ packages/backend/src/services/searchService/integrationTests/searchSpecimen/query/filters/taxonomyTags/tagValueAggregation/unitTests/testSpecification/phrases.js
@@ -2,6 +2,7 @@ module.exports = [
   {
     input: '"pusa hispida"',
     matching: ['pusa hispida'],
+    matchingNotInSampleData: ['pusa hispida test'],
     notMatching: ['pusa', 'hispida pusa'],
   },
   {
