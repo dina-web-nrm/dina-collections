ee3f601d0959e756698d7544bc3c1516b3755fbc
Fredrik Olovsson <fogithub@gmail.com>
2019-03-26 23:14:11 +0100

    [SCRIPTS] Add ci-delete-temporary-images

---
+++ packages/scripts/src/bash/ci-delete-temporary-images.sh
@@ -0,0 +1,17 @@
+set -v
+
+if [ -z "$CI" ]; then
+  echo "CI is empty, Abort. Only allowed to publish from CI" ;
+  exit 0
+fi
+
+DOCKER_HUB_TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'$DOCKER_USERNAME'", "password": "'$DOCKER_PASSWORD'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
+
+imageNames=("dina-collections-ui" "dina-collections-api" "dina-collections-migrations" "dina-collections-docs" "dina-semantic-ui")
+
+for imageName in "${imageNames[@]}"; do
+  echo -e "\n\n\nDeleting docker image $imageName:$TRAVIS_BUILD_NUMBER"
+  curl -X DELETE -s -i -H "Authorization: JWT ${DOCKER_HUB_TOKEN}" https://hub.docker.com/v2/repositories/dina/$imageName/tags/$TRAVIS_BUILD_NUMBER/
+done
+
+exit 0
