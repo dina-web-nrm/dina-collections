54136141118cbf5813fb9ffa20813aa9adb39d77
Fredrik Olovsson <fogithub@gmail.com>
2019-03-28 21:33:44 +0100

    [SCRIPTS] Cleanup in ci-before-script

--- packages/scripts/src/bash/ci-before-script.sh
+++ packages/scripts/src/bash/ci-before-script.sh
@@ -4,44 +4,45 @@ set -v
 echo "$(date +'%T') start ci-before-script"
 START_DIRECTORY=$PWD
 
-if [ "$CI_SETUP_ENV_DOCKER" = true ]; then
-  yarn setup:env:ci:docker
-  yarn setup:env
-else
-  yarn setup:env:ci:local
-fi
-
 if [ "$CI_START_API" = true ]; then
   : "${CI_LINK_MIGRATIONS?CI_LINK_MIGRATIONS Has to be true}"
-  echo "Starting databases"
+  echo "Setting up env"
+  yarn setup:env:ci:local
+
+  echo "Stopping Travis postgresql"
   sudo /etc/init.d/postgresql stop
+
+  echo "Starting databases"
   yarn start:postgres && yarn start:elasticsearch
   cd ./packages/migrations && yarn db:test:create
-  cd $START_DIRECTORY
 
-  echo "Setting up sample data"
-  yarn setup:sample-data && cd ./packages/migrations && yarn setup:development
+  echo "Setting up & importing sample data"
   cd $START_DIRECTORY
+  yarn setup:sample-data && cd ./packages/migrations && yarn setup:development
 
   echo "Starting API"
+  cd $START_DIRECTORY
   cd ./packages/backend && yarn start:node &
+
   echo "API started"
   cd $START_DIRECTORY
 fi
 
 if [ "$CI_START_E2E" = true ]; then
-  : "${CI_SETUP_ENV_DOCKER?CI_SETUP_ENV_DOCKER Has to be true}"
-  docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD";
+  echo "Setting up env"
+  yarn setup:env:ci:docker
 
   echo "Stopping Travis postgresql"
   sudo /etc/init.d/postgresql stop
 
+  echo "Setting up sample data"
+  yarn setup:sample-data
+
+  echo "Pulling docker images"
+  docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD";
   docker pull dina/dina-collections-ui:$TRAVIS_BUILD_NUMBER
   docker pull dina/dina-collections-api:$TRAVIS_BUILD_NUMBER
   docker pull dina/dina-collections-migrations:$TRAVIS_BUILD_NUMBER
-
-  echo "Setting up sample data"
-  yarn setup:sample-data
 
   echo "Starting databases and keycloak"
   docker-compose -f docker-compose.yaml -f docker-compose.ci.yaml up -d elasticsearch keycloak mysql postgres
@@ -50,7 +51,7 @@ if [ "$CI_START_E2E" = true ]; then
   echo "Importing sample data"
   TAG=$TRAVIS_BUILD_NUMBER docker-compose -f docker-compose.data.yaml -f docker-compose.data.ci.yaml up import
 
-  echo "Starting application and worker containers"
+  echo "Starting ui, api and workers"
   TAG=$TRAVIS_BUILD_NUMBER docker-compose -f docker-compose.yaml -f docker-compose.ci.yaml up -d api ui baseWorker searchIndexWorker
 
   if [ $? -ne 0 ]; then
