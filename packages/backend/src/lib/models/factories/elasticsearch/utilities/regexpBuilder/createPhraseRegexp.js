c656794c10e8eb9e5b2101640317e849d86ad15a
antonoberg <oberg.anton@gmail.com>
2019-03-29 11:40:58 +0100

    [BACKEND] Fix phrase behaviour
    Now don't require phrases containing whole words

--- packages/backend/src/lib/models/factories/elasticsearch/utilities/regexpBuilder/createPhraseRegexp.js
+++ packages/backend/src/lib/models/factories/elasticsearch/utilities/regexpBuilder/createPhraseRegexp.js
@@ -1,16 +1,13 @@
 const extractFlags = require('./extractFlags')
 const interpretStar = require('./interpretStar')
 
-const { MATCH_ANY, WORD_DELIMITER } = require('./constants')
+const { MATCH_ANY } = require('./constants')
 
 module.exports = function createPhraseRegexp(input) {
   const { hasStar } = extractFlags(input)
   let str = input.slice(1, -1)
   if (hasStar) {
     str = interpretStar(str)
-  } else {
-    str = [WORD_DELIMITER, str, WORD_DELIMITER].join('')
   }
-
   return [MATCH_ANY, str, MATCH_ANY].join('')
 }
