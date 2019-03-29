4aa054437cca8930c612841a5fd2a4c4c8649592
antonoberg <oberg.anton@gmail.com>
2019-03-29 12:34:54 +0100

    [BACKEND] Added more test cases for identifiers

--- packages/backend/src/services/searchService/integrationTests/searchSpecimen/query/filters/identifierTags/tagValueAggregation/unitTests/testSpecification/specialCharacters.js
+++ packages/backend/src/services/searchService/integrationTests/searchSpecimen/query/filters/identifierTags/tagValueAggregation/unitTests/testSpecification/specialCharacters.js
@@ -9,4 +9,38 @@ module.exports = [
     matching: ['v0253/98'],
     notMatching: ['v0253 98'],
   },
+  {
+    input: '1;',
+    matching: ['1; 4406', '1; 4406; 52'],
+  },
+  {
+    input: '*-*',
+    matching: ['2012-21'],
+  },
+  {
+    input: '*/*',
+    matching: ['v0253/98', 'v0615/98'],
+  },
+  {
+    input: '*,*',
+    matching: ['1,285', 'bly 45, 1008, 3124', '1,285'],
+  },
+  {
+    input: '" "',
+    matching: ['1; 4406', 'bly 45, 1008, 3124', '1; 4406; 52'],
+    notMatching: ['a'],
+  },
+  {
+    input: ' ',
+    notMatching: ['a'],
+  },
+
+  {
+    input: '*?*',
+    matchingNotInSampleData: ['a; ?a'],
+  },
+  {
+    input: '*.*',
+    matchingNotInSampleData: ['an.de '],
+  },
 ]
