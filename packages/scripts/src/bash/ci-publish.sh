3d96f284b08057e0aabf57d8e38e06ac551e6b94
Fredrik Olovsson <fogithub@gmail.com>
2019-04-01 13:39:54 +0200

    [SCRIPTS] Use for loop in ci-publish

--- packages/scripts/src/bash/ci-publish.sh
+++ packages/scripts/src/bash/ci-publish.sh
@@ -14,49 +14,14 @@ fi
 
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD";
 
-echo "Pushing dina/dina-collections-ui:$TRAVIS_TAG"
-docker pull dina/dina-collections-ui:$TRAVIS_BUILD_NUMBER
-docker tag dina/dina-collections-ui:$TRAVIS_BUILD_NUMBER dina/dina-collections-ui:$TRAVIS_TAG
-docker push dina/dina-collections-ui:$TRAVIS_TAG
+imageNames=("dina-collections-ui" "dina-collections-api" "dina-collections-migrations" "dina-collections-docs" "dina-semantic-ui")
 
-echo "Pushing dina/dina-collections-ui:latest"
-docker tag dina/dina-collections-ui:$TRAVIS_BUILD_NUMBER dina/dina-collections-ui:latest
-docker push dina/dina-collections-ui:latest
+for imageName in "${imageNames[@]}"; do
+  echo "Pushing $imageName:$TRAVIS_TAG"
+  docker pull $imageName:$TRAVIS_BUILD_NUMBER
+  docker tag $imageName:$TRAVIS_BUILD_NUMBER $imageName:$TRAVIS_TAG
+  docker push $imageName:$TRAVIS_TAG
 
-echo "Pushing dina/dina-collections-api:$TRAVIS_TAG"
-docker pull dina/dina-collections-api:$TRAVIS_BUILD_NUMBER
-docker tag dina/dina-collections-api:$TRAVIS_BUILD_NUMBER dina/dina-collections-api:$TRAVIS_TAG
-docker push dina/dina-collections-api:$TRAVIS_TAG
-
-echo "Publishing dina/dina-collections-api:latest"
-docker tag dina/dina-collections-api:$TRAVIS_BUILD_NUMBER dina/dina-collections-api:latest
-docker push dina/dina-collections-api:latest
-
-echo "Pushing dina/dina-collections-migrations:$TRAVIS_TAG"
-docker pull dina/dina-collections-migrations:$TRAVIS_BUILD_NUMBER
-docker tag dina/dina-collections-migrations:$TRAVIS_BUILD_NUMBER dina/dina-collections-migrations:$TRAVIS_TAG
-docker push dina/dina-collections-migrations:$TRAVIS_TAG
-
-echo "Publishing dina/dina-collections-migrations:latest"
-docker tag dina/dina-collections-migrations:$TRAVIS_BUILD_NUMBER dina/dina-collections-migrations:latest
-docker push dina/dina-collections-migrations:latest
-
-echo "Pushing dina/dina-collections-docs:$TRAVIS_TAG"
-docker pull dina/dina-collections-docs:$TRAVIS_BUILD_NUMBER
-docker tag dina/dina-collections-docs:$TRAVIS_BUILD_NUMBER dina/dina-collections-docs:$TRAVIS_TAG
-docker push dina/dina-collections-docs:$TRAVIS_TAG
-
-echo "Publishing dina/dina-collections-docs:latest"
-docker tag dina/dina-collections-docs:$TRAVIS_BUILD_NUMBER dina/dina-collections-docs:latest
-docker push dina/dina-collections-docs:latest
-
-echo "Pushing dina/dina-semantic-ui-docs:$TRAVIS_TAG"
-docker pull dina/dina-semantic-ui-docs:$TRAVIS_BUILD_NUMBER
-docker tag dina/dina-semantic-ui-docs:$TRAVIS_BUILD_NUMBER dina/dina-semantic-ui-docs:$TRAVIS_TAG
-docker push dina/dina-semantic-ui-docs:$TRAVIS_TAG
-
-echo "Publishing dina/dina-semantic-ui-docs:latest"
-docker tag dina/dina-semantic-ui-docs:$TRAVIS_BUILD_NUMBER dina/dina-semantic-ui-docs:latest
-docker push dina/dina-semantic-ui-docs:latest
+done
 
 echo "$(date +'%T') end ci-publish"
