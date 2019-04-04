050cb947cc37674854f1110ae9142e401929359a
antonoberg <oberg.anton@gmail.com>
2019-04-04 14:45:04 +0200

    [BACKEND] Remove createJob

--- packages/backend/src/lib/controllers/factories/jobs/index.js
+++ packages/backend/src/lib/controllers/factories/jobs/index.js
@@ -1,4 +1,3 @@
-exports.createJob = require('./createJob')
 exports.setJobFailed = require('./setJobFailed')
 exports.setJobSuccess = require('./setJobSuccess')
 exports.startJob = require('./startJob')
