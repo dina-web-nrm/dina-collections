fff635126f9e942acb3345283027ce4ee19e9414
Fredrik Olovsson <fogithub@gmail.com>
2019-04-05 13:15:06 +0200

    [SHARED] Fix ui external url env var reading

--- packages/ui/src/config/index.js
+++ packages/ui/src/config/index.js
@@ -3,7 +3,12 @@ import createHistory from 'history/createBrowserHistory'
 import createEnvReader from 'common/es5/env/createEnvReader'
 import envDefinitions from './envDefinitions'
 
-const { readKey, readBoolKey } = createEnvReader({
+const {
+  readBoolKey,
+  readKey,
+  readWindowBoolKey,
+  readWindowKey,
+} = createEnvReader({
   envDefinitions,
   processEnv: process.env,
 })
@@ -13,9 +18,10 @@ const isDevelopment = env === 'development'
 const isProduction = env === 'production'
 const isTest = env === 'test'
 
-const disableAuth =
-  window.REACT_APP_DISABLE_AUTH === 'true' ||
-  readBoolKey('REACT_APP_DISABLE_AUTH')
+const disableAuth = readWindowBoolKey(
+  'REACT_APP_DISABLE_AUTH',
+  readKey('REACT_APP_DISABLE_AUTH')
+)
 
 const history = createHistory()
 
@@ -30,14 +36,22 @@ const config = {
   devToolsExtension: isDevelopment && typeof devToolsExtension === 'function',
   env,
   externalUrls: {
-    api: readKey('REACT_APP_EXTERNAL_URL_API', 'https://dina-api.nrm.se'),
-    docs: readKey('REACT_APP_EXTERNAL_URL_DOCS', 'https://dina-docs.nrm.se'),
+    api: readWindowKey(
+      'REACT_APP_EXTERNAL_URL_API',
+      readKey('REACT_APP_EXTERNAL_URL_API', 'https://dina-api.nrm.se')
+    ),
+    docs: readWindowKey(
+      'REACT_APP_EXTERNAL_URL_DOCS',
+      readKey('REACT_APP_EXTERNAL_URL_DOCS', 'https://dina-docs.nrm.se')
+    ),
     githubDina: 'https://github.com/DINA-Web',
     githubRepo: 'https://github.com/DINA-Web/dina-collections',
-    style: readKey('REACT_APP_EXTERNAL_URL_STYLE', 'https://dina-style.nrm.se'),
     wiki: readKey(
       'REACT_APP_EXTERNAL_URL_DINA_WIKI',
       'https://www.dina-project.net/wiki/Welcome_to_the_DINA_project!'
+    style: readWindowKey(
+      'REACT_APP_EXTERNAL_URL_STYLE',
+      readKey('REACT_APP_EXTERNAL_URL_STYLE', 'https://dina-style.nrm.se')
     ),
   },
   isDevelopment,
