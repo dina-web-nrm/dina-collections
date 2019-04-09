864551bb0e42fa72c782ea4a13b1251bae5a9b9e
Fredrik Olovsson <fogithub@gmail.com>
2019-04-09 16:54:14 +0200

    [UI] Move Footer to viewModules/components
    Because it is app-specific

--- packages/ui/src/coreModules/commonUi/components/index.js
+++ packages/ui/src/coreModules/commonUi/components/index.js
@@ -1,7 +1,6 @@
 export { default as Accordion } from './Accordion'
 export { default as ColumnRowHeader } from './ColumnRowHeader'
 export { default as DateString } from './DateString'
-export { default as Footer } from './Footer'
 export { default as LayerModal } from './LayerModal'
 export { default as PageTemplate } from './PageTemplate'
 export { default as ThreeColumnGrid } from './ThreeColumnGrid'
