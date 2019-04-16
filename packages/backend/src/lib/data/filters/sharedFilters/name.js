7f4d8a7c5a38d1d2c253229b452514a9bf162550
Fredrik Olovsson <fogithub@gmail.com>
2019-04-16 10:45:43 +0200

    [BACKEND] Fix editorial typo

--- packages/backend/src/lib/data/filters/sharedFilters/name.js
+++ packages/backend/src/lib/data/filters/sharedFilters/name.js
@@ -1,11 +1,11 @@
-const nameFiterFunction = require('common/src/search/resources/shared/filterFunctions/name')
+const nameFilterFunction = require('common/src/search/resources/shared/filterFunctions/name')
 
 module.exports = {
   description: 'Filter by name match',
   inputSchema: {
     type: 'string',
   },
-  jsFilterFunction: nameFiterFunction,
+  jsFilterFunction: nameFilterFunction,
   key: 'name',
   sequelizeFilterFunction: ({ value }) => {
     if (value === undefined) {
