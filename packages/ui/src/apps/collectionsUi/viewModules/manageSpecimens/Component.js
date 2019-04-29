63994ad75ffc4e1fb7a6d8daef2a443401f74b7a
Fredrik Olovsson <fogithub@gmail.com>
2019-04-29 16:58:17 +0200

    [UI] Add manageSpecimens viewModule

---
+++ packages/ui/src/apps/collectionsUi/viewModules/manageSpecimens/Component.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { SpecimenManager } from 'serviceModules/specimen/components'
+
+const ManageSpecimens = () => <SpecimenManager />
+
+export default ManageSpecimens
