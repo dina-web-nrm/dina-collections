0c0965bb0b29f22029c8fbe091e366eb56b19a24
Fredrik Olovsson <fogithub@gmail.com>
2019-05-07 15:16:02 +0200

    WIP: Backup e2e tests for MammalManager

---
+++ packages/ui/cypress/e2e/app/specimensOld/write/remove.js
@@ -0,0 +1,27 @@
+export default () =>
+  describe('delete', () => {
+    before(() => {
+      cy.resetDevelopmentSqlDb()
+      cy.resetElasticSpecimenIndex()
+    })
+
+    beforeEach(() => {
+      cy.visit('/app/specimens/mammals/search')
+      cy.get('[data-testid="infinityTableHeader"]', {
+        log: false,
+        timeout: 60000,
+      })
+    })
+
+    it('deletes record', () => {
+      cy.getByText('985729').click()
+      cy.getByTestId('formTabMenuItem').click()
+      cy.getByTestId('deleteButton').click()
+      cy.getByTestId('confirmDeleteButton').click()
+      cy.getByText('The specimen was deleted')
+      cy.url()
+        .should('include', 'search')
+        .should('not.include', 'edit')
+      cy.quickQueryByText('985729').should('not.exist')
+    })
+  })
