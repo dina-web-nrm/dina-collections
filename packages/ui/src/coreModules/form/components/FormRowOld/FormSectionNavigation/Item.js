1d31416410cd4755ff0e5b2232d204eea5c2c741
Fredrik Olovsson <fogithub@gmail.com>
2019-05-07 15:14:59 +0200

    WIP: Backup Form and FormRow before refactor
    So MammalManager will continue to work while building the new
    SpecimenManager that will use the resourceManager

---
+++ packages/ui/src/coreModules/form/components/FormRowOld/FormSectionNavigation/Item.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { Step } from 'semantic-ui-react'
+
+import config from 'config'
+import formSupportSelectors from 'coreModules/formSupport/globalSelectors'
+import { emToPixels } from 'coreModules/layout/utilities'
+import { ModuleTranslate } from 'coreModules/i18n/components'
+
+const mapStateToProps = (
+  state,
+  { activeFormSectionIndex, formName, index, sectionName }
+) => {
+  if (config.isTest) {
+    return {
+      sectionIsInvalid: false,
+    }
+  }
+
+  const isActive = index === activeFormSectionIndex
+
+  return {
+    sectionIsInvalid: isActive
+      ? formSupportSelectors.computeSectionIsInvalid(state, {
+          formName,
+          sectionName,
+        })
+      : formSupportSelectors.getSectionIsInvalid(state, {
+          formName,
+          sectionName,
+        }),
+  }
+}
+
+const propTypes = {
+  activeFormSectionIndex: PropTypes.number,
+  index: PropTypes.number.isRequired,
+  module: PropTypes.string.isRequired,
+  onSetActiveFormSection: PropTypes.func.isRequired,
+  sectionIsInvalid: PropTypes.bool.isRequired,
+  sectionName: PropTypes.string.isRequired,
+}
+const defaultProps = {
+  activeFormSectionIndex: undefined,
+}
+
+class FormSectionNavigationItem extends Component {
+  render() {
+    const {
+      activeFormSectionIndex,
+      index,
+      module,
+      onSetActiveFormSection: handleSetActiveFormSection,
+      sectionName,
+      sectionIsInvalid,
+    } = this.props
+
+    return (
+      <Step
+        active={index === activeFormSectionIndex}
+        className={sectionIsInvalid ? 'error' : undefined}
+        data-testid={`formSectionNavigationItem-${sectionName}`}
+        key={index}
+        onClick={event => handleSetActiveFormSection(event, index)}
+        style={{
+          width: emToPixels(21.875),
+        }}
+      >
+        <Step.Content>
+          <Step.Title>
+            <ModuleTranslate
+              capitalize
+              module={module}
+              textKey={`formSectionTitles.${sectionName}`}
+            />
+          </Step.Title>
+          <Step.Description>
+            <ModuleTranslate
+              capitalize
+              module={module}
+              textKey={`formSectionDescriptions.${sectionName}`}
+            />
+          </Step.Description>
+        </Step.Content>
+      </Step>
+    )
+  }
+}
+
+FormSectionNavigationItem.propTypes = propTypes
+FormSectionNavigationItem.defaultProps = defaultProps
+
+export default compose(connect(mapStateToProps))(FormSectionNavigationItem)
---
+++ packages/ui/src/coreModules/form/components/FormRowOld/index.js
@@ -0,0 +1,262 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import { push } from 'react-router-redux'
+import objectPath from 'object-path'
+import { validateSections } from 'coreModules/formSupport/actionCreators'
+import createLog from 'utilities/log'
+import extractProps from 'utilities/extractProps'
+import {
+  actionCreators as formSupportKeyObjectActionCreators,
+  globalSelectors as formSupportKeyObjectSelectors,
+} from 'coreModules/formSupport/keyObjectModule'
+import { ColumnLayout } from 'coreModules/layout/components'
+import { emToPixels } from 'coreModules/layout/utilities'
+import FormSectionNavigation, {
+  propTypes as formSectionNavigationPropTypes,
+} from './FormSectionNavigation'
+import FormSectionView, {
+  propTypes as formSectionViewPropTypes,
+} from './FormSectionView'
+
+const log = createLog('coreModules/form/components/FormRow')
+
+const columns = [
+  { key: 'formSectionNavigation', width: emToPixels(25) },
+  { key: 'formSectionView', style: { overflow: 'auto' } },
+]
+
+const mapStateToProps = (state, { formName, match }) => {
+  return {
+    activeFormSectionIndex: formSupportKeyObjectSelectors.get[
+      'sectionNavigation.:formName.activeFormSectionIndex'
+    ](state, { formName }),
+    sectionId: objectPath.get(match, 'params.sectionId'),
+    showAllFormSections: formSupportKeyObjectSelectors.get[
+      'sectionNavigation.:formName.showAllFormSections'
+    ](state, {
+      formName,
+    }),
+  }
+}
+const mapDispatchToProps = {
+  push,
+  setActiveFormSectionIndex:
+    formSupportKeyObjectActionCreators.set[
+      'sectionNavigation.:formName.activeFormSectionIndex'
+    ],
+  setShowAllFormSections:
+    formSupportKeyObjectActionCreators.set[
+      'sectionNavigation.:formName.showAllFormSections'
+    ],
+  validateSections,
+}
+
+const propTypes = {
+  activeFormSectionIndex: PropTypes.number,
+  customParts: PropTypes.objectOf(PropTypes.func.isRequired),
+  formName: PropTypes.string.isRequired,
+  itemHeader: PropTypes.node.isRequired,
+  itemSubHeader: PropTypes.node,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      specimenId: PropTypes.string,
+    }).isRequired,
+    path: PropTypes.string.isRequired,
+  }).isRequired,
+  moduleName: PropTypes.string.isRequired,
+  passthroughProps: PropTypes.arrayOf(PropTypes.string.isRequired),
+  push: PropTypes.func.isRequired,
+  resourceIdPathParamKey: PropTypes.string,
+  sectionId: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  sectionSpecs: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      units: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+    }).isRequired
+  ).isRequired,
+  setActiveFormSectionIndex: PropTypes.func.isRequired,
+  setShowAllFormSections: PropTypes.func.isRequired,
+  showAllFormSections: PropTypes.bool,
+  showSectionsInNavigation: PropTypes.bool,
+  validateSections: PropTypes.func.isRequired,
+}
+const defaultProps = {
+  activeFormSectionIndex: undefined,
+  customParts: undefined,
+  itemSubHeader: undefined,
+  passthroughProps: ['resourceActivities'],
+  resourceIdPathParamKey: 'itemId',
+  sectionId: undefined,
+  showAllFormSections: undefined,
+  showSectionsInNavigation: false,
+}
+
+class FormRow extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.handleSectionIdUpdate = this.handleSectionIdUpdate.bind(this)
+    this.handleSetActiveFormSection = this.handleSetActiveFormSection.bind(this)
+    this.handleGoToNextSection = this.handleGoToNextSection.bind(this)
+    this.handleGoToPreviousSection = this.handleGoToPreviousSection.bind(this)
+    this.handleShowAllFormSections = this.handleShowAllFormSections.bind(this)
+    this.renderColumn = this.renderColumn.bind(this)
+  }
+
+  componentWillMount() {
+    this.handleSectionIdUpdate()
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.sectionId !== nextProps.sectionId) {
+      this.handleSectionIdUpdate(nextProps)
+    }
+  }
+
+  handleSectionIdUpdate(props = this.props) {
+    const { formName, sectionId } = props
+    const sectionIndex = Number(sectionId)
+
+    if (Number.isInteger(sectionIndex)) {
+      this.props.setActiveFormSectionIndex(sectionIndex, { formName })
+      this.props.setShowAllFormSections(false, { formName })
+    } else if (sectionId === 'all') {
+      this.props.setActiveFormSectionIndex(null, { formName })
+      this.props.setShowAllFormSections(true, { formName })
+    }
+
+    setTimeout(() => this.props.validateSections({ formName: props.formName }))
+  }
+
+  handleSetActiveFormSection(event, activeFormSectionIndex) {
+    if (event) event.preventDefault()
+
+    const { resourceIdPathParamKey } = this.props
+    const { path, params } = this.props.match
+
+    const url = path
+      .replace(`:${resourceIdPathParamKey}`, params[resourceIdPathParamKey])
+      .replace(':sectionId', activeFormSectionIndex)
+
+    this.props.push(url)
+  }
+
+  handleGoToNextSection(event) {
+    const { activeFormSectionIndex, sectionSpecs } = this.props
+
+    if (activeFormSectionIndex < sectionSpecs.length - 1) {
+      this.handleSetActiveFormSection(event, activeFormSectionIndex + 1)
+    }
+  }
+
+  handleGoToPreviousSection(event) {
+    const { activeFormSectionIndex } = this.props
+
+    if (activeFormSectionIndex > 0) {
+      this.handleSetActiveFormSection(event, activeFormSectionIndex - 1)
+    }
+  }
+
+  handleShowAllFormSections(event) {
+    this.handleSetActiveFormSection(event, 'all')
+  }
+
+  renderColumn(key, props) {
+    switch (key) {
+      case 'formSectionNavigation': {
+        const { extractedProps } = extractProps({
+          keys: [
+            ...Object.keys(formSectionNavigationPropTypes),
+            ...this.props.passthroughProps,
+          ],
+          props,
+        })
+
+        const { itemHeader, itemSubHeader } = this.props
+
+        return (
+          <FormSectionNavigation
+            {...extractedProps}
+            header={itemHeader}
+            subHeader={itemSubHeader}
+          />
+        )
+      }
+
+      case 'formSectionView': {
+        const { extractedProps } = extractProps({
+          keys: [
+            'itemHeader',
+            'itemSubHeader',
+            ...Object.keys(formSectionViewPropTypes),
+            ...this.props.passthroughProps,
+          ],
+          props,
+        })
+
+        return <FormSectionView {...extractedProps} />
+      }
+
+      default: {
+        throw new Error(`Unknown column: ${key}`)
+      }
+    }
+  }
+
+  render() {
+    log.render()
+    const {
+      activeFormSectionIndex,
+      customParts,
+      match,
+      moduleName,
+      sectionSpecs,
+      showAllFormSections,
+      showSectionsInNavigation,
+      ...rest
+    } = this.props
+
+    if (
+      showSectionsInNavigation &&
+      activeFormSectionIndex === undefined &&
+      showAllFormSections === undefined
+    ) {
+      return null
+    }
+
+    return (
+      <React.Fragment>
+        <ColumnLayout
+          {...rest}
+          activeFormSectionIndex={activeFormSectionIndex}
+          columns={columns}
+          customParts={customParts}
+          moduleName={moduleName}
+          onGoToNextSection={this.handleGoToNextSection}
+          onGoToPreviousSection={this.handleGoToPreviousSection}
+          onRemoteSubmit={this.handleRemoteSubmit}
+          onSetActiveFormSection={this.handleSetActiveFormSection}
+          onShowAllFormSections={this.handleShowAllFormSections}
+          renderColumn={this.renderColumn}
+          sectionSpecs={sectionSpecs}
+          showAllFormSections={showAllFormSections}
+          showSectionsInNavigation={showSectionsInNavigation}
+          specimenId={match.params.specimenId}
+        />
+      </React.Fragment>
+    )
+  }
+}
+
+FormRow.propTypes = propTypes
+FormRow.defaultProps = defaultProps
+
+export default compose(
+  withRouter,
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )
+)(FormRow)
