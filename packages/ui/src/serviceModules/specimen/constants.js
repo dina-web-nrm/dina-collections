2b84d1c7890ea25f982f84ec8de96a059e02d9dc
Fredrik Olovsson <fogithub@gmail.com>
2019-05-07 15:18:28 +0200

    [UI] Fix specimen filter form name

--- packages/ui/src/serviceModules/specimen/constants.js
+++ packages/ui/src/serviceModules/specimen/constants.js
@@ -1,6 +1,6 @@
 export const MAMMAL_FORM_NAME = 'mammalForm'
 export const MODULE_NAME = 'specimen'
-export const SPECIMEN_FILTERS_FORM_NAME = 'specimenFilters'
+export const SPECIMEN_FILTERS_FORM_NAME = 'specimenFilter'
 export const SPECIMENS_MAMMALS_TABLE_COLUMNS = 'specimensMammalsTableColumns'
 export const SPECIMENS_MAMMALS_TABLE_COLUMNS_SORTING =
   'specimensMammalsTableColumnsSorting'
