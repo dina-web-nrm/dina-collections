2a6e4c0ce22999322da5d6d903e63e192222e804
Fredrik Olovsson <fogithub@gmail.com>
2019-05-08 18:26:37 +0200

    [UI] Add tests for form section navigation

--- packages/ui/cypress/e2e/app/specimens/read/form.js
+++ packages/ui/cypress/e2e/app/specimens/read/form.js
@@ -8,7 +8,7 @@ export default () =>
       cy.get('[data-testid="basicInformation"]', { log: false, timeout: 60000 })
     })
 
-    it('has record history and help texts', () => {
+    it('has record history and help texts and form step navigation', () => {
       cy.getByText('Record history')
       cy.getByText('Created by Admin', { exact: false })
       cy.getByTestId('sourceDataLink').shouldHaveHref(
@@ -43,6 +43,35 @@ export default () =>
       })
       cy.getByTestId('inlineNotification').should('contain', 'Identifiers')
 
-      cy.getByText('Undo changes').click() // to avoid preventing browser unload
+      cy.log('has working form step navigation')
+      cy.getByTestId('formSectionNavigationItems')
+        .children()
+        .should('have.length', 6)
+      cy.getByTestId('formSectionNavigationItems')
+        .children()
+        .first()
+        .should('have.class', 'active')
+      cy.get('[data-testid=formSectionNavigationItems] .active').should(
+        'have.length',
+        1
+      )
+
+      cy.getByTestId('nextFormSectionButton').click()
+      cy.getByTestId('formSectionNavigationItems')
+        .children()
+        .first()
+        .should('not.have.class', 'active')
+        .next()
+        .should('have.class', 'active')
+      cy.get('[data-testid=formSectionNavigationItems] .active').should(
+        'have.length',
+        1
+      )
+
+      cy.getByTestId('formSectionNavigationItem-expandAllSections').click()
+      cy.get('[data-testid=formSectionNavigationItems] .active').should(
+        'have.length',
+        0
+      )
     })
   })
