e1482b576345187bc69e6df75f49cc58e1a6fa19
Fredrik Olovsson <fogithub@gmail.com>
2019-05-10 12:27:30 +0200

    [UI] Refactor RowLayout to use .Row & children

--- packages/ui/src/coreModules/layout/components/RowLayout/index.js
+++ packages/ui/src/coreModules/layout/components/RowLayout/index.js
@@ -1,107 +1,50 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-import extractProps from 'utilities/extractProps'
+import Row from './Row'
 
 const propTypes = {
   availableHeight: PropTypes.number.isRequired,
-  renderRow: PropTypes.func,
-  rows: PropTypes.arrayOf(
-    PropTypes.shape({
-      height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-      key: PropTypes.string.isRequired,
-      renderRow: PropTypes.func,
-    }).isRequired
-  ),
-  wrapperClassNames: PropTypes.string,
-  wrapperId: PropTypes.string,
-  wrapperStyle: PropTypes.object,
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  dataTestId: PropTypes.string,
+  id: PropTypes.string,
+  style: PropTypes.object,
 }
 const defaultProps = {
-  renderRow: undefined,
-  rows: undefined,
-  wrapperClassNames: undefined,
-  wrapperId: undefined,
-  wrapperStyle: undefined,
+  className: undefined,
+  dataTestId: undefined,
+  id: undefined,
+  style: undefined,
 }
 
 class RowLayout extends Component {
+  static Row = Row
+
   render() {
     const {
       availableHeight,
-      renderRow,
-      rows,
-      wrapperClassNames,
-      wrapperId,
-      wrapperStyle,
+      children,
+      className,
+      dataTestId,
+      id,
+      style,
     } = this.props
-
-    if (!rows || !rows.length) {
-      return null
-    }
-
-    const { rest: thisPropsRest } = extractProps({
-      keys: [
-        'renderRow',
-        'rows',
-        'wrapperClassNames',
-        'wrapperId',
-        'wrapperStyle',
-      ],
-      props: this.props,
-    })
 
     return (
       <div
-        className={wrapperClassNames}
-        data-testid={wrapperId}
-        id={wrapperId}
+        className={className}
+        data-testid={dataTestId}
+        id={id}
         style={{
           display: 'flex',
           flexDirection: 'column',
           height: `${availableHeight}px`,
           overflow: 'hidden',
-          ...(wrapperStyle || {}),
+          ...(style || {}),
         }}
       >
-        {rows.map((rowSpec, index) => {
-          const { extractedProps: rowProps, rest: rowPropsRest } = extractProps(
-            {
-              keys: ['classNames', 'id', 'height', 'key', 'renderRow', 'style'],
-              props: rowSpec,
-            }
-          )
-
-          return (
-            <div
-              className={rowProps.classNames}
-              data-testid={rowProps.id}
-              id={rowProps.id}
-              key={rowProps.key || index}
-              style={
-                rowProps.height
-                  ? {
-                      flex: 'none',
-                      float: 'left',
-                      height: rowProps.height,
-                      ...(rowProps.style || {}),
-                    }
-                  : {
-                      flex: 'auto',
-                      float: 'left',
-                      minHeight: 0, // needed to fix flexbox issue, kind of like: https://css-tricks.com/flexbox-truncated-text/
-                      ...(rowProps.style || {}),
-                    }
-              }
-            >
-              {!rowProps.renderRow &&
-                renderRow &&
-                renderRow(rowProps.key, { ...thisPropsRest, ...rowPropsRest })}
-              {rowProps.renderRow &&
-                rowProps.renderRow({ ...thisPropsRest, ...rowPropsRest })}
-            </div>
-          )
-        })}
+        {children}
       </div>
     )
   }
