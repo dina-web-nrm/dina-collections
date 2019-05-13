97e36c3facd7787f3d4175679a8cce4421b0be28
antonoberg <oberg.anton@gmail.com>
2019-05-13 09:20:46 +0200

    [BACKEND] Add stringSearchMapping

---
+++ packages/backend/src/lib/data/mappings/factories/createStringSearchMapping.js
@@ -0,0 +1,24 @@
+module.exports = function createKeywordMapping({ fieldPath, searchFieldPath }) {
+  return [
+    {
+      elasticsearch: () => {
+        return {
+          ignore_above: 256,
+          normalizer: 'lowerCaseNormalizer',
+          type: 'keyword',
+        }
+      },
+      fieldPath: searchFieldPath,
+    },
+
+    {
+      elasticsearch: () => {
+        return {
+          ignore_above: 256,
+          type: 'keyword',
+        }
+      },
+      fieldPath,
+    },
+  ]
+}
