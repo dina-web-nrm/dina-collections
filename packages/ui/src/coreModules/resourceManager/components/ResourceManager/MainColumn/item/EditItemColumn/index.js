e0ef7c2bd41383c317795872130f4270f6911055
Fredrik Olovsson <fogithub@gmail.com>
2019-05-14 12:53:26 +0200

    [UI] Adapt components to new RowLayout
    Also replace extractProps where possible with lodash pick and remove
    propTypes that are not explicitly used in the current component

--- packages/ui/src/coreModules/resourceManager/components/ResourceManager/MainColumn/item/EditItemColumn/index.js
+++ packages/ui/src/coreModules/resourceManager/components/ResourceManager/MainColumn/item/EditItemColumn/index.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { compose } from 'redux'
+import { pick } from 'lodash'
 
 import { capitalizeFirstLetter } from 'common/src/stringFormatters'
 import { createGetNestedItemById } from 'coreModules/crud/higherOrderComponents'
 import { RowLayout } from 'coreModules/layout/components'
 import { emToPixels } from 'coreModules/layout/utilities'
-import extractProps from 'utilities/extractProps'
 import { EditItemActionBar } from '../ActionBars'
+
+const overflowAuto = { overflow: 'auto' }
 
 const defaultBuildItemHeaders = nestedItem => {
   if (!nestedItem) {
@@ -24,57 +26,40 @@ const propTypes = {
   availableHeight: PropTypes.number.isRequired,
   buildEditItemHeaders: PropTypes.func,
   formName: PropTypes.string,
-  itemId: PropTypes.string.isRequired,
-  loadingDelete: PropTypes.bool,
   nestedItem: PropTypes.object,
-  onInteraction: PropTypes.func.isRequired,
-  relationshipsToCheckBeforeDelete: PropTypes.arrayOf(PropTypes.string),
   renderEditForm: PropTypes.func.isRequired,
   resource: PropTypes.string.isRequired,
 }
 const defaultProps = {
   buildEditItemHeaders: defaultBuildItemHeaders,
   formName: undefined,
-  loadingDelete: false,
   nestedItem: undefined,
-  relationshipsToCheckBeforeDelete: [],
 }
 
-const rows = [
-  {
-    key: 'itemEditForm',
-    style: { overflow: 'auto' },
-  },
-  {
-    height: emToPixels(4.625),
-    key: 'bottomBar',
-  },
-]
+const EditItemColumn = props => {
+  const {
+    availableHeight,
+    buildEditItemHeaders,
+    formName,
+    nestedItem,
+    renderEditForm,
+    resource,
+  } = props
 
-class EditItemColumn extends Component {
-  constructor(props) {
-    super(props)
+  const { itemHeader, itemSubHeader } = buildEditItemHeaders(nestedItem)
 
-    this.renderRow = this.renderRow.bind(this)
-  }
-
-  renderRow(key) {
-    const { buildEditItemHeaders, nestedItem, resource } = this.props
-
-    const { itemHeader, itemSubHeader } = buildEditItemHeaders(nestedItem)
-
-    switch (key) {
-      case 'itemEditForm': {
-        return this.props.renderEditForm({
-          ...this.props,
+  return (
+    <RowLayout availableHeight={availableHeight}>
+      <RowLayout.Row style={overflowAuto}>
+        {renderEditForm({
+          ...props,
           itemHeader,
           itemSubHeader,
-        })
-      }
-      case 'bottomBar': {
-        const { formName } = this.props
-        const { extractedProps } = extractProps({
-          keys: [
+        })}
+      </RowLayout.Row>
+      <RowLayout.Row height={emToPixels(4.625)}>
+        <EditItemActionBar
+          {...pick(props, [
             'fetchOneItemById',
             'fetchRelationshipsBeforeDelete',
             'filterResourceCount',
@@ -85,37 +70,14 @@ class EditItemColumn extends Component {
             'relationshipsToCheckBeforeDelete',
             'resource',
             'transformOutput',
-          ],
-          props: this.props,
-        })
-
-        return (
-          <EditItemActionBar
-            {...extractedProps}
-            formName={formName || `${resource}Edit`}
-            itemHeader={itemHeader}
-            itemSubHeader={itemSubHeader}
-          />
-        )
-      }
-      default: {
-        throw new Error(`Unknown row: ${key}`)
-      }
-    }
-  }
-
-  render() {
-    const { availableHeight, itemId } = this.props
-
-    return (
-      <RowLayout
-        availableHeight={availableHeight}
-        itemId={itemId}
-        renderRow={this.renderRow}
-        rows={rows}
-      />
-    )
-  }
+          ])}
+          formName={formName || `${resource}Edit`}
+          itemHeader={itemHeader}
+          itemSubHeader={itemSubHeader}
+        />
+      </RowLayout.Row>
+    </RowLayout>
+  )
 }
 
 EditItemColumn.defaultProps = defaultProps
