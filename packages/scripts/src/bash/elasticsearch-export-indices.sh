bf8abf2ec0d535677e34bbb4969ef1d0ebc23ae6
antonoberg <oberg.anton@gmail.com>
2019-05-14 14:24:49 +0200

    [SCRIPTS] Refactor elastic export and import scripts
    Also rename them

---
+++ packages/scripts/src/bash/elasticsearch-export-indices.sh
@@ -0,0 +1,14 @@
+FULL_PATH=$(dirname "$0")
+FILE_PATH="${FILE_PATH:-./data/.index}"
+
+while getopts f: option
+ do
+  case "${option}"
+   in
+    f) FILE_PATH=${OPTARG};
+  esac
+ done
+
+$FULL_PATH/elasticsearch-export-index.sh -f $FILE_PATH -n searchspecimen
+$FULL_PATH/elasticsearch-export-index.sh -f $FILE_PATH -n searchplace
+
---
+++ packages/scripts/src/bash/elasticsearch-import-index.sh
@@ -0,0 +1,46 @@
+while getopts f:n: option
+ do
+  case "${option}"
+   in
+    f) FILE_BASE_PATH=${OPTARG};
+  esac
+  case "${option}"
+   in
+    n) INDEX_NAME=${OPTARG};
+  esac
+ done
+
+
+if [ -z "$FILE_BASE_PATH" ]
+then
+   echo "usage: $0 -f <FILE_BASE_PATH> -n <INDEX_NAME>"
+   exit 1
+fi
+
+if [ -z "$INDEX_NAME" ]
+then
+   echo "usage: $0 -f <FILE_BASE_PATH> -n <INDEX_NAME>"
+   exit 1
+fi
+
+
+echo "Importing specimen search index to index $INDEX_NAME"
+
+
+INDEX_PATH="$FILE_BASE_PATH.$INDEX_NAME"
+INDEX_URL="http://127.0.0.1:9200/$INDEX_NAME"
+DELETE_URL="http://127.0.0.1:9200/$INDEX_NAME/_delete_by_query"
+
+
+curl -X POST $DELETE_URL -H 'Content-Type: application/json' -d'
+{
+ "query": {
+     "match_all": {}
+  }
+}
+'
+
+./packages/scripts/node_modules/.bin/elasticdump \
+  --input=$INDEX_PATH \
+  --output=$INDEX_URL \
+  --type=data
