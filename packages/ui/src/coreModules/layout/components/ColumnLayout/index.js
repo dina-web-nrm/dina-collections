bb10d644f9b057fd9b106c83dcf2aced8c7379d5
Fredrik Olovsson <fogithub@gmail.com>
2019-05-14 12:52:07 +0200

    WIP: Keep old Column and RowLayout working for MammalManager

--- packages/ui/src/coreModules/layout/components/ColumnLayout/index.js
+++ packages/ui/src/coreModules/layout/components/ColumnLayout/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
+import extractProps from 'utilities/extractProps'
+
 import Column from './Column'
 
 const propTypes = {
@@ -22,6 +24,99 @@ class ColumnLayout extends Component {
 
   render() {
     const { children, className, dataTestId, id, style } = this.props
+
+    /* eslint-disable react/prop-types */
+    // To be removed after completing resourceManager
+    if (this.props.columns) {
+      const {
+        columns,
+        renderColumn,
+        wrapperClassNames,
+        wrapperId,
+        wrapperStyle,
+      } = this.props
+
+      if (!columns || !columns.length) {
+        return null
+      }
+
+      const { rest: thisPropsRest } = extractProps({
+        keys: [
+          'columns',
+          'renderColumn',
+          'wrapperClassNames',
+          'wrapperId',
+          'wrapperStyle',
+        ],
+        props: this.props,
+      })
+
+      return (
+        <div
+          className={wrapperClassNames}
+          data-testid={wrapperId}
+          id={wrapperId}
+          style={{
+            display: 'flex',
+            ...(wrapperStyle || {}),
+          }}
+        >
+          {columns.map((columnSpec, index) => {
+            const {
+              extractedProps: columnProps,
+              rest: columnPropsRest,
+            } = extractProps({
+              keys: [
+                'classNames',
+                'id',
+                'key',
+                'renderColumn',
+                'style',
+                'width',
+              ],
+              props: columnSpec,
+            })
+
+            return (
+              <div
+                className={columnProps.classNames}
+                data-testid={columnProps.id}
+                id={columnProps.id}
+                key={columnProps.key || index}
+                style={
+                  columnProps.width
+                    ? {
+                        flex: 'none',
+                        float: 'left',
+                        width: columnProps.width,
+                        ...(columnProps.style || {}),
+                      }
+                    : {
+                        flex: 'auto',
+                        float: 'left',
+                        minWidth: 0, // needed to fix flexbox issue, kind of like: https://css-tricks.com/flexbox-truncated-text/
+                        ...(columnProps.style || {}),
+                      }
+                }
+              >
+                {!columnProps.renderColumn &&
+                  renderColumn &&
+                  renderColumn(columnProps.key, {
+                    ...thisPropsRest,
+                    ...columnPropsRest,
+                  })}
+                {columnProps.renderColumn &&
+                  columnProps.renderColumn({
+                    ...thisPropsRest,
+                    ...columnPropsRest,
+                  })}
+              </div>
+            )
+          })}
+        </div>
+      )
+    }
+    /* eslint-enable react/prop-types */
 
     return (
       <div
--- packages/ui/src/coreModules/layout/components/RowLayout/index.js
+++ packages/ui/src/coreModules/layout/components/RowLayout/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
+import extractProps from 'utilities/extractProps'
+
 import Row from './Row'
 
 const propTypes = {
@@ -30,6 +32,92 @@ class RowLayout extends Component {
       id,
       style,
     } = this.props
+
+    /* eslint-disable react/prop-types */
+    // To be removed after completing resourceManager
+    if (this.props.rows) {
+      const {
+        rows,
+        renderRow,
+        wrapperClassNames,
+        wrapperId,
+        wrapperStyle,
+      } = this.props
+
+      if (!rows.length) {
+        return null
+      }
+
+      const { rest: thisPropsRest } = extractProps({
+        keys: [
+          'renderRow',
+          'rows',
+          'wrapperClassNames',
+          'wrapperId',
+          'wrapperStyle',
+        ],
+        props: this.props,
+      })
+
+      return (
+        <div
+          className={wrapperClassNames}
+          data-testid={wrapperId}
+          id={wrapperId}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            height: `${availableHeight}px`,
+            overflow: 'hidden',
+            ...(wrapperStyle || {}),
+          }}
+        >
+          {rows.map((rowSpec, index) => {
+            const {
+              extractedProps: rowProps,
+              rest: rowPropsRest,
+            } = extractProps({
+              keys: ['classNames', 'id', 'height', 'key', 'renderRow', 'style'],
+              props: rowSpec,
+            })
+
+            return (
+              <div
+                className={rowProps.classNames}
+                data-testid={rowProps.id}
+                id={rowProps.id}
+                key={rowProps.key || index}
+                style={
+                  rowProps.height
+                    ? {
+                        flex: 'none',
+                        float: 'left',
+                        height: rowProps.height,
+                        ...(rowProps.style || {}),
+                      }
+                    : {
+                        flex: 'auto',
+                        float: 'left',
+                        minHeight: 0, // needed to fix flexbox issue, kind of like: https://css-tricks.com/flexbox-truncated-text/
+                        ...(rowProps.style || {}),
+                      }
+                }
+              >
+                {!rowProps.renderRow &&
+                  renderRow &&
+                  renderRow(rowProps.key, {
+                    ...thisPropsRest,
+                    ...rowPropsRest,
+                  })}
+                {rowProps.renderRow &&
+                  rowProps.renderRow({ ...thisPropsRest, ...rowPropsRest })}
+              </div>
+            )
+          })}
+        </div>
+      )
+    }
+    /* eslint-enable react/prop-types */
 
     return (
       <div
