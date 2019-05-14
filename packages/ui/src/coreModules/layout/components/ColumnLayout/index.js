ba569d987432cd71c508554b3a3b41fdf19eae95
Fredrik Olovsson <fogithub@gmail.com>
2019-05-14 14:29:52 +0200

    [UI] Refactor ColumnLayout to use .Row & children

--- packages/ui/src/coreModules/layout/components/ColumnLayout/index.js
+++ packages/ui/src/coreModules/layout/components/ColumnLayout/index.js
@@ -1,110 +1,39 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-import extractProps from 'utilities/extractProps'
+import Column from './Column'
 
 const propTypes = {
-  columns: PropTypes.arrayOf(
-    PropTypes.shape({
-      key: PropTypes.string.isRequired,
-      renderColumn: PropTypes.func,
-      style: PropTypes.object,
-      width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-    }).isRequired
-  ),
-  renderColumn: PropTypes.func,
-  wrapperClassNames: PropTypes.string,
-  wrapperId: PropTypes.string,
-  wrapperStyle: PropTypes.object,
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  dataTestId: PropTypes.string,
+  id: PropTypes.string,
+  style: PropTypes.object,
 }
 const defaultProps = {
-  columns: undefined,
-  renderColumn: undefined,
-  wrapperClassNames: undefined,
-  wrapperId: undefined,
-  wrapperStyle: undefined,
+  className: undefined,
+  dataTestId: undefined,
+  id: undefined,
+  style: undefined,
 }
 
 class ColumnLayout extends Component {
+  static Column = Column
+
   render() {
-    const {
-      columns,
-      renderColumn,
-      wrapperClassNames,
-      wrapperId,
-      wrapperStyle,
-    } = this.props
-
-    if (!columns || !columns.length) {
-      return null
-    }
-
-    const { rest: thisPropsRest } = extractProps({
-      keys: [
-        'columns',
-        'renderColumn',
-        'wrapperClassNames',
-        'wrapperId',
-        'wrapperStyle',
-      ],
-      props: this.props,
-    })
+    const { children, className, dataTestId, id, style } = this.props
 
     return (
       <div
-        className={wrapperClassNames}
-        data-testid={wrapperId}
-        id={wrapperId}
+        className={className}
+        data-testid={dataTestId}
+        id={id}
         style={{
           display: 'flex',
-          ...(wrapperStyle || {}),
+          ...(style || {}),
         }}
       >
-        {columns.map((columnSpec, index) => {
-          const {
-            extractedProps: columnProps,
-            rest: columnPropsRest,
-          } = extractProps({
-            keys: ['classNames', 'id', 'key', 'renderColumn', 'style', 'width'],
-            props: columnSpec,
-          })
-
-          return (
-            <div
-              className={columnProps.classNames}
-              data-testid={columnProps.id}
-              id={columnProps.id}
-              key={columnProps.key || index}
-              style={
-                columnProps.width
-                  ? {
-                      flex: 'none',
-                      float: 'left',
-                      width: columnProps.width,
-                      ...(columnProps.style || {}),
-                    }
-                  : {
-                      flex: 'auto',
-                      float: 'left',
-                      minWidth: 0, // needed to fix flexbox issue, kind of like: https://css-tricks.com/flexbox-truncated-text/
-                      ...(columnProps.style || {}),
-                    }
-              }
-            >
-              {!columnProps.renderColumn &&
-                renderColumn &&
-                renderColumn(columnProps.key, {
-                  ...thisPropsRest,
-                  ...columnPropsRest,
-                })}
-              {columnProps.renderColumn &&
-                columnProps.renderColumn({
-                  ...thisPropsRest,
-                  ...columnPropsRest,
-                })}
-            </div>
-          )
-        })}
+        {children}
       </div>
     )
   }
