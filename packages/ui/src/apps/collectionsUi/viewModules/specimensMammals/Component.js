586f7a3f1b635915637bb56091e915e1e9aa8190
Fredrik Olovsson <fogithub@gmail.com>
2019-05-24 23:19:22 +0200

    [UI] Remove specimensMammals viewModule

--- packages/ui/src/apps/collectionsUi/viewModules/specimensMammals/Component.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import MammalManager from 'serviceModules/specimen/components/MammalManager'
-
-const SpecimensMammals = () => <MammalManager />
-
-export default SpecimensMammals
