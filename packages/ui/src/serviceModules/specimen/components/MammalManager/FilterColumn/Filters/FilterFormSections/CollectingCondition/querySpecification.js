8e8e69f06aab16b6b0ed07d6b7f10ff906dc0a00
Fredrik Olovsson <fogithub@gmail.com>
2019-05-24 23:18:34 +0200

    [UI] Remove MammalManager

--- packages/ui/src/serviceModules/specimen/components/MammalManager/FilterColumn/Filters/FilterFormSections/CollectingCondition/querySpecification.js
+++
@@ -1,8 +0,0 @@
-import createCheckboxSpecification from 'coreModules/search/utilities/queryBuilderFactory/utilities/factories/tagTypesSpecification'
-
-export default createCheckboxSpecification({
-  aggregationFunctionName: 'aggregateConditionTags',
-  fieldName: 'collectingCondition.collectingConditions',
-  matchFilterFunctionName: 'matchConditionTags',
-  sectionName: 'collectingCondition',
-})
