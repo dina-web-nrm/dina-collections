b8b920aa6ca5284726819cdc432315f391e471d1
Fredrik Olovsson <fogithub@gmail.com>
2019-05-24 23:18:34 +0200

    [UI] Remove MammalManager

--- packages/ui/src/serviceModules/specimen/components/MammalManager/FilterColumn/Filters/FilterFormSections/Sex/querySpecification.js
+++
@@ -1,8 +0,0 @@
-import createCheckboxSpecification from 'coreModules/search/utilities/queryBuilderFactory/utilities/factories/tagTypesSpecification'
-
-export default createCheckboxSpecification({
-  aggregationFunctionName: 'aggregateSexTags',
-  fieldName: 'sex.sex',
-  matchFilterFunctionName: 'matchSexTags',
-  sectionName: 'collectingSex',
-})
