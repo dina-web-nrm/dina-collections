99c0e58b1b0909dc380d11e628c8317949e4ee8c
Fredrik Olovsson <fogithub@gmail.com>
2019-05-24 23:18:34 +0200

    WIP: Remove MammalManager

--- packages/ui/src/serviceModules/specimen/components/MammalManager/FilterColumn/Filters/FilterFormSections/Bones/querySpecification.js
+++
@@ -1,8 +0,0 @@
-import createCheckboxSpecification from 'coreModules/search/utilities/queryBuilderFactory/utilities/factories/tagTypesSpecification'
-
-export default createCheckboxSpecification({
-  aggregationFunctionName: 'aggregateBoneTags',
-  fieldName: 'bones.bones',
-  matchFilterFunctionName: 'matchBoneTags',
-  sectionName: 'bones',
-})
