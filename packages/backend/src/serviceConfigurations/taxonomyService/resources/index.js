e76d9b760358f5d250f4bc398a68c7911843b1a9
Ida Li <ida.li@nrm.se>
2019-05-29 18:23:19 +0200

    [BACKEND] Add searchTaxonName

--- packages/backend/src/serviceConfigurations/taxonomyService/resources/index.js
+++ packages/backend/src/serviceConfigurations/taxonomyService/resources/index.js
@@ -1,3 +1,4 @@
 exports.searchTaxon = require('./searchTaxon')
+exports.searchTaxonName = require('./searchTaxonName')
 exports.taxon = require('./taxon')
 exports.taxonName = require('./taxonName')
