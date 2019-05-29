55251d777faa433094ae8bdf41edf696fadc29a8
Ida Li <ida.li@nrm.se>
2019-05-29 18:46:21 +0200

    [BACKEND] Add indexHook for searchTaxonName

--- packages/backend/src/serviceConfigurations/taxonomyService/resources/taxonName/postHooks/index.js
+++ packages/backend/src/serviceConfigurations/taxonomyService/resources/taxonName/postHooks/index.js
@@ -6,7 +6,21 @@ const {
   createRegisterResourceActivityHook,
 } = require('../../../../historyService/serviceInteractions')
 
+const {
+  createIndexJob,
+  rebuildInProgress,
+} = require('../../../serviceInteractions')
+
+const { createIndexHook } = require('../../../../../lib/data/hooks')
+
+const indexHook = createIndexHook({
+  createIndexJob,
+  rebuildInProgress,
+  resource: 'searchTaxonName',
+})
+
 exports.create = [
+  indexHook,
   createRegisterResourceActivityHook({
     action: 'create',
     service: 'taxonomyService',
@@ -14,6 +28,7 @@ exports.create = [
 ]
 
 exports.update = [
+  indexHook,
   createRegisterResourceActivityHook({
     action: 'update',
     service: 'taxonomyService',
