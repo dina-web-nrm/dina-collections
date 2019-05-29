48ba14854007e9014c118a6fde9adf1e14b33e7a
Ida Li <ida.li@nrm.se>
2019-05-29 18:29:44 +0200

    [BACKEND] Add searchTaxonName fieldsSpecification

---
+++ packages/backend/src/serviceConfigurations/taxonomyService/resources/searchTaxonName/fieldsSpecification/index.js
@@ -0,0 +1,8 @@
+const id = require('./fields/id')
+const rank = require('./fields/rank')
+const rubinNumber = require('./fields/rubinNumber')
+const scientificName = require('./fields/scientificName')
+
+module.exports = {
+  fields: [id, rank, rubinNumber, scientificName],
+}
