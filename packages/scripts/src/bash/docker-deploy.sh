3a64e0d0a54ba2d8e09a9d127ecdc68029ff29c4
antonoberg <oberg.anton@gmail.com>
2019-06-04 09:25:34 +0200

    [SCRIPTS] Add m option to docker deploy
    If provided will run migrations

--- packages/scripts/src/bash/docker-deploy.sh
+++ packages/scripts/src/bash/docker-deploy.sh
@@ -2,11 +2,15 @@
 # ./packages/scripts/src/bash/docker-deploy.sh  -t 4.5.2
 FULL_PATH=$(dirname "$0")
 
-while getopts t: option
+while getopts t:m: option
  do
   case "${option}"
    in
     t) TAG=${OPTARG};;
+  esac
+  case "${option}"
+   in
+    m) MIGRATE=${OPTARG};;
   esac
  done
 
@@ -22,3 +26,12 @@ $FULL_PATH/git-checkout-tag.sh -t $TAG
 
 # uses local images if they exist, otherwise fetches the images from hub.docker.com
 TAG=$TAG docker-compose up -d
+
+
+if [ -z "$MIGRATE" ]
+then
+  echo "Not running migrations"
+else
+  echo "Running migrations"
+  $FULL_PATH/docker-migrate-latest.sh -t $TAG
+fi
