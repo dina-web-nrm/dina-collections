3eed760c41cbeff4eb10351b314dc8d04405d256
Fredrik Olovsson <fogithub@gmail.com>
2019-06-05 18:36:32 +0200

    [UI] Add assertion in e2e table test
    Needed to avoid test runner making changes before input has updated
    (faster-than-human kind of issue)

--- packages/ui/cypress/e2e/app/specimens/read/table.js
+++ packages/ui/cypress/e2e/app/specimens/read/table.js
@@ -99,6 +99,7 @@ export default () =>
       cy.getByText('Mus musculoides')
 
       cy.getInputByParentTestId('currentTableRowInput')
+        .should('have.value', '3')
         .clear()
         .type('16')
       cy.getByText('500001')
