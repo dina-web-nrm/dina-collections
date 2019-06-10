b2990749808e4c3643974dfda48a3771dfc68a34
Fredrik Olovsson <fogithub@gmail.com>
2019-06-10 15:13:14 +0200

    [UI] Add cypress commands to reset elastic indices

--- packages/ui/cypress/support/db/index.js
+++ packages/ui/cypress/support/db/index.js
@@ -6,10 +6,50 @@ Cypress.Commands.add('resetDevelopmentSqlDb', () => {
   )
 })
 
+Cypress.Commands.add('resetSearchNormalizedAgentIndex', () => {
+  cy.log('Resetting elasticsearch searchnormalizedagent index')
+  return cy.exec(
+    'cd ../../ && ./packages/scripts/src/bash/elasticsearch-import-indices.sh -f ./data/sample.index -n searchnormalizedagent',
+    { log: false }
+  )
+})
+
+Cypress.Commands.add('resetSearchPlaceIndex', () => {
+  cy.log('Resetting elasticsearch searchplace index')
+  return cy.exec(
+    'cd ../../ && ./packages/scripts/src/bash/elasticsearch-import-indices.sh -f ./data/sample.index -n searchplace',
+    { log: false }
+  )
+})
+
 Cypress.Commands.add('resetSearchSpecimenIndex', () => {
   cy.log('Resetting elasticsearch searchspecimen index')
   return cy.exec(
     'cd ../../ && ./packages/scripts/src/bash/elasticsearch-import-indices.sh -f ./data/sample.index -n searchspecimen',
+    { log: false }
+  )
+})
+
+Cypress.Commands.add('resetSearchStorageLocationIndex', () => {
+  cy.log('Resetting elasticsearch searchstoragelocation index')
+  return cy.exec(
+    'cd ../../ && ./packages/scripts/src/bash/elasticsearch-import-indices.sh -f ./data/sample.index -n searchstoragelocation',
+    { log: false }
+  )
+})
+
+Cypress.Commands.add('resetSearchTaxonIndex', () => {
+  cy.log('Resetting elasticsearch searchtaxon index')
+  return cy.exec(
+    'cd ../../ && ./packages/scripts/src/bash/elasticsearch-import-indices.sh -f ./data/sample.index -n searchtaxon',
+    { log: false }
+  )
+})
+
+Cypress.Commands.add('resetSearchTaxonNameIndex', () => {
+  cy.log('Resetting elasticsearch searchtaxonname index')
+  return cy.exec(
+    'cd ../../ && ./packages/scripts/src/bash/elasticsearch-import-indices.sh -f ./data/sample.index -n searchtaxonname',
     { log: false }
   )
 })
