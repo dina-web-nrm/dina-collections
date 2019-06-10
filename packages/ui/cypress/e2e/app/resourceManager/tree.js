a6ee547fddc7066c91fef416737dff9172905069
Fredrik Olovsson <fogithub@gmail.com>
2019-06-10 22:56:21 +0200

    WIP: Add tree view e2e tests

---
+++ packages/ui/cypress/e2e/app/resourceManager/tree.js
@@ -0,0 +1,16 @@
+export default () =>
+  describe('tree', () => {
+    before(() => {
+      cy.resetSearchPlaceIndex()
+    })
+
+    describe('starting in tree', () => {
+      it.only('does', () => {
+        cy.visit('/app/localities?mainColumn=tree')
+        cy.get('[data-testid=tree] .row').should('have.length', 1)
+
+        cy.getByTestId('expandIcon').click()
+        cy.get('[data-testid=tree] .row').should('have.length', 4)
+      })
+    })
+  })
