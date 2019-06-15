847da9f5885022c3b764daee7a23a656904b2bc5
Fredrik Olovsson <fogithub@gmail.com>
2019-06-15 18:15:41 +0200

    [UI] Update TaxonManager filter and table columns

--- packages/ui/src/serviceModules/taxon/components/TaxonManager/tableColumnSpecifications.js
+++ packages/ui/src/serviceModules/taxon/components/TaxonManager/tableColumnSpecifications.js
@@ -1,197 +1,48 @@
-import React from 'react'
-
-const findParentWithSpecificRank = (item, rank) => {
-  if (!item) {
-    return null
-  }
-
-  if (item && item.acceptedTaxonName && item.acceptedTaxonName.rank === rank) {
-    return item
-  }
-
-  if (!(item && item.parent)) {
-    return null
-  }
-
-  return findParentWithSpecificRank(item.parent, rank)
-}
-
 const tableColumnSpecifications = [
   {
-    fieldPath: 'acceptedTaxonName.name',
+    fieldPath: 'acceptedName',
     label: 'modules.taxon.fieldLabels.acceptedTaxonName.name',
     width: 350,
   },
   {
-    fieldPath: 'acceptedTaxonName.rank',
+    fieldPath: 'acceptedNameRank',
     label: 'modules.taxon.fieldLabels.acceptedTaxonName.rank',
     width: 350,
   },
 
   {
-    buildText: ({ value }) => {
-      const parent = findParentWithSpecificRank(value, 'class')
-      if (!parent) {
-        return ''
-      }
-
-      const { acceptedTaxonName } = parent
-      if (!acceptedTaxonName) {
-        return ''
-      }
-
-      if (parent.deactivatedAt) {
-        return (
-          <span style={{ color: 'red' }}>{`${
-            acceptedTaxonName.name
-          } (removed)`}</span>
-        )
-      }
-
-      return acceptedTaxonName.name
-    },
-    fieldPath: '',
+    fieldPath: 'classAcceptedName',
     label: 'modules.taxon.fieldLabels.class',
     width: 250,
   },
   {
-    buildText: ({ value }) => {
-      const parent = findParentWithSpecificRank(value, 'order')
-      if (!parent) {
-        return ''
-      }
-
-      const { acceptedTaxonName } = parent
-      if (!acceptedTaxonName) {
-        return ''
-      }
-
-      if (parent.deactivatedAt) {
-        return (
-          <span style={{ color: 'red' }}>{`${
-            acceptedTaxonName.name
-          } (removed)`}</span>
-        )
-      }
-
-      return acceptedTaxonName.name
-    },
-    fieldPath: '',
+    fieldPath: 'orderAcceptedName',
     label: 'modules.taxon.fieldLabels.order',
     width: 250,
   },
   {
-    buildText: ({ value }) => {
-      const parent = findParentWithSpecificRank(value, 'family')
-      if (!parent) {
-        return ''
-      }
-
-      const { acceptedTaxonName } = parent
-      if (!acceptedTaxonName) {
-        return ''
-      }
-
-      if (parent.deactivatedAt) {
-        return (
-          <span style={{ color: 'red' }}>{`${
-            acceptedTaxonName.name
-          } (removed)`}</span>
-        )
-      }
-
-      return acceptedTaxonName.name
-    },
-    fieldPath: '',
+    fieldPath: 'familyAcceptedName',
     label: 'modules.taxon.fieldLabels.family',
     width: 250,
   },
   {
-    buildText: ({ value }) => {
-      const parent = findParentWithSpecificRank(value, 'genus')
-      if (!parent) {
-        return ''
-      }
-
-      const { acceptedTaxonName } = parent
-      if (!acceptedTaxonName) {
-        return ''
-      }
-
-      if (parent.deactivatedAt) {
-        return (
-          <span style={{ color: 'red' }}>{`${
-            acceptedTaxonName.name
-          } (removed)`}</span>
-        )
-      }
-
-      return acceptedTaxonName.name
-    },
-    fieldPath: '',
+    fieldPath: 'genusAcceptedName',
     label: 'modules.taxon.fieldLabels.genus',
     width: 250,
   },
   {
-    buildText: ({ value }) => {
-      const parent = findParentWithSpecificRank(value, 'species')
-      if (!parent) {
-        return ''
-      }
-
-      const { acceptedTaxonName } = parent
-      if (!acceptedTaxonName) {
-        return ''
-      }
-
-      if (parent.deactivatedAt) {
-        return (
-          <span style={{ color: 'red' }}>{`${
-            acceptedTaxonName.name
-          } (removed)`}</span>
-        )
-      }
-
-      return acceptedTaxonName.name
-    },
-    fieldPath: '',
+    fieldPath: 'speciesAcceptedName',
     label: 'modules.taxon.fieldLabels.species',
     width: 250,
   },
   {
-    buildText: ({ value }) => {
-      const parent = findParentWithSpecificRank(value, 'subspecies')
-      if (!parent) {
-        return ''
-      }
-
-      const { acceptedTaxonName } = parent
-      if (!acceptedTaxonName) {
-        return ''
-      }
-
-      if (parent.deactivatedAt) {
-        return (
-          <span style={{ color: 'red' }}>{`${
-            acceptedTaxonName.name
-          } (removed)`}</span>
-        )
-      }
-
-      return acceptedTaxonName.name
-    },
-    fieldPath: '',
+    fieldPath: 'subspeciesAcceptedName',
     label: 'modules.taxon.fieldLabels.subspecies',
     width: 250,
   },
-
   {
     buildText: ({ value: vernacularNames }) => {
-      return vernacularNames
-        .map(vernacularName => {
-          return vernacularName.name
-        })
-        .join(', ')
+      return vernacularNames.join(', ')
     },
     fieldPath: 'vernacularNames',
     label: 'modules.taxon.fieldLabels.vernacularNames.name',
@@ -199,11 +50,7 @@ const tableColumnSpecifications = [
   },
   {
     buildText: ({ value: synonyms }) => {
-      return synonyms
-        .map(synonym => {
-          return synonym.name
-        })
-        .join(', ')
+      return synonyms.join(', ')
     },
     fieldPath: 'synonyms',
     label: 'modules.taxon.fieldLabels.synonyms.name',
