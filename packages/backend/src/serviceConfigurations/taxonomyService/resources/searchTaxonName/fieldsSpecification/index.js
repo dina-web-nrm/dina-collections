d46356a9c1fc37cd02f56f865c17b22ce4d14ff6
Fredrik Olovsson <fogithub@gmail.com>
2019-06-15 22:33:17 +0200

    [BACKEND] Add taxonNameType field

--- packages/backend/src/serviceConfigurations/taxonomyService/resources/searchTaxonName/fieldsSpecification/index.js
+++ packages/backend/src/serviceConfigurations/taxonomyService/resources/searchTaxonName/fieldsSpecification/index.js
@@ -2,7 +2,8 @@ const id = require('./fields/id')
 const name = require('./fields/name')
 const rank = require('./fields/rank')
 const rubinNumber = require('./fields/rubinNumber')
+const taxonNameType = require('./fields/taxonNameType')
 
 module.exports = {
-  fields: [id, name, rank, rubinNumber],
+  fields: [id, name, rank, rubinNumber, taxonNameType],
 }
