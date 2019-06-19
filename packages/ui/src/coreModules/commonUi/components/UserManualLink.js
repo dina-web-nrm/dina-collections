91a1c839feb38e6c72eceb20f3731d873507f753
antonoberg <oberg.anton@gmail.com>
2019-06-19 10:51:55 +0200

    [UI] Fix footer cypress test

--- packages/ui/src/coreModules/commonUi/components/UserManualLink.js
+++ packages/ui/src/coreModules/commonUi/components/UserManualLink.js
@@ -2,17 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const propTypes = {
+  dataTestId: PropTypes.string,
   text: PropTypes.string,
 }
 
 const defaultProps = {
+  dataTestId: 'userManualLink',
   text: 'User manual',
 }
 
-const UserManualLink = ({ text, ...rest }) => {
+const UserManualLink = ({ dataTestId, text, ...rest }) => {
   return (
     <a
       {...rest}
+      data-testid={dataTestId}
       href="/images/DINA Collections User Manual 2019 June 17.pdf"
       target="_blank"
     >
